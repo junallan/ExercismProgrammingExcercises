8ad2812bddfd99eab3ad99f782e3d850f20c5f65
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-02-23 08:35:18 -0700

    Remove unecessary spaces

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -2,7 +2,6 @@ using System;
 
 public static class Leap
 {
-     private static bool IsDivisibleBy(int year, int n) => (year % n) == 0;
-    public static bool IsLeapYear(int year) => IsDivisibleBy(year, 4) && (!IsDivisibleBy(year, 100) || IsDivisibleBy(year, 400));
-    
+    private static bool IsDivisibleBy(int year, int n) => (year % n) == 0;
+    public static bool IsLeapYear(int year) => IsDivisibleBy(year, 4) && (!IsDivisibleBy(year, 100) || IsDivisibleBy(year, 400)); 
 }
