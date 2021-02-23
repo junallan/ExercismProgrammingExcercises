c948c0f50c743d59ba481720c4d7834765990e13
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-02-23 08:14:46 -0700

    Excercism solution to leap problem

---
+++ csharp/leap/Leap.cs
@@ -0,0 +1,14 @@
+using System;
+
+public static class Leap
+{
+     private static bool IsDivisibleBy(int year, int n)
+    {
+        return (year % n) == 0;
+    }
+
+    public static bool IsLeapYear(int year)
+    {
+        return IsDivisibleBy(year, 4) && (!IsDivisibleBy(year, 100) || IsDivisibleBy(year, 400));
+    }
+}
