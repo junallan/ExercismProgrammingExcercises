1ad982b547e1e9690f3aeba0a867459ef95fa73d
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-02-25 07:54:28 -0700

    Exercism solution to learp problem

---
+++ javascript/leap/leap.js
@@ -0,0 +1,3 @@
+const isDivisibleBy = (year, n) => (year % n) == 0;
+
+export const isLeap = (year) => isDivisibleBy(year, 4) ? !isDivisibleBy(year, 100) || isDivisibleBy(year, 400) ? true : false : false;
