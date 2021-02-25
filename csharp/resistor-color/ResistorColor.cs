254392354c9fdade7eec7ece787de7683e06459b
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-02-25 08:29:06 -0700

    Solution to exercism resistor color problem

---
+++ csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    public static string[] ColorMap => new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+
+    public static int ColorCode(string color) =>  Array.FindIndex(ColorMap, x => x.Equals(color));
+    public static string[] Colors() => ColorMap;
+}
