9d914dc4ccbe5fe311d18dd7dbcc57377327ebc3
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-02-27 08:17:23 -0700

    Solution to exercism space age problem

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -13,37 +13,28 @@ public class SpaceAge
         _seconds = seconds;
     }
 
-    private double DetermineAge(int orbitalPeriod) => Math.Round(_seconds / SecondsTranslation(orbitalPeriod), 2);
+    private struct OrbitalPeriodRelativeToEarthYears
+    {
+        public const double Earth = 1;
+        public const double Mercury = 0.2408467;
+        public const double Venus = 0.61519726;
+        public const double Mars = 1.8808158;
+        public const double Jupiter = 11.862615;
+        public const double Saturn = 29.447498;
+        public const double Uranus = 84.016846;
+        public const double Neptune =164.79132;
+    }
+
+    private double DetermineAge(double orbitalPeriodDays) => Math.Round(_seconds / orbitalPeriodDays, 2);
     private double PlanteOrbitalPeriodInEarthDays(double orbitalPertiod) => orbitalPertiod * EARTH_ORBITAL_PERIOD_DAYS;
     private double SecondsTranslation(double days) => days * HOURS_IN_DAY * MINUTES_IN_HOUR * SECONDS_IN_MINUTE;
     
-
-    public double OnEarth() => DetermineAge(SecondsTranslation(EARTH_ORBITAL_PERIOD_DAYS));
-    public double OnMercury() => Math.Round(_seconds / SecondsTranslation(0.2408467 * EARTH_ORBITAL_PERIOD_DAYS),2);
-    public double OnVenus() => Math.Round(_seconds / SecondsTranslation(0.61519726 * EARTH_ORBITAL_PERIOD_DAYS), 2);
-
-    public double OnMars()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public double OnJupiter()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public double OnSaturn()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public double OnUranus()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
-    public double OnNeptune()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public double OnEarth() => DetermineAge(SecondsTranslation(OrbitalPeriodRelativeToEarthYears.Earth * EARTH_ORBITAL_PERIOD_DAYS));
+    public double OnMercury() => DetermineAge(SecondsTranslation(OrbitalPeriodRelativeToEarthYears.Mercury * EARTH_ORBITAL_PERIOD_DAYS));
+    public double OnVenus() => DetermineAge(SecondsTranslation(OrbitalPeriodRelativeToEarthYears.Venus * EARTH_ORBITAL_PERIOD_DAYS));
+    public double OnMars() => DetermineAge(SecondsTranslation(OrbitalPeriodRelativeToEarthYears.Mars * EARTH_ORBITAL_PERIOD_DAYS));
+    public double OnJupiter() => DetermineAge(SecondsTranslation(OrbitalPeriodRelativeToEarthYears.Jupiter * EARTH_ORBITAL_PERIOD_DAYS));
+    public double OnSaturn() => DetermineAge(SecondsTranslation(OrbitalPeriodRelativeToEarthYears.Saturn * EARTH_ORBITAL_PERIOD_DAYS));
+    public double OnUranus() => DetermineAge(SecondsTranslation(OrbitalPeriodRelativeToEarthYears.Uranus * EARTH_ORBITAL_PERIOD_DAYS));
+    public double OnNeptune() => DetermineAge(SecondsTranslation(OrbitalPeriodRelativeToEarthYears.Neptune * EARTH_ORBITAL_PERIOD_DAYS));
 }
