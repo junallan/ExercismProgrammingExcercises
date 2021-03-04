4257231511c605b65dadb74e05b6e0a138add16d
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-03-04 07:48:07 -0700

    Solution to exercism Armstrong problem

---
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        var numberLength = (double)$"{number}".Length;
+        var result = $"{number}".Aggregate(0, (total, digit) => total + (int)Math.Pow(char.GetNumericValue(digit), numberLength));
+
+        return result == (double)number;
+    }
+}
