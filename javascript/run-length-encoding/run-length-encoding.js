b7b71b8527e3f2c7ced9880934a122819a140e48
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-03-06 13:52:19 -0700

    Simplified solution to exercism run length encoding problem

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -1,64 +1,8 @@
 export const encode = (value) => {
-	let encodedValue = '';
-	let previousValue = ''; 
-	let repeatedValueCount = 0;
-
-	[...value].forEach(function (element) {
-		if (previousValue !== '') {
-			if (element === previousValue) {
-				repeatedValueCount++;
-			}
-			else {
-				if (repeatedValueCount === 0) {
-					encodedValue += previousValue;
-				}
-				else {
-					encodedValue += (++repeatedValueCount).toString() + previousValue;
-					repeatedValueCount = 0;
-				}			
-			}
-		}	
-
-		previousValue = element;
-	});
-
-	if (repeatedValueCount > 0) {
-		encodedValue += (++repeatedValueCount).toString() + previousValue;
-	}
-	else {
-		encodedValue += previousValue;
-	}
-
-	return encodedValue;
+	return value.replace(/(.)\1*/g, (match, repeatValue) => match.length === 1 ? repeatValue : match.length + repeatValue);
 };
 
 
 export const decode = (value) => {
-	let valueParsed = value.split(/(\d+)/).filter(x => x.length > 0);
-
-	let decodedValue = '';
-	let i = 0;
-	
-	while (i < valueParsed.length) {
-		if (!isNaN(valueParsed[i])) {
-			if (valueParsed[i + 1].length == 1) {
-				decodedValue += valueParsed[i + 1].repeat(valueParsed[i]);			
-			}
-			else {
-				decodedValue += valueParsed[i + 1][0].repeat(valueParsed[i]);
-				let otherElements = valueParsed[i + 1].substring(1, valueParsed[i + 1].length);
-				[...otherElements].forEach(function (element) {
-					decodedValue += element;
-				});
-			}
-
-			i += 2;
-		}
-		else {
-			decodedValue += valueParsed[i];
-			i++;
-		}		
-	}
-
-	return decodedValue;
+	return value.replace(/(\d+)(.)/g, (_, count, data) => count === 1 ? data : data.repeat(count));
 };
