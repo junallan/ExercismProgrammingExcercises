4194bb7321106204616a7a1871a3b19edf3745ad
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-03-07 14:15:11 -0700

    Refactored roman numeral problem to traverse symbol mapping with recursion

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -1,56 +1,29 @@
-const symbolMapping = {
-	I:1,
-	V:5,
-	X: 10,
-	L: 50,
-	C: 100,
-	D: 500,
-	M: 1000
-};
+let symbolMappings = [
+	{ value: 1000, numeral: "M" },
+	{ value: 900, numeral: "CM" },
+	{ value: 500, numeral: "D" },
+	{ value: 400, numeral: "CD" },
+	{ value: 100, numeral: "C" },
+	{ value: 90, numeral: "XC" },
+	{ value: 50, numeral: "L" },
+	{ value: 40, numeral: "XL" },
+	{ value: 10, numeral: "X" },
+	{ value: 9, numeral: "IX" },
+	{ value: 5, numeral: "V" },
+	{ value: 4, numeral: "IV" },
+	{ value: 1, numeral: "I" }
+];
 
 export const toRoman = (number) => {
-	if (number === 0) {
-		return '';
+	let romanNumeral = '';
+
+	let mappedSymbol = symbolMappings.find(x => x.value <= number);
+
+	if (mappedSymbol !== undefined) {
+		return mappedSymbol.value === number ? mappedSymbol.numeral : `${mappedSymbol.numeral}${toRoman(number - mappedSymbol.value)}`;	 
 	}
-	else if (number < symbolMapping.V - symbolMapping.I) {
-		return 'I'.repeat(number);
-	}
-	else if (number === symbolMapping.V - symbolMapping.I) {
-		return 'IV'
-	}
-	else if (number < symbolMapping.X - symbolMapping.I) {
-		return `V${'I'.repeat(number - symbolMapping.V)}`;
-	}
-	else if (number === symbolMapping.X - symbolMapping.I) {
-		return 'IX';
-	}
-	else if (number < symbolMapping.L- symbolMapping.X) {
-		return `X${toRoman(number - symbolMapping.X)}`;
-	}
-	else if (number < symbolMapping.L) {
-		return `XL${toRoman(number - (symbolMapping.L - symbolMapping.X))}`;
-	}
-	else if (number < symbolMapping.C - symbolMapping.X) {
-		return `L${toRoman(number - symbolMapping.L)}`;
-	}
-	else if (number < symbolMapping.C) {
-		return `XC${toRoman(number - (symbolMapping.C - symbolMapping.X))}`;
-	}
-	else if (number < symbolMapping.D - symbolMapping.C) {
-		return `C${toRoman(number - symbolMapping.C)}`;
-	}
-	else if (number < symbolMapping.D) {
-		return `CD${toRoman(number - (symbolMapping.D - symbolMapping.C))}`;
-	}
-	else if (number < symbolMapping.M - symbolMapping.C) {
-		return `D${toRoman(number - symbolMapping.D)}`;
-	}
-	else if (number < symbolMapping.M) {
-		return `CM${toRoman(number - (symbolMapping.M - symbolMapping.C))}`;
-	}
-	else {
-		return `M${toRoman(number - symbolMapping.M)}`;
-	}
+
+	return romanNumeral;
 };
 
 
