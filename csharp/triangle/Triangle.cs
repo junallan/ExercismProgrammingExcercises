970f12a83f415dc16a46c41d8bcd4ed92adac0be
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-03-09 21:06:57 -0700

    Solution to exercism triangle problem

---
+++ csharp/triangle/Triangle.cs
@@ -0,0 +1,9 @@
+using System;
+
+public static class Triangle
+{
+    public static bool IsScalene(double side1, double side2, double side3) => side1 != side2 && side2 != side3 && IsValidLength(side1, side2, side3);
+    public static bool IsIsosceles(double side1, double side2, double side3) => ((side1 == side2 || side2 == side3 || side1 == side3) && IsValidLength(side1, side2, side3));
+    public static bool IsEquilateral(double side1, double side2, double side3) => side1 == side2 && side2 == side3 && IsValidLength(side1, side2, side3);
+    private static bool IsValidLength(double side1, double side2, double side3) => ((side1 + side2) > side3) && ((side1 + side3) > side2) && ((side2 + side3) > side1);
+}
