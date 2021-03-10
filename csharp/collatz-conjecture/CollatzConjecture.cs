b7dab1115902329c9548b29de99ea0cf3edea459
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-03-10 08:17:00 -0700

    Solution to exercism Collatz Conjecture problem

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        if (number <= 0) { throw new ArgumentOutOfRangeException(); }
+
+        if (number == 1) { return 0; }
+
+        return (number % 2) == 0 ? 1 + Steps(number / 2) : 1 + Steps(3 * number + 1);
+    }
+}
