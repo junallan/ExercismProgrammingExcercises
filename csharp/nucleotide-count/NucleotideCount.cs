75790f6498b61c6e60508925568e00556264f964
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-03-11 07:16:00 -0700

    Refactored exercism nucleotide count solution

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -7,11 +7,15 @@ public static class NucleotideCount
     public static IDictionary<char, int> Count(string sequence)
     {
         Dictionary<char, int> dnaStrands = new Dictionary<char, int> { ['A'] = 0, ['C'] = 0, ['G'] = 0, ['T'] = 0 };
-        
-        var invalidStrands = sequence.AsEnumerable().Where(strand => !dnaStrands.ContainsKey(strand));
-        if (invalidStrands.Any()) { throw new ArgumentException(); }
 
-        sequence.ToList().ForEach(strand => dnaStrands[strand]++);
+        if (string.IsNullOrEmpty(sequence)){ return dnaStrands; }
+
+        foreach(var strand in sequence)
+        {
+            if (!dnaStrands.ContainsKey(strand)) {  throw new ArgumentException(); }
+
+            dnaStrands[strand]++;
+        }
 
         return dnaStrands;
     }
