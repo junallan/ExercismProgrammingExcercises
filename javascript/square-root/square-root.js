13363cc18900abfd992d4b1fc6a9edd01494fcd4
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-03-13 15:25:34 -0700

    Partial completion of exercism square root problem.  Still need to fix bug of square root of 196.

--- javascript/square-root/square-root.js
+++ javascript/square-root/square-root.js
@@ -3,34 +3,67 @@
 // convenience to get you started writing code faster.
 //
 
-export const squareRoot = () => {
+export const squareRoot = (number) => {
 	//TODO: ALGORITHM STEPS
 	// Bit split
 	// Evaluate _
 	// Subtract
 	// Bring down and double digit
-	throw new Error('Remove this statement and implement this function');
+	
+	let binaryComponents = splitToBinaryComponents(number);
+	let placeholderNumber = undefined;
+	let placeholderEvaluationResult = 0;
+	let evaluatedResult = [];
+	let subtractedResult = 0;
 
+	for (let i = 0; i < binaryComponents.length; i++) {
+		subtractedResult += binaryComponents[i];
+		placeholderEvaluationResult = placeholderEvaluation(binaryComponents[i], placeholderNumber);
+		evaluatedResult.push(placeholderEvaluationResult);
+
+		placeholderNumber = placeholderEvaluationResult;
+		subtractedResult = (subtractedResult - placeholderNumber) << 2;
+	}
+
+	return evaluatedResult.reduce(function (accumulator, currentValue, currentIndex, array) {
+		return accumulator + (currentValue === 0 && currentIndex === (array.length - 1) ? 0 : (currentValue * 2) ** ((array.length - 1) - currentIndex));
+	}, 0);
+};
+
+function splitToBinaryComponents(number) {
+	//let number = 5;//255;//81;
 	let maskedNumber = 3;
-	let splitIndex = 2;
-
-	console.log(81 & maskedNumber);
-	maskedNumber = maskedNumber << splitIndex;
-	console.log((81 & maskedNumber) >> 2);
-	maskedNumber = maskedNumber << splitIndex;
-	console.log((81 & maskedNumber) >> 4);
-	maskedNumber = maskedNumber << splitIndex;
-	console.log((81 & maskedNumber) >> 6);
-
-
-
-	let number = 81;
-	let maskedNumber = 3;
-
-	console.log(maskedNumber);
+	let evaluatedNumber = 0;
+	let shifts = 0;
+	let numberParsed = [];
 
 	do {
+		evaluatedNumber = number & maskedNumber;
+		numberParsed.push(evaluatedNumber >> shifts);
+		shifts += 2;
 		maskedNumber = maskedNumber << 2;
-		console.log(maskedNumber);
 	} while (number >= maskedNumber)
-};
+
+	evaluatedNumber = number & maskedNumber;
+
+	let result = evaluatedNumber >> shifts;
+	if (result > 0) {
+		numberParsed.push(result);
+	}
+
+	numberParsed = numberParsed.reverse();
+
+	return numberParsed;
+}
+
+function placeholderEvaluation(number, placeholderNumber) {
+	if (placeholderNumber === undefined) { return number; }
+
+	let evaluatedNumber = (placeholderNumber << 2) + 1;
+	if (number >= evaluatedNumber) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
