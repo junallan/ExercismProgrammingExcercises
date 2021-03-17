3ad66f58bebf52b05df5e34b2c21a029dd3ac09f
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-03-17 06:08:02 -0600

    Change solution to remove unused list data structure and made functions more concise with lambda expression

--- csharp/allergies/Allergies.cs
+++ csharp/allergies/Allergies.cs
@@ -17,34 +17,8 @@ public enum Allergen
 public class Allergies
 {
     private readonly int _mask;
-    private List<Allergen> _foodAllergies = new List<Allergen>();
-
-    public Allergies(int mask)
-    {
-        _mask = mask;
-
-        DetermineFoodAllergies();
-    }
-
-    private void DetermineFoodAllergies()
-    {
-        if (IsAllergicTo(Allergen.Eggs)) { _foodAllergies.Add(Allergen.Eggs); }
-        if (IsAllergicTo(Allergen.Peanuts)) { _foodAllergies.Add(Allergen.Peanuts); }
-        if (IsAllergicTo(Allergen.Shellfish)) { _foodAllergies.Add(Allergen.Shellfish); }
-        if (IsAllergicTo(Allergen.Strawberries)) { _foodAllergies.Add(Allergen.Strawberries); }
-        if (IsAllergicTo(Allergen.Tomatoes)) { _foodAllergies.Add(Allergen.Tomatoes); }
-        if (IsAllergicTo(Allergen.Chocolate)) { _foodAllergies.Add(Allergen.Chocolate); }
-        if (IsAllergicTo(Allergen.Pollen)) { _foodAllergies.Add(Allergen.Pollen); }
-        if (IsAllergicTo(Allergen.Cats)) { _foodAllergies.Add(Allergen.Cats); }
-    }
-
-    public bool IsAllergicTo(Allergen allergen)
-    {
-        return (_mask & (int)allergen) > 0;
-    }
-
-    public Allergen[] List()
-    {
-        return Enum.GetValues(typeof(Allergen)).Cast<Allergen>().Where(IsAllergicTo).ToArray();
-    }
+    
+    public Allergies(int mask) => _mask = mask;
+    public bool IsAllergicTo(Allergen allergen) => (_mask & (int)allergen) > 0; 
+    public Allergen[] List() =>  Enum.GetValues(typeof(Allergen)).Cast<Allergen>().Where(IsAllergicTo).ToArray();
 }
