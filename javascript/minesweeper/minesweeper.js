f6ec2ad51279200ab59c0f78f8e0840da6052596
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-03-24 07:59:41 -0600

    Removed comments

--- javascript/minesweeper/minesweeper.js
+++ javascript/minesweeper/minesweeper.js
@@ -2,10 +2,6 @@ const MineField = '*';
 const EmptyField = ' ';
 
 export const annotate = (input) => {
-	//if (isAllElementsSetTo(EmptyField, input) || isAllElementsSetTo(MineField, input)) {
-	//	return input;
-	//}
-
 	let evaluatedInput = [];
 	
 	for (let rowIndex = 0; rowIndex < input.length; rowIndex++) {
@@ -27,10 +23,6 @@ export const annotate = (input) => {
 	return evaluatedInput;
 };
 
-//function isAllElementsSetTo(element, input) {
-//	return input.reduce((accumulator, currentValue) => accumulator && [...currentValue].every(x => x === element), true);
-//}
-
 function isCoordinateInBoard(xCoordinate, yCoordinate, input) {
 	return (xCoordinate >= 0 && yCoordinate >= 0 && xCoordinate < input.length && yCoordinate < input[xCoordinate].length);
 }
@@ -39,7 +31,6 @@ function mineCount(xCoordinate, yCoordinate, input) {
 	let surroudingNeighbors = [[xCoordinate - 1, yCoordinate - 1], [xCoordinate - 1, yCoordinate], [xCoordinate - 1, yCoordinate + 1],
 							   [xCoordinate, yCoordinate - 1], [xCoordinate, yCoordinate + 1],
 							   [xCoordinate + 1, yCoordinate - 1], [xCoordinate + 1, yCoordinate], [xCoordinate + 1, yCoordinate + 1]];
-
 
 	return surroudingNeighbors.reduce((accumulator, coordinate) =>
 									accumulator + (isCoordinateInBoard(coordinate[0], coordinate[1], input) && input[coordinate[0]][coordinate[1]] === MineField ? 1 : 0), 0); 
