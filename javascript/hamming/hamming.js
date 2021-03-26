d7733e693f0164c78abc0675858d166205a5a912
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-03-26 07:06:12 -0600

    Removed unecessary conditional in evaluation

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -4,7 +4,7 @@ export const compute = (strand1, strand2) => {
 	if (strand2.length === 0) { throw new Error('right strand must not be empty'); }
 	if (strand1.length !== strand2.length) { throw new Error('left and right strands must be of equal length'); }
 
-	let differenceCount = [...strand1].reduce((accumulator, currentValue, currentIndex) => { return accumulator + (currentValue === strand2[currentIndex] ? 0 : 1) }, 0);
+	let differenceCount = [...strand1].reduce((accumulator, currentValue, currentIndex) => { return accumulator + (currentValue !== strand2[currentIndex]) }, 0);
 
 	return differenceCount;
 };
