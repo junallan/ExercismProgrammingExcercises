59e396d2e49f12a88666122184cc92855e7a23ba
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-03-28 10:07:02 -0600

    refactor to eliminate unecessary get range date function

--- csharp/meetup/Meetup.cs
+++ csharp/meetup/Meetup.cs
@@ -26,20 +26,12 @@ public class Meetup
     public DateTime Day(DayOfWeek dayOfWeek, Schedule schedule)
     {
         var daysInMonth = DateTime.DaysInMonth(_year, _month);
-        DateTime startDate = new DateTime();
-
-        startDate = new DateTime(_year, _month, (int)schedule);
-
-        return schedule == Schedule.Last ? GetDateRange(new DateTime(_year, _month, (int)Schedule.Fourth), new DateTime(_year,_month, daysInMonth)).Where(x => x.DayOfWeek == dayOfWeek).Last() :
-                                            GetDateRange(startDate, startDate.AddDays(6)).Where(x => x.DayOfWeek == dayOfWeek).Single();
-    }
-
-    private IEnumerable<DateTime> GetDateRange(DateTime startDate, DateTime endDate)
-    {
-        while(startDate <= endDate)
-        {
-            yield return startDate;
-            startDate = startDate.AddDays(1);
-        }
+        
+        return schedule == Schedule.Last ? Enumerable.Range((int)Schedule.Fourth, daysInMonth + 1 - (int)Schedule.Fourth)
+                                                                .Select(day => new DateTime(_year, _month, day))
+                                                                .Where(d => d.DayOfWeek == dayOfWeek).Last() :
+                                           Enumerable.Range((int)schedule, 7)
+                                                                .Select(day => new DateTime(_year, _month, day))
+                                                                .Where(d => d.DayOfWeek == dayOfWeek).Single();
     }
 }
