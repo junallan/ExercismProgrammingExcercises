ab913050882299afdb8191c5db0fa2f7bf951b0b
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-05 08:27:10 -0600

    Solution to exercism sum of multiples problem

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        List<int> multipleNumbers = new List<int>();
+
+        foreach(var multiple in multiples.Where(m => m > 0))
+        {
+            var i = 1;
+            var accumulation = multiple * i;
+
+            while(accumulation < max)
+            {
+                if (!multipleNumbers.Contains(accumulation)) 
+                {
+                    multipleNumbers.Add(accumulation);
+                }
+                
+                i++;
+                accumulation = multiple * i;
+            }
+        }
+
+        return multipleNumbers.Sum();
+    }
+}
