789278dde982f13b8e553630831b7fa675b4375f
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-09 08:00:49 -0600

    Solution to exercism reverse string problem

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input) => input == string.Empty || input.Length == 1 ? input : $"{ Reverse(input.Substring(1))}{input[0]}";
+}
