e619f371d205abb62f6afa5acdc9807bb8369df6
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-12 07:26:22 -0600

    Solution to exercism difference of squares problem

---
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max) => (int)Math.Pow((Enumerable.Range(1, max).Sum()),2);
+    
+    public static int CalculateSumOfSquares(int max) => Enumerable.Range(1, max).Aggregate(0, (total, i) => total + (int)Math.Pow(i, 2));
+
+    public static int CalculateDifferenceOfSquares(int max)=> CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+}
