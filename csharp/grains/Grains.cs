94de8d62acca96b0124c3f143f435c6a6dff6331
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-13 08:08:44 -0600

    Solution to exercism grains problem

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Linq;
+
+public static class Grains
+{
+    public static ulong Square(int n) => (n < 1 || n > 64) ? throw new ArgumentOutOfRangeException() : (ulong)Math.Pow(2, n - 1);
+    public static ulong Total() => (ulong)Enumerable.Range(1, 64).Aggregate(0, (accum, val) => accum == 0 ? 1 : accum + (int)Square(val));
+}
