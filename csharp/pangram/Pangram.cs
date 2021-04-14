15a5d5206943162e57d9ddfd993f09d616c538bf
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-14 07:55:04 -0600

    Refactored, no need using lambda expression

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -3,5 +3,5 @@ using System.Linq;
 
 public static class Pangram
 {
-    public static bool IsPangram(string input) => "abcdefghijklmnopqrstuvwxyz".All(c => input.ToLower().Contains(c));
+    public static bool IsPangram(string input) => "abcdefghijklmnopqrstuvwxyz".All(input.ToLower().Contains);
 }
