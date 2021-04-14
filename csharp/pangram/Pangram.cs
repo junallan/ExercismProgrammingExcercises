a1cee9ada43dc4acc89dd191d6533ca8dc1f53f9
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-14 07:48:58 -0600

    Solution to exercism Pangram problem

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+       var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+       return alphabet == new string(input.ToLower().Where(c => alphabet.Contains(c)).OrderBy(x => x).Distinct().ToArray());
+    }
+}
