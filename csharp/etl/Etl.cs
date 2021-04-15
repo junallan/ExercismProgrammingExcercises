f9cb9f18d49f5a4ad3e41e8533d0dfa9ddf70a83
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-15 07:36:25 -0600

    Key method of etl solution

--- csharp/etl/Etl.cs
+++ csharp/etl/Etl.cs
@@ -7,11 +7,18 @@ public static class Etl
     public static Dictionary<string, int> Transform(Dictionary<int, string[]> old)
     {
         Dictionary<string, int> tranformedData = new Dictionary<string, int>();
-        
-        foreach(KeyValuePair<int, string[]> entry in old)
+
+        //old.Keys.ToList().ForEach(k => old[k].ToList().ForEach(v => tranformedData.Add(v.ToLower(), k)));
+
+        foreach (var key in old.Keys)
         {
-            entry.Value.ToList().ForEach(v => tranformedData.Add(v.ToLower(), entry.Key));
+            old[key].ToList().ForEach(v => tranformedData.Add(v.ToLower(), key));
         }
+
+        //foreach(KeyValuePair<int, string[]> entry in old)
+        //{
+        //    entry.Value.ToList().ForEach(v => tranformedData.Add(v.ToLower(), entry.Key));
+        //}
 
         return tranformedData;
     }
