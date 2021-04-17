439d7358924448083060f6ca2bd3cca431a796f2
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-17 11:51:52 -0600

    Solution done with select many method

--- csharp/raindrops/Raindrops.cs
+++ csharp/raindrops/Raindrops.cs
@@ -5,17 +5,18 @@ using System.Text;
 
 public static class Raindrops
 {
-    public static string Convert(int number) => 
-        new[] { (Factor: 3, Message: "Pling"), 
-                (Factor: 5, Message: "Plang"), 
-                (Factor: 7, Message: "Plong"), }
-        .Aggregate(
-            number.ToString(),
-            (accumulate, next) =>
-            {
-                if (number % next.Factor != 0) { return accumulate; }
+    public static string Convert(int number)
+    {
+        bool isFactor(int number, int factor) => number % factor == 0;
 
-                return int.TryParse(accumulate, out _) ? 
-                        next.Message : string.Concat(accumulate, next.Message);
-            });
+        var specialFactors = new[] { (Factor: 3, Message: "Pling"),
+                (Factor: 5, Message: "Plang"),
+                (Factor: 7, Message: "Plong") };
+
+       var messages = specialFactors
+                        .Select(x => isFactor(number, x.Factor) ? x.Message : string.Empty)
+                        .SelectMany(x => x).ToArray();
+
+        return messages.Any() ? new string(messages) : number.ToString();
+    }
 }
