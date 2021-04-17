4c1c0ec4b5a310d79b9c0225249a5edad330bfb8
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-17 11:54:13 -0600

    Eliminate local function

--- csharp/raindrops/Raindrops.cs
+++ csharp/raindrops/Raindrops.cs
@@ -7,14 +7,12 @@ public static class Raindrops
 {
     public static string Convert(int number)
     {
-        bool isFactor(int number, int factor) => number % factor == 0;
-
         var specialFactors = new[] { (Factor: 3, Message: "Pling"),
                 (Factor: 5, Message: "Plang"),
                 (Factor: 7, Message: "Plong") };
 
-       var messages = specialFactors
-                        .Select(x => isFactor(number, x.Factor) ? x.Message : string.Empty)
+        var messages = specialFactors
+                        .Select(x => number % x.Factor == 0 ? x.Message : string.Empty)
                         .SelectMany(x => x).ToArray();
 
         return messages.Any() ? new string(messages) : number.ToString();
