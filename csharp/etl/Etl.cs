7748b015e487e784489fc9e62594ce198d631fbc
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-17 08:54:20 -0600

    Expression body for transformation

--- csharp/etl/Etl.cs
+++ csharp/etl/Etl.cs
@@ -4,8 +4,5 @@ using System.Linq;
 
 public static class Etl
 {
-    public static Dictionary<string, int> Transform(Dictionary<int, string[]> old)
-    {
-        return old.SelectMany(x => x.Value, (scoreLetter, letter) => new { Score = scoreLetter.Key, Letter = letter }).ToDictionary(x => x.Letter.ToLower(), x => x.Score);
-    }
+    public static Dictionary<string, int> Transform(Dictionary<int, string[]> old) => old.SelectMany(x => x.Value, (scoreLetter, letter) => new { Score = scoreLetter.Key, Letter = letter }).ToDictionary(x => x.Letter.ToLower(), x => x.Score);
 }
