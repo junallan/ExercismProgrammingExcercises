742ced622a49b97d1454b633b5bdfb9c54d675f0
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-17 11:20:00 -0600

    Cleanup to make more readable

--- csharp/raindrops/Raindrops.cs
+++ csharp/raindrops/Raindrops.cs
@@ -6,12 +6,16 @@ using System.Text;
 public static class Raindrops
 {
     public static string Convert(int number) => 
-        new[] { (Factor: 3, Message: "Pling"), (Factor: 5, Message: "Plang"), (Factor: 7, Message: "Plong"), }
-            .Aggregate(number.ToString(),
-                        (accumulate, next) =>
-                        {
-                            if (number % next.Factor != 0) { return accumulate; }
+        new[] { (Factor: 3, Message: "Pling"), 
+                (Factor: 5, Message: "Plang"), 
+                (Factor: 7, Message: "Plong"), }
+        .Aggregate(
+            number.ToString(),
+            (accumulate, next) =>
+            {
+                if (number % next.Factor != 0) { return accumulate; }
 
-                            return int.TryParse(accumulate, out _) ? next.Message : string.Concat(accumulate, next.Message);
-                        });
+                return int.TryParse(accumulate, out _) ? 
+                        next.Message : string.Concat(accumulate, next.Message);
+            });
 }
