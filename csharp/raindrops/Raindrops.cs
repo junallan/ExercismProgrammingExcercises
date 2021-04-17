6e2a702681b91041cc6158e6c85e9e9d2cef5dc7
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-17 11:55:48 -0600

    Eliminate uncessary list variable

--- csharp/raindrops/Raindrops.cs
+++ csharp/raindrops/Raindrops.cs
@@ -7,11 +7,9 @@ public static class Raindrops
 {
     public static string Convert(int number)
     {
-        var specialFactors = new[] { (Factor: 3, Message: "Pling"),
-                (Factor: 5, Message: "Plang"),
-                (Factor: 7, Message: "Plong") };
-
-        var messages = specialFactors
+        var messages = new[] {  (Factor: 3, Message: "Pling"),
+                                (Factor: 5, Message: "Plang"),
+                                (Factor: 7, Message: "Plong") }
                         .Select(x => number % x.Factor == 0 ? x.Message : string.Empty)
                         .SelectMany(x => x).ToArray();
 
