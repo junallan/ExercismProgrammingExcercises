d7cda2099cb8937e3b7ab4647d85dca2a46b9570
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-21 07:40:21 -0600

    Solution to exercism space age problem

---
+++ javascript/space-age/space-age.js
@@ -0,0 +1,22 @@
+export const age = (planet, secondsTravelled) => {
+	const planetSecondPeriods = {
+		earth: 31557600,
+		mercury: 7600543.82,
+		venus: 19414149.05,
+		mars: 59354032.69,
+		jupiter: 374355659.1,
+		saturn: 929292362.9,
+		uranus: 2651370019,
+		neptune: 5200418560
+	};
+
+	//caluculation to deal with rare case of floating point precision in calculation being off, below calculation is more accurate
+	let round = (number, decimalPlaces) => Number(Math.round(number + 'e' + decimalPlaces) + 'e-' + decimalPlaces);
+
+	//const round = (number, decimalPlaces) => {
+	//	const factorOfTen = Math.pow(10, decimalPlaces);
+	//	return Math.round(number * factorOfTen) / factorOfTen;
+	//};
+
+	return round(secondsTravelled / planetSecondPeriods[planet], 2);
+};
