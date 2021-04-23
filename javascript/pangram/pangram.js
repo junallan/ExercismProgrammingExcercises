d8fb2e433469eea8382ebe720faffa0e08774c28
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-23 06:40:57 -0600

    Refactored removing unecessary operations and fixed regular expression

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,8 +1,6 @@
 export const isPangram = (sentence) => {
-	let sentenceFilteredLetters = sentence.replace(/[^A-Za-z]*/g, "").toLowerCase();
-	let sentenceFiltredUniqueLetters = [...new Set(Array.from(sentenceFilteredLetters))];
-	let sentenceUniqueLettersSorted = sentenceFiltredUniqueLetters.sort().join("");
+	let sentenceFilteredLetters = sentence.replace(/[^A-Za-z]/g, "").toLowerCase();
+	let sentenceFiltredUniqueLetters = new Set(Array.from(sentenceFilteredLetters));
 
-	//let sentenceUniqueAlphabetLetters = [...new Set(Array.from(sentence.replace(/^[A-Za-z]+$/, "")))].sort().join("");
-	return sentenceUniqueLettersSorted === 'abcdefghijklmnopqrstuvwxyz';
+	return sentenceFiltredUniqueLetters.size === 26;
 };
