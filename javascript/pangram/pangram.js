910aa6e57c57543bd9f6f48a1be02db1f6938b72
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-23 06:45:16 -0600

    Revised refactored eliminating uncessary operations and fixed regular expression

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,6 +1,6 @@
 export const isPangram = (sentence) => {
 	let sentenceFilteredLetters = sentence.replace(/[^A-Za-z]/g, "").toLowerCase();
 	let sentenceFiltredUniqueLetters = new Set(Array.from(sentenceFilteredLetters));
-
+	
 	return sentenceFiltredUniqueLetters.size === 26;
 };
