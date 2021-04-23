a8a8afc9dc339c32dd7a77961ea5798cabaef7e3
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-23 07:49:37 -0600

    Refactored with optimization utilizing Sorted list and performing sort on add operation instead of retrieval of Roster and Grade

--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -5,13 +5,14 @@ using System.Linq;
 
 public class GradeSchool
 {
-    private Hashtable _classrooms = new Hashtable();
+    private SortedList<int, SortedSet<string>> _classrooms = new SortedList<int, SortedSet<string>>();
 
     public void Add(string student, int grade)
     {
+       
         if(_classrooms.ContainsKey(grade))
         {
-            var classroom = (SortedSet<string>)_classrooms[grade];
+            var classroom = _classrooms[grade];
             classroom.Add(student);
         }
         else
@@ -20,26 +21,7 @@ public class GradeSchool
         }
     }
 
-    public IEnumerable<string> Roster()
-    {
-        List<string> allStudents = new List<string>();
-        SortedSet<int> grades = new SortedSet<int>();
-        
-        foreach(var grade in _classrooms.Keys)
-        {
-            grades.Add((int)grade);
-        }
-  
-        foreach (var grade in grades)
-        {
-            foreach (var student in (SortedSet<string>)_classrooms[grade])
-            {
-                allStudents.Add(student);
-            }
-        }
+    public IEnumerable<string> Roster() => _classrooms.Values.SelectMany(student => student);
 
-        return allStudents;
-    }
-
-    public IEnumerable<string> Grade(int grade) => (SortedSet<string>)_classrooms[grade] ?? new SortedSet<string>();
+    public IEnumerable<string> Grade(int grade) => _classrooms.GetValueOrDefault(grade, new SortedSet<string>()); 
 }
