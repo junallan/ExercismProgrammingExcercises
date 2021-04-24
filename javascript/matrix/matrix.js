1ca3a3b4f0e43dbc577644b0985bae0b096e908c
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-24 12:09:21 -0600

    Refactored using single list to set rows and columns

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,7 +1,9 @@
 export class Matrix {
     constructor(matrix) {
-        this._rows= [];    
+        let elements = [];
+        this._rows = [];    
         this._columns = [];
+
         let matrixRow = matrix.split("\n");
         this._matrixRowsCount = matrixRow.length;
         this._matrixColumnsCount = matrixRow[0].split(" ").length;
@@ -11,21 +13,21 @@ export class Matrix {
             let elementRow = [];
 
             for (let j = 0; j < this._matrixColumnsCount; j++) {
-                elementRow.push(Number(matrixColumn[j]));
+                elements.push(Number(matrixColumn[j]));
             }
+        }
 
-            this._rows.push(elementRow);
+        for (let i = 0; i < this._matrixRowsCount; i++) {
+            let rowItems = elements.filter((_, index) => Math.floor(index / this._matrixColumnsCount) === i);
+            this._rows.push(rowItems);
         }
 
         for (let i = 0; i < this._matrixColumnsCount; i++) {
-            let elementColumn = [];
-            for (let j = 0; j < this._matrixRowsCount; j++) {
-                elementColumn.push(this._rows[j][i]);
-            }
-            this._columns.push(elementColumn);
+            let columnItems = elements.filter((_, index) => index % this._matrixColumnsCount === i);
+            this._columns.push(columnItems);
+               
         }
   }
-
 
   get rows() {
       return this._rows;
