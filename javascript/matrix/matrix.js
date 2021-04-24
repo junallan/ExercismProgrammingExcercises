fcec96d6f7b01043893d4ae19711b14d0d89b0fc
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-24 10:38:04 -0600

    Cleanup of code

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,28 +1,26 @@
 export class Matrix {
     constructor(matrix) {
-        this._matrix = [];
-       
+        this._rows= [];    
         this._columns = [];
         let matrixRow = matrix.split("\n");
         this._matrixRowsCount = matrixRow.length;
         this._matrixColumnsCount = matrixRow[0].split(" ").length;
 
-        for (let i = 0; i < matrixRow.length; i++){
+        for (let i = 0; i < this._matrixRowsCount; i++){
             let matrixColumn = matrixRow[i].split(" ");
             let elementRow = [];
-            for (let j = 0; j < matrixColumn.length; j++) {
+
+            for (let j = 0; j < this._matrixColumnsCount; j++) {
                 elementRow.push(Number(matrixColumn[j]));
             }
 
-            this._matrix.push(elementRow);
+            this._rows.push(elementRow);
         }
-
-        let flattenedMatrix = this._matrix.flat();
 
         for (let i = 0; i < this._matrixColumnsCount; i++) {
             let elementColumn = [];
             for (let j = 0; j < this._matrixRowsCount; j++) {
-                elementColumn.push(this._matrix[j][i]);
+                elementColumn.push(this._rows[j][i]);
             }
             this._columns.push(elementColumn);
         }
@@ -30,7 +28,7 @@ export class Matrix {
 
 
   get rows() {
-      return this._matrix;
+      return this._rows;
   }
 
   get columns() {
