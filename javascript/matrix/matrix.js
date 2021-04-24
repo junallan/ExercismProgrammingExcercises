76ac611e4651b5221ae01dbc616ba1756ede02d7
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-24 12:20:13 -0600

    Refactored to eliminate nested for loop

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,21 +1,14 @@
 export class Matrix {
     constructor(matrix) {
-        let elements = [];
         this._rows = [];    
         this._columns = [];
 
         let matrixRow = matrix.split("\n");
+    
         this._matrixRowsCount = matrixRow.length;
         this._matrixColumnsCount = matrixRow[0].split(" ").length;
 
-        for (let i = 0; i < this._matrixRowsCount; i++){
-            let matrixColumn = matrixRow[i].split(" ");
-            let elementRow = [];
-
-            for (let j = 0; j < this._matrixColumnsCount; j++) {
-                elements.push(Number(matrixColumn[j]));
-            }
-        }
+        let elements = matrix.split("\n").join().split(" ").join().split(",").map(Number);
 
         for (let i = 0; i < this._matrixRowsCount; i++) {
             let rowItems = elements.filter((_, index) => Math.floor(index / this._matrixColumnsCount) === i);
@@ -24,8 +17,7 @@ export class Matrix {
 
         for (let i = 0; i < this._matrixColumnsCount; i++) {
             let columnItems = elements.filter((_, index) => index % this._matrixColumnsCount === i);
-            this._columns.push(columnItems);
-               
+            this._columns.push(columnItems);         
         }
   }
 
