062a423de6c54d9fad6dd764c55262d4dd3bb6a2
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-26 06:56:11 -0600

    Renamed items variable

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -2,9 +2,9 @@ export const transform = (data) => {
 	let dataMap = new Map(Object.entries(data));
 	let transformedMap = new Map();
 
-	dataMap.forEach((item, key) => {
-		for (let i = 0; i < item.length; i++) {
-			transformedMap.set(new String(item[i]).toLowerCase(), parseInt(key));
+	dataMap.forEach((items, key) => {
+		for (let i = 0; i < items.length; i++) {
+			transformedMap.set(new String(items[i]).toLowerCase(), parseInt(key));
 		}
 	});
 
