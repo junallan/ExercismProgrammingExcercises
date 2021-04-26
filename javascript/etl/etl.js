0ec62c5ace5edca26500848a8c11f517dbbb8dbb
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-26 07:17:35 -0600

    Refactored to remove unecessary Map object

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,12 +1,12 @@
 export const transform = (data) => {
 	let dataMap = new Map(Object.entries(data));
-	let transformedMap = new Map();
+	let transformedMap = {};
 
 	dataMap.forEach((items, key) => {
 		for (let i = 0; i < items.length; i++) {
-			transformedMap.set(new String(items[i]).toLowerCase(), parseInt(key));
+			transformedMap[new String(items[i]).toLowerCase()] = parseInt(key);
 		}
 	});
 
-	return Object.fromEntries(transformedMap);
+	return transformedMap;
 };
