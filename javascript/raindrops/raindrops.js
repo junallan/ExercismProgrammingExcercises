c1c3aae82edd2f5a0adbb0a4fd2ce0836b1ffbd7
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-27 07:34:54 -0600

    Solution to exercism raindrops problem

---
+++ javascript/raindrops/raindrops.js
@@ -0,0 +1,13 @@
+export const convert = (number) => {
+	let sounds = { 3: 'Pling' , 5: 'Plang' , 7: 'Plong' };
+
+	let response = "";
+
+	Object.keys(sounds).forEach(function (key) {
+		if (number % key === 0) {
+			response += sounds[key];
+		}
+	});
+
+	return response.length === 0 ? number.toString() : response;
+};
