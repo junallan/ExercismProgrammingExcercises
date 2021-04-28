28859fa5f571f3df06f9996989459593deb344e5
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-28 08:01:23 -0600

    Use built in hasOwnProperty method

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -5,7 +5,7 @@ export const countWords = (sentence) => {
 	let wordMap = {};
 
 	for (let i = 0; i < words.length; i++) {	
-		wordMap[words[i]] = wordMap[words[i]] ? ++wordMap[words[i]] : 1;
+		wordMap[words[i]] = wordMap.hasOwnProperty([words[i]]) ? ++wordMap[words[i]] : 1;
 	}
 
 	return wordMap;
