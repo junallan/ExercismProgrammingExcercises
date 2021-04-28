c6834b4d4cca1c6fceda7ecc252abf73011eba32
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-04-28 07:45:59 -0600

    Solution to exercism word count problem

---
+++ javascript/word-count/word-count.js
@@ -0,0 +1,13 @@
+export const countWords = (sentence) => {
+	//const regex = /\w+(?:[']?\w+)*/g;
+	const regex = /\w+([']?\w+)*/g;
+	const words = sentence.toLowerCase().match(regex);
+	let wordMap = {};
+
+	for (let i = 0; i < words.length; i++) {
+		
+		wordMap[words[i]] = typeof wordMap[words[i]] === 'undefined' ? 1 : wordMap[words[i]] + 1;
+	}
+
+	return wordMap;
+};
