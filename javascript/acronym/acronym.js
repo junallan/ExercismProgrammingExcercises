b62e7d07192606710b701b64277cf92ebae52020
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-05-02 08:31:18 -0600

    Refactored to utilize built in reduce function

--- javascript/acronym/acronym.js
+++ javascript/acronym/acronym.js
@@ -1,11 +1,5 @@
 export const parse = (name) => {
 	let parsedName = name.match(/[A-Z]+'?[A-Z]?/gi);
 
-	let acronym = '';
-
-	for (let i = 0; i < parsedName.length; i++) {
-		acronym += parsedName[i][0].toUpperCase();
-	}
-
-	return acronym;
+	return parsedName.reduce((accumulator, word) => { return accumulator + word[0].toUpperCase() }, '');
 }
