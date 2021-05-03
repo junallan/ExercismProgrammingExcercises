1c419c3aa98a679fe1935ff9aff0467fd632db0e
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-05-03 07:44:38 -0600

    Solution to exercism high scores problem

---
+++ javascript/high-scores/high-scores.js
@@ -0,0 +1,22 @@
+export class HighScores {
+  constructor(scores) {
+      this._scores = scores;
+  }
+
+  get scores() {
+      return this._scores;
+  }
+
+  get latest() {
+      return this._scores.slice(-1)[0];
+  }
+
+  get personalBest() {
+      return Math.max(...this._scores);
+  }
+
+  get personalTopThree() {
+      return [...this._scores].sort((a, b) => b - a)
+                              .slice(0, this._scores.length < 3 ? this._scores.length : 3);
+  }
+}
