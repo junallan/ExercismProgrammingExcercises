d135ed7a0d8d37c739b8501e278bd52345b3697c
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-05-09 08:08:30 -0600

    Solution ot exercism series problem

---
+++ javascript/series/series.js
@@ -0,0 +1,22 @@
+export class Series {
+  constructor(series) {
+      this.series = series;
+  }
+
+  slices(sliceLength) {
+      let allSlices = [];
+
+      if (this.series.length === 0) { throw Error('series cannot be empty'); }
+      if (sliceLength > this.series.length) { throw Error('slice length cannot be greater than series length'); }
+      if (sliceLength === 0) { throw Error('slice length cannot be zero'); }
+      if (sliceLength < 0) { throw Error('slice length cannot be negative'); }
+
+      for (let i = 0; i + sliceLength <= this.series.length; i++) {
+          allSlices.push(this.series.substring(i, i + sliceLength)
+                                    .split('')
+                                    .map(n => parseInt(n)));
+      }
+
+      return allSlices;
+  }
+}
