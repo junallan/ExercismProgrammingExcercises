e624df87c65dcb431e42128f390b6ee3052ff721
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-05-17 07:49:58 -0600

    Refactored eliminating uncessary function

--- javascript/prime-factors/prime-factors.js
+++ javascript/prime-factors/prime-factors.js
@@ -1,29 +1,20 @@
 export const primeFactors = (number) => {
-	let factors = [];
-	let startDivisor = 2;
+	let divisors = [];
+	let currentDivisor = 2;
 	let factorForNumber = number;
 
 	while (factorForNumber !== 1) {
-		const { result, factor } = firstFactorsOf(factorForNumber, startDivisor);
-		factorForNumber = result;
-		startDivisor = factor;
-
-		factors.push(factor);
-	}
-
-	return factors;
-};
-
-function firstFactorsOf(number, startDivisor) {
-	for (let i = startDivisor; i <= number; i++) {
-		let remainder = number % i;
-
-		if (remainder === 0) {
-			return { result: number / i, factor: i };
+		if (factorForNumber % currentDivisor !== 0) {
+			currentDivisor++;
+		}
+		else {
+			divisors.push(currentDivisor);
+			factorForNumber /= currentDivisor; 
 		}
 	}
 
-	return { result: 1, factor: number };
-}
+	return divisors;
+};
+
 
 
