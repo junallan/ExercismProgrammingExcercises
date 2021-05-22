b1b294ac1ed54f4cd05e732c06b067536cf3cdd3
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-05-22 15:12:51 -0600

    Solution to exercism Proverb problem

---
+++ csharp/proverb/Proverb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects)
+    {
+        List<string> messages = new List<string>();
+
+        if (subjects.Length == 0) { return subjects; }
+
+        for(int i=0; i<subjects.Length-1; i++)
+        {
+            messages.Add($"For want of a {subjects[i]} the {subjects[i+1]} was lost.");
+        }
+
+        messages.Add($"And all for the want of a {subjects[0]}.");
+
+        return messages.ToArray();
+    }
+}
