0266b204fa33d5f3cf9543c2364314d33eacd0cf
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-05-23 09:51:38 -0600

    Refactored t use anonymous function to eliminate conditional logic

---
+++ csharp/secret-handshake/SecretHandshake.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SecretHandshake
+{
+    public static string[] Commands(int commandValue)
+    {
+        const int ReverseCommand = 16;
+
+        Func<int,bool> isCode = (commandToEvaluate) => (commandValue & commandToEvaluate) > 0;
+
+        Dictionary<int, string> handshake = new Dictionary<int, string>
+        {
+            { 1, "wink" },
+            { 2, "double blink" },
+            { 4, "close your eyes" },
+            { 8, "jump"}
+        };
+
+        var commands = handshake.Keys.Where(code => isCode(code))
+                                     .Select(code => handshake[code]);
+
+        return isCode(ReverseCommand) ? commands.Reverse().ToArray() : commands.ToArray();
+    }
+}
