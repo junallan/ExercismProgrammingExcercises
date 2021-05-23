eb93ea8dd096b0af68ed69cb164ccef8a069949f
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-05-22 18:46:51 -0600

    Solution to exercism perfect number problem

---
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        if(number == 1) { return Classification.Deficient; }
+        if (number < 1) { throw new ArgumentOutOfRangeException(); }
+
+        List<int> factors = new List<int>() {1};
+        int checkNumber = 2;
+
+        while(checkNumber < number)
+        {
+            if (IsFactor(number, checkNumber))
+            {
+                if (factors.Contains(checkNumber)) { break; }
+
+                factors.Add(checkNumber);
+
+                var otherFactor = number / checkNumber;
+                if (otherFactor == checkNumber) { break; }
+
+                factors.Add(otherFactor);
+            }
+
+            checkNumber++;
+        }
+
+        var summation = factors.Sum();
+
+        if (summation == number) { return Classification.Perfect; }
+        else if (summation > number) { return Classification.Abundant; }
+        else { return Classification.Deficient; };
+    }
+
+    private static bool IsFactor(int number, int startFactor) =>  number % startFactor == 0;
+}
