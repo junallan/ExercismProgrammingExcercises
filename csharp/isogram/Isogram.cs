332206cec8ddc261f7ce0eb264539f065ffb0923
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-05-23 08:47:24 -0600

    Solution to exercism Isogram problem

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        string parsedWord = Regex.Replace(word, "[-\\s]", string.Empty);
+        Match match = Regex.Match(parsedWord, @"^.*(.).*\1.*$", RegexOptions.IgnoreCase);
+
+        return !match.Success;
+    }
+}
