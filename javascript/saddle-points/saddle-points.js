f02a20bc33a47a2fc56c5f6b09e6d0dbaedebbb0
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-06-05 15:35:47 -0600

    Refactored to remove unnecessary secondary looping

--- javascript/saddle-points/saddle-points.js
+++ javascript/saddle-points/saddle-points.js
@@ -1,23 +1,5 @@
 export const saddlePoints = (grid) => {
 	let saddlePoints = [];
-	let coordinatesPointsOfInterestRows = [];
-
-	for (let i = 0; i < grid.length; i++) {
-		let gridRow = grid[i];
-	
-		let rowMaxValue = Math.max(...gridRow);
-		
-		let idxRow = gridRow.indexOf(rowMaxValue);
-		
-		while (idxRow !== -1) {
-			coordinatesPointsOfInterestRows.push({ row: i + 1, column: idxRow + 1 });
-
-			idxRow = gridRow.indexOf(rowMaxValue, idxRow + 1);
-		}	
-	}
-
-
-	let coordinatesPointsOfInterestColumns = [];
 	
 	for (let i = 0; i < grid[0].length; i++) {
 		let columnValues = [];
@@ -27,25 +9,18 @@ export const saddlePoints = (grid) => {
 		
 		let columnMinValue = Math.min(...columnValues);
 	
-		let idxColumn = columnValues.indexOf(columnMinValue);
+		let idxRow = columnValues.indexOf(columnMinValue);
 	
-		while (idxColumn !== -1) {
-			coordinatesPointsOfInterestColumns.push({ row: idxColumn + 1, column: i + 1 });
+		while (idxRow !== -1) {
+			let rowMaxValue = Math.max(...grid[idxRow]);
 
-			idxColumn = columnValues.indexOf(columnMinValue, idxColumn + 1);
+			if (grid[idxRow][i] === rowMaxValue) {
+				saddlePoints.push({ row: idxRow + 1, column: i + 1 });
+			}
+			
+			idxRow = columnValues.indexOf(columnMinValue, idxRow + 1);
 		}
 	}
-
-
-	for (let i = 0; i < coordinatesPointsOfInterestRows.length; i++) {
-		const isMatch = coordinatesPointsOfInterestColumns.some(item => item.row === coordinatesPointsOfInterestRows[i].row && item.column === coordinatesPointsOfInterestRows[i].column);
-
-		if (isMatch){
-			saddlePoints.push(coordinatesPointsOfInterestRows[i]);
-		}
-	}
-
-	console.log(saddlePoints);
 
 	return saddlePoints;
 };
