d6e99552abd21d7f8552a17edf67526df7bfa0d2
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-06-05 16:20:38 -0600

    Refactored solution with extract method

--- javascript/saddle-points/saddle-points.js
+++ javascript/saddle-points/saddle-points.js
@@ -1,26 +1,41 @@
 export const saddlePoints = (grid) => {
-	let saddlePoints = [];
+	let points = [];
 	
 	for (let i = 0; i < grid[0].length; i++) {
-		let columnValues = [];
-		for (let j = 0; j < grid.length; j++) {
-			columnValues.push(grid[j][i]);
-		}
-		
+		let columnValues = getColumn(i, grid);
 		let columnMinValue = Math.min(...columnValues);
-	
 		let idxRow = columnValues.indexOf(columnMinValue);
 	
-		while (idxRow !== -1) {
-			let rowMaxValue = Math.max(...grid[idxRow]);
-
-			if (grid[idxRow][i] === rowMaxValue) {
-				saddlePoints.push({ row: idxRow + 1, column: i + 1 });
-			}
-			
-			idxRow = columnValues.indexOf(columnMinValue, idxRow + 1);
-		}
+		points.push(getSaddlePoints(idxRow, i, grid, columnValues, columnMinValue));
 	}
 
-	return saddlePoints;
+	return points.flat();
 };
+
+
+function getColumn(columnIndex, grid) {
+	let columnValues = [];
+
+	for (let rowIndex = 0; rowIndex < grid.length; rowIndex++) {
+		columnValues.push(grid[rowIndex][columnIndex]);
+	}
+
+	return columnValues;
+}
+
+
+function getSaddlePoints(idxRow, idxColumn, grid, columnValues, columnMinValue) {
+	let points = [];
+
+	while (idxRow !== -1) {
+		let rowMaxValue = Math.max(...grid[idxRow]);
+
+		if (grid[idxRow][idxColumn] === rowMaxValue) {
+			points.push({ row: idxRow + 1, column: idxColumn + 1 });
+		}
+
+		idxRow = columnValues.indexOf(columnMinValue, idxRow + 1);
+	}
+	
+	return points;
+}
