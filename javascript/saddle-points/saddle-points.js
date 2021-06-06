3aa534388bd34d6332cd1f9f43ba4233d8975120
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-06-06 09:40:13 -0600

    Refactor, simplified logic eliminating getColumn and getSaddlePoints function utilizing map and every array functions to simplify code.

--- javascript/saddle-points/saddle-points.js
+++ javascript/saddle-points/saddle-points.js
@@ -1,41 +1,19 @@
 export const saddlePoints = (grid) => {
 	let points = [];
-	
+
 	for (let i = 0; i < grid[0].length; i++) {
-		let columnValues = getColumn(i, grid);
+		let columnValues = grid.map(rowNumbers => rowNumbers[i]);
 		let columnMinValue = Math.min(...columnValues);
 		let idxRow = columnValues.indexOf(columnMinValue);
-	
-		points.push(getSaddlePoints(idxRow, i, grid, columnValues, columnMinValue));
-	}
 
-	return points.flat();
-};
+		while (idxRow !== -1) {
+			if (grid[idxRow].every(x => columnMinValue >= x)) {
+				points.push({ row: idxRow + 1, column: i + 1 });
+			}
 
-
-function getColumn(columnIndex, grid) {
-	let columnValues = [];
-
-	for (let rowIndex = 0; rowIndex < grid.length; rowIndex++) {
-		columnValues.push(grid[rowIndex][columnIndex]);
-	}
-
-	return columnValues;
-}
-
-
-function getSaddlePoints(idxRow, idxColumn, grid, columnValues, columnMinValue) {
-	let points = [];
-
-	while (idxRow !== -1) {
-		let rowMaxValue = Math.max(...grid[idxRow]);
-
-		if (grid[idxRow][idxColumn] === rowMaxValue) {
-			points.push({ row: idxRow + 1, column: idxColumn + 1 });
+			idxRow = columnValues.indexOf(columnMinValue, idxRow + 1);
 		}
-
-		idxRow = columnValues.indexOf(columnMinValue, idxRow + 1);
 	}
-	
+
 	return points;
-}
+};
