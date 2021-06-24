de8d8995d318e609e4cdc092acf09573216a3619
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-06-24 08:16:22 -0600

    First passing test on exercism Acronym problem

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        MatchCollection matches = Regex.Matches(phrase, @"\b[a-zA-Z]");
+
+        return string.Join("", matches.Select(m => m.Value.ToUpper()));
+    }
+}
