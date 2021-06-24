833d969087ac7222817d55373e1e520af67590c5
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-06-24 08:23:23 -0600

    Fixed indentation, and all tests passing

--- csharp/acronym/Acronym.cs
+++ csharp/acronym/Acronym.cs
@@ -7,7 +7,9 @@ public static class Acronym
 {
     public static string Abbreviate(string phrase)
     {
-        MatchCollection matches = Regex.Matches(phrase.Replace("'",string.Empty).Replace("_",string.Empty), @"\b[a-zA-Z]");
+        MatchCollection matches = Regex.Matches(phrase.Replace("'",string.Empty)
+                                                      .Replace("_",string.Empty), 
+                                                @"\b[a-zA-Z]");
 
         return string.Join(string.Empty, matches.Select(m => m.Value.ToUpper()));
     }
