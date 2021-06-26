bad42743753ac05b14e897a33578dff8e0396245
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-06-26 09:37:26 -0600

    Refactored by simplifying constructor with linq statement and initializing array of array instead of 2 dimensional array

--- csharp/matrix/Matrix.cs
+++ csharp/matrix/Matrix.cs
@@ -3,31 +3,19 @@ using System.Linq;
 
 public class Matrix
 {
-    private int[,] _matrix;
+    private int[][] _matrix;
 
-    public Matrix(string input)
-    {
-        var rowElements = input.Split("\n");
-        int columnLength = rowElements[0].Split(" ").Length;
+    public Matrix(string input) => _matrix = input.Split('\n')
+                                                  .Select(row => row.Split(' ')
+                                                                    .Select(column => int.Parse(column))
+                                                                    .ToArray())
+                                                  .ToArray();
 
-        _matrix = new int[rowElements.Length, columnLength];
+    public int Rows => _matrix.Length;
 
-        for(int i=0; i < rowElements.Length; i++)
-        {
-            var columnElements = rowElements[i].Split(" ");
+    public int Cols => _matrix.First().Length;
 
-            for(int j=0; j < columnElements.Length; j++)
-            {
-                _matrix[i,j] = int.Parse(columnElements[j]);
-            }
-        }
-    }
+    public int[] Row(int row) => Enumerable.Range(0, Cols).Select(x => _matrix[row - 1][x]).ToArray();
 
-    public int Rows => _matrix.GetLength(0);
-
-    public int Cols => _matrix.GetLength(1);
-
-    public int[] Row(int row) => Enumerable.Range(0, Cols).Select(x => _matrix[row - 1, x]).ToArray();
-
-    public int[] Column(int col) => Enumerable.Range(0, Rows).Select(x => _matrix[x, col - 1]).ToArray();
+    public int[] Column(int col) => Enumerable.Range(0, Rows).Select(x => _matrix[x][col - 1]).ToArray();
 }
