57d271355c2e0c68d9c78acbffde37d5381fbb12
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-06-30 07:49:26 -0600

    First 2 passing tests for exercism Circular Buffer problem

---
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class CircularBuffer<T>
+{
+    private T[] _bufferData;
+    private int _startIndex;
+    private int _endIndex;
+
+    public CircularBuffer(int capacity)
+    {
+        _bufferData = new T[capacity];
+    }
+
+    public T Read()
+    {
+        if (_startIndex == _endIndex) { throw new InvalidOperationException(); }
+
+        return _bufferData[_startIndex++];
+    }
+
+    public void Write(T value)
+    {
+        _bufferData[_endIndex++] = value;
+
+    }
+
+    public void Overwrite(T value)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
