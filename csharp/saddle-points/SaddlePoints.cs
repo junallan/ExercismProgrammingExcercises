d4026abf1c49f8ece271c9eb94bb481f2da6b2fc
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-07-05 08:21:57 -0600

    Changed function to expression

--- csharp/saddle-points/SaddlePoints.cs
+++ csharp/saddle-points/SaddlePoints.cs
@@ -31,17 +31,11 @@ public static class SaddlePoints
         return saddlePointCoordinates;
     }
 
-    public static int[] GetColumn(int[,] matrix, int columnNumber)
-    {
-        return Enumerable.Range(0, matrix.GetLength(0))
-                         .Select(x => matrix[x, columnNumber])
-                         .ToArray();
-    }
+    public static int[] GetColumn(int[,] matrix, int columnNumber) => Enumerable.Range(0, matrix.GetLength(0))
+                                                                                .Select(x => matrix[x, columnNumber])
+                                                                                .ToArray();
 
-    public static int[] GetRow(int[,] matrix, int rowNumber)
-    {
-        return Enumerable.Range(0, matrix.GetLength(1))
-                         .Select(x => matrix[rowNumber, x])
-                         .ToArray();
-    }
+    public static int[] GetRow(int[,] matrix, int rowNumber) => Enumerable.Range(0, matrix.GetLength(1))
+                                                                          .Select(x => matrix[rowNumber, x])
+                                                                          .ToArray();
 }
