8c3a0ad181ecfbaad61c8aaf037a3011c006eb6e
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-07-07 08:02:45 -0600

    Add to noun and verb dictionary to pass first 11 tests

--- csharp/house/HouseTests.cs
+++ csharp/house/HouseTests.cs
@@ -74,7 +74,7 @@ public class HouseTests
         Assert.Equal(expected, House.Recite(10));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Verse_11_the_farmer_sowing_his_corn()
     {
         var expected = "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.";
