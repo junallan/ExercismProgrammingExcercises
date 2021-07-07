f29d43b8fa392ef2088c5fbe2feae1b2e233cf61
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-07-07 08:05:19 -0600

    Add to noun and verb dictionary to pass first 12 tests

--- csharp/house/HouseTests.cs
+++ csharp/house/HouseTests.cs
@@ -81,7 +81,7 @@ public class HouseTests
         Assert.Equal(expected, House.Recite(11));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Verse_12_the_horse_and_the_hound_and_the_horn()
     {
         var expected = "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.";
