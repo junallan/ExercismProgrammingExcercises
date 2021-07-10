ce34daa3fe7224b686f8267ca35c4b2fd43ef5a3
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-07-10 11:43:43 -0600

    Refactored with double for loop with yield for optimal performance.

--- csharp/pythagorean-triplet/PythagoreanTriplet.cs
+++ csharp/pythagorean-triplet/PythagoreanTriplet.cs
@@ -1,15 +1,18 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 public static class PythagoreanTriplet
 {
     public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
     {
-        return  Enumerable.Range(1, (sum / 3) - 1)
-                        .Select(a => Enumerable.Range(a + 1, (sum / 2) - 1).Select(b => (a, b))
-                                               .Where(x => (x.a * x.a + x.b * x.b) == (sum-x.a-x.b) * (sum-x.a-x.b))
-                                               .Select(x => (x.a, x.b, sum - x.a - x.b)))
-                        .SelectMany(x => x);
+        for (int a = 1; a < sum / 3; a++)
+        {
+            for (int b = a + 1; b < sum / 2; b++)
+            {
+                var c = sum - a - b;
+
+                if (a * a + b * b == c * c) yield return (a, b, c);
+            }
+        }
     }
 }
