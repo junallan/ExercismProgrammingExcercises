f8eec609293ec05d7246a3a2ee6802f042d50c80
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-07-10 11:33:40 -0600

    Removed unecessary inner loop iterations

--- csharp/pythagorean-triplet/PythagoreanTriplet.cs
+++ csharp/pythagorean-triplet/PythagoreanTriplet.cs
@@ -9,7 +9,7 @@ public static class PythagoreanTriplet
         Func<int, int, int, bool> isPythagoreanTriplet = (a, b, c) => (Math.Pow(a, 2) + Math.Pow(b, 2)) == Math.Pow(c, 2);
 
         return  Enumerable.Range(1, (sum / 3) - 1)
-                        .Select(a => Enumerable.Range(a + 1, sum - a - 1).Select(b => (a, b))
+                        .Select(a => Enumerable.Range(a + 1, (sum / 2) - 1).Select(b => (a, b))
                                                .Where(x => isPythagoreanTriplet(x.a, x.b, sum - x.a - x.b))
                                                .Select(x => (x.a, x.b, sum - x.a - x.b)))
                         .SelectMany(x => x);
