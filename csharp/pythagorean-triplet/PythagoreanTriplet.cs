33ad9c99775e28ab9c8f91bb5b0eff200f2dd82e
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-07-10 11:38:54 -0600

    Eliminate func expression improving performance

--- csharp/pythagorean-triplet/PythagoreanTriplet.cs
+++ csharp/pythagorean-triplet/PythagoreanTriplet.cs
@@ -6,11 +6,9 @@ public static class PythagoreanTriplet
 {
     public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
     {
-        Func<int, int, int, bool> isPythagoreanTriplet = (a, b, c) => (Math.Pow(a, 2) + Math.Pow(b, 2)) == Math.Pow(c, 2);
-
         return  Enumerable.Range(1, (sum / 3) - 1)
                         .Select(a => Enumerable.Range(a + 1, (sum / 2) - 1).Select(b => (a, b))
-                                               .Where(x => isPythagoreanTriplet(x.a, x.b, sum - x.a - x.b))
+                                               .Where(x => (x.a * x.a + x.b * x.b) == (sum-x.a-x.b) * (sum-x.a-x.b))
                                                .Select(x => (x.a, x.b, sum - x.a - x.b)))
                         .SelectMany(x => x);
     }
