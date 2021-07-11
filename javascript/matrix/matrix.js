0e220294dcc081feeeab548b2d55b1a3357ed57f
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-07-11 12:03:36 -0600

    Refactored using function expression and eliminating uncessessary function

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -2,15 +2,22 @@ export class Matrix {
     constructor(matrix) {
         this._matrix = matrix.split("\n").map(row => row.split(" ").map(Number));
 
-        let transpose = (matrixToTranspose) => {
-            return matrixToTranspose[0].map((_, i) => {
-                return matrixToTranspose.map((row) => {
-                    return row[i];
-                });
-            });
-        };  
+        //let transpose = (matrixToCopy) => {
+        //    let copiedMatrix = [];
 
-        this._transposedMatrix = transpose(this._matrix);
+        //    for (let i = 0; i < matrixToCopy[0].length; i++) {
+        //        copiedMatrix[i] = [];
+        //        for (let j = 0; j < matrixToCopy.length; j++) {
+        //            copiedMatrix[i][j] = matrixToCopy[j][i];
+        //        }
+        //    }
+
+        //    return copiedMatrix;
+        //}
+
+        //this._transposedMatrix = transpose(this._matrix);
+   
+        this._transposedMatrix = this._matrix[0].map((_, colIndex) => this._matrix.map(row => row[colIndex]));
   }
 
   get rows() {
