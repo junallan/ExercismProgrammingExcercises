438a9441064744b39831a788d266cf7d30730248
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-07-12 08:25:43 -0600

    Refactored reducing number of times calling ToLower

--- csharp/anagram/Anagram.cs
+++ csharp/anagram/Anagram.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 
 public class Anagram
 {
-    private readonly string _baseWord;
+    private readonly string _baseWordLowercase;
     private readonly string _baseWordSortedLowercase;
 
     public Anagram(string baseWord)
     {
-        _baseWord = baseWord;
-        _baseWordSortedLowercase = SortedWord(baseWord.ToLower());
+        _baseWordLowercase = baseWord.ToLower();
+        _baseWordSortedLowercase = SortedWord(_baseWordLowercase);
     }
 
     public string[] FindAnagrams(string[] potentialMatches)
@@ -18,7 +18,7 @@ public class Anagram
 
         foreach (var potentialMatch in potentialMatches)
         {
-            if (_baseWord.ToLower() == potentialMatch.ToLower()) continue;
+            if (_baseWordLowercase == potentialMatch.ToLower()) continue;
             if (_baseWordSortedLowercase == SortedWord(potentialMatch.ToLower())) anagrams.Add(potentialMatch);
         }
 
