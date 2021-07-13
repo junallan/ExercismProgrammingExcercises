973c29d03d772f9b4ca127a619c85754351a6545
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-07-13 08:36:25 -0600

    First 2 passing tests

--- javascript/transpose/transpose.js
+++ javascript/transpose/transpose.js
@@ -1,3 +1,7 @@
 export const transpose = (matrix) => {
-	return matrix;
+	if (matrix.length === 0) return [];
+
+	let result = [...matrix[0]].map((_, index) => matrix.map(row => row[index]));
+	//console.log(result);
+	return result.flat();
 };
