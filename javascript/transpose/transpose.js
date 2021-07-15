b9e9acee0d472896c466aca010cdb8925e30d3ae
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-07-15 08:57:59 -0600

    Refactored with reduce right

--- javascript/transpose/transpose.js
+++ javascript/transpose/transpose.js
@@ -1,14 +1,9 @@
 export const transpose = (matrix) => {
-	if (matrix.length === 0) return [];
-
 	let maxRowLength = Math.max(...matrix.map(x => x.length));
-	let lastRowLength = matrix[matrix.length-1].length;
 	
-	let result = [...Array(maxRowLength).keys()]
-						.map((_, index) => matrix.map(row => index < row.length ? row[index] : ' ')
-						.join(''));
-
-	if (maxRowLength > lastRowLength) result[result.length - 1] = result[result.length - 1].trimRight();
-	
-	return result;
+	return  [...Array(maxRowLength).keys()]
+				.map((_, columnIndex) => matrix.reduceRight((accumulator, currentRow) =>
+															currentRow[columnIndex]
+																? currentRow[columnIndex] + accumulator
+																: (accumulator ? ' ' : '') + accumulator, ''));
 };
