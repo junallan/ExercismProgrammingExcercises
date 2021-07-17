237194628f230970929c2f2839bb3c58e7f14f15
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-07-17 10:58:39 -0600

    Refactored removed unecessary {} to reduce code noise

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -3,19 +3,9 @@ export const hey = (message) => {
   let isYelling = /[A-Z]+/.test(message) && /^[A-Z !?,0-9%^@#$(*]*$/.test(message);
   let isSilence = /^\s*$/.test(message);
 
-  if(isSilence){
-    return "Fine. Be that way!";
-  }
-  else if(isQuestion && isYelling){
-    return "Calm down, I know what I'm doing!";
-  }
-  else if(isQuestion) {
-    return "Sure.";
-  }
-  else if(isYelling) {
-    return "Whoa, chill out!";
-  }
-  else {
-    return "Whatever.";
-  }
+  if(isSilence)                     return "Fine. Be that way!";
+  else if(isQuestion && isYelling)  return "Calm down, I know what I'm doing!"; 
+  else if(isQuestion)               return "Sure.";
+  else if(isYelling)                return "Whoa, chill out!";
+  else                              return "Whatever.";
 };
