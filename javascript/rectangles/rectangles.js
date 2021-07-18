afed5872e24b8c7c4ad460f5a68472c585472b52
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-07-18 17:25:37 -0600

    Refactored with extracted functions

--- javascript/rectangles/rectangles.js
+++ javascript/rectangles/rectangles.js
@@ -33,43 +33,35 @@ export class Rectangles {
         return isValidSide;
     }
 
+    static findRowCornerPairs(data, rowIndex) {
+        let cornerPairs = [];
+        let columnIndexesOfCorners = [];
+
+        for (let columnIndex = 0; columnIndex < data[0].length; columnIndex++) {
+            if (data[rowIndex][columnIndex] === '+') columnIndexesOfCorners.push(columnIndex);
+        }
+
+        for (let i = 0; i < columnIndexesOfCorners.length - 1; i++) {
+            for (let j = i + 1; j < columnIndexesOfCorners.length; j++) {
+                if (this.isTopBottomComplete(columnIndexesOfCorners, i, j, data, rowIndex)) cornerPairs.push({ leftCorner: { x: rowIndex, y: columnIndexesOfCorners[i] }, rightCorner: { x: rowIndex, y: columnIndexesOfCorners[j] } });
+            }
+        }
+
+        return cornerPairs;
+    }
+
     static count(data) {
         let rectangleCoordinates = [];
         let parsedData = data.map(row => [...row]);
 
-        //row by row find those with more than 1 +, get all permutations and combinations of the column positions for pairs
-            // if found pair matches of + go through sequential rows that match column position for left + and right +
-                    //Add to count
-
-        let cornerPairs = [];
-
-        for (let rowIndex = 0; rowIndex < parsedData.length; rowIndex++) {
-            let columnIndexesOfCorners = [];
-
-            for (let columnIndex = 0; columnIndex < parsedData[0].length; columnIndex++) {
-                if (parsedData[rowIndex][columnIndex] === '+') columnIndexesOfCorners.push(columnIndex);
-            }
-
-            for (let i = 0; i < columnIndexesOfCorners.length-1; i++) {
-                for (let j = i + 1; j < columnIndexesOfCorners.length; j++) {
-                    //let isValidSide = true;
-                    
-                    //for (let sideColumnIndex = columnIndexesOfCorners[i] + 1; sideColumnIndex < columnIndexesOfCorners[j]; sideColumnIndex++) {
-                    //    if (data[rowIndex][sideColumnIndex] !== '-' && data[rowIndex][sideColumnIndex] !== '+') {
-                    //        isValidSide = false;
-                    //        break;
-                    //    }
-                    //}
-          
-                    if (this.isTopBottomComplete(columnIndexesOfCorners, i, j, parsedData, rowIndex)) cornerPairs.push({ leftCorner: { x: rowIndex, y: columnIndexesOfCorners[i] }, rightCorner: { x: rowIndex, y: columnIndexesOfCorners[j] } });
-                }
-            }
-        }
-
         let rectangleCount = 0;
 
-        cornerPairs.forEach(topCornerPair => rectangleCount += this.countBottomCornerPair(topCornerPair, parsedData));
+        for (let rowIndex = 0; rowIndex < parsedData.length; rowIndex++) {
+            let cornerPairs = this.findRowCornerPairs(parsedData, rowIndex);
 
+            cornerPairs.forEach(topCornerPair => rectangleCount += this.countBottomCornerPair(topCornerPair, parsedData));
+        }
+    
         return rectangleCount;
     }
 }
