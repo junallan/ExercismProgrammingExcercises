6ab5bc67eb8390456977f6c73c95b222a42dc4b0
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-07-18 16:32:00 -0600

    Complete solution all test passing

--- javascript/rectangles/rectangles.js
+++ javascript/rectangles/rectangles.js
@@ -5,7 +5,26 @@ export class Rectangles {
             //console.log(data[rowIndex][topCornerPair.leftCorner.y]);
             //console.log(data[rowIndex][topCornerPair.rightCorner.y]);
             if (data[rowIndex][topCornerPair.leftCorner.y] === '+' && data[rowIndex][topCornerPair.rightCorner.y] === '+') {
-                bottomCornerMatches++;
+                let isValidSide = true;
+
+                for (let sideRowIndex = topCornerPair.leftCorner.x + 1; sideRowIndex < rowIndex; sideRowIndex++) {
+                    //console.log(sideRowIndex);
+                    //console.log(topCornerPair.leftCorner.y);
+                    //console.log(topCornerPair.rightCorner.y);
+                    //if (((data[sideRowIndex][topCornerPair.leftCorner.y] !== '|') || (data[sideRowIndex][topCornerPair.rightCorner.y] !== '|')) &&
+                    //    ((data[sideRowIndex][topCornerPair.leftCorner.y] !== '+') || (data[sideRowIndex][topCornerPair.rightCorner.y] !== '+'))) {
+                    //    isValidSide = false;
+                    //    break;
+                    //}
+            
+                    if ((data[sideRowIndex][topCornerPair.leftCorner.y] !== '|' && data[sideRowIndex][topCornerPair.leftCorner.y] !== '+') ||
+                        (data[sideRowIndex][topCornerPair.rightCorner.y] !== '|' && data[sideRowIndex][topCornerPair.rightCorner.y] !== '+')) {
+                        isValidSide = false;
+                        break;
+                    }
+                }
+
+                if(isValidSide) bottomCornerMatches++;
             }
         }
 
@@ -22,6 +41,7 @@ export class Rectangles {
 
         let cornerPairs = [];
 
+        //console.log(parsedData.length);
         for (let rowIndex = 0; rowIndex < parsedData.length; rowIndex++) {
             let columnIndexesOfCorners = [];
 
@@ -29,40 +49,37 @@ export class Rectangles {
                 if (parsedData[rowIndex][columnIndex] === '+') columnIndexesOfCorners.push(columnIndex);
             }
 
-            console.log(columnIndexesOfCorners);
+            //console.log(columnIndexesOfCorners);
             for (let i = 0; i < columnIndexesOfCorners.length-1; i++) {
-                for (let j = i+1; j < columnIndexesOfCorners.length; j++) {
-                    cornerPairs.push({ leftCorner: { x: rowIndex, y: i }, rightCorner: { x: rowIndex, y: columnIndexesOfCorners[j] } });
+                for (let j = i + 1; j < columnIndexesOfCorners.length; j++) {
+                    let isValidSide = true;
+
+                    //console.log(`columnIndexesOfCorners[i] + 1: ${columnIndexesOfCorners[i] + 1}`);
+                    //console.log(`columnIndexesOfCorners[j]: ${columnIndexesOfCorners[j]}`);
+                    
+                    for (let sideColumnIndex = columnIndexesOfCorners[i] + 1; sideColumnIndex < columnIndexesOfCorners[j]; sideColumnIndex++) {
+                        //console.log(rowIndex);
+                        //console.log(rowIndex);
+                        //console.log(sideColumnIndex);
+                        if (data[rowIndex][sideColumnIndex] !== '-' && data[rowIndex][sideColumnIndex] !== '+') {
+                            isValidSide = false;
+                            break;
+                        }
+                    }
+                    //console.log(`isValid: ${isValidSide}`);
+                    //console.log(`rowIndex:${rowIndex}`);
+                    //console.log(`columnIndexesOfCorners[i]:${columnIndexesOfCorners[i]}`);
+                    //console.log(`columnIndexesOfCorners[j]:${columnIndexesOfCorners[j]}`);
+                    if(isValidSide) cornerPairs.push({ leftCorner: { x: rowIndex, y: columnIndexesOfCorners[i] }, rightCorner: { x: rowIndex, y: columnIndexesOfCorners[j] } });
                 }
             }
         }
 
         let rectangleCount = 0;
-
+        //console.log(cornerPairs);
         cornerPairs.forEach(topCornerPair => rectangleCount += this.countBottomCornerPair(topCornerPair, parsedData));
 
-        //cornerPairs.forEach(coordinate =>
-        //    rectangleCount += cornerPairs.filter(element => element.leftCorner.y === coordinate.leftCorner.y
-        //                                                                                    && element.rightCorner.y === coordinate.rightCorner.y
-        //                                                                                    && element.leftCorner.x !== coordinate.leftCorner.x
-        //        && element.rightCorner.x !== coordinate.rightCorner.x).length
-        //);
-
-        console.log(cornerPairs);
-
-        //for (let rowIndex = 0; rowIndex < parsedData.length; rowIndex++) {
-        //    for (let columnIndex = 0; columnIndex < parsedData[0].length; columnIndex++) {
-        //        if (Rectangles.isBaseRectangle(rowIndex, columnIndex, parsedData) ||
-        //            Rectangles.isRectangleOfHeightOne(rowIndex, columnIndex, parsedData) || 
-        //            Rectangles.isRectangleOfWidthOne(rowIndex, columnIndex, parsedData) ||
-        //            Rectangles.isSquareOneByOne(rowIndex,columnIndex, parsedData)) {
-
-        //            rectangleCoordinates.push({x: rowIndex, y: columnIndex });
-        //        }
-        //    }
-        //}
-
-        //let rectangleCount = rectangleCoordinates.length;
+        //console.log(cornerPairs);
 
         return rectangleCount;
     }
--- javascript/rectangles/rectangles.spec.js
+++ javascript/rectangles/rectangles.spec.js
@@ -29,80 +29,80 @@ describe('Rectangles', () => {
     expect(actual).toEqual(expected);
   });
 
-  //test('two rectangles without shared parts', () => {
-  //  const expected = 2;
-  //  const actual = Rectangles.count([
-  //    '  +-+',
-  //    '  | |',
-  //    '+-+-+',
-  //    '| |  ',
-  //    '+-+  ',
-  //  ]);
+  test('two rectangles without shared parts', () => {
+    const expected = 2;
+    const actual = Rectangles.count([
+      '  +-+',
+      '  | |',
+      '+-+-+',
+      '| |  ',
+      '+-+  ',
+    ]);
 
-  //  expect(actual).toEqual(expected);
-  //});
+    expect(actual).toEqual(expected);
+  });
 
-  //test('five rectangles with shared parts', () => {
-  //  const expected = 5;
-  //  const actual = Rectangles.count([
-  //    '  +-+',
-  //    '  | |',
-  //    '+-+-+',
-  //    '| | |',
-  //    '+-+-+',
-  //  ]);
+  test('five rectangles with shared parts', () => {
+    const expected = 5;
+    const actual = Rectangles.count([
+      '  +-+',
+      '  | |',
+      '+-+-+',
+      '| | |',
+      '+-+-+',
+    ]);
 
-  //  expect(actual).toEqual(expected);
-  //});
+    expect(actual).toEqual(expected);
+  });
 
-  //test('rectangle of height 1 is counted', () => {
-  //  const expected = 1;
-  //  const actual = Rectangles.count(['+--+', '+--+']);
+  test('rectangle of height 1 is counted', () => {
+    const expected = 1;
+    const actual = Rectangles.count(['+--+', '+--+']);
 
-  //  expect(actual).toEqual(expected);
-  //});
+    expect(actual).toEqual(expected);
+  });
 
-  //test('rectangle of width 1 is counted', () => {
-  //  const expected = 1;
-  //  const actual = Rectangles.count(['++', '||', '++']);
+  test('rectangle of width 1 is counted', () => {
+    const expected = 1;
+    const actual = Rectangles.count(['++', '||', '++']);
 
-  //  expect(actual).toEqual(expected);
-  //});
+    expect(actual).toEqual(expected);
+  });
 
-  //test('1x1 square is counted', () => {
-  //  const expected = 1;
-  //  const actual = Rectangles.count(['++', '++']);
+  test('1x1 square is counted', () => {
+    const expected = 1;
+    const actual = Rectangles.count(['++', '++']);
 
-  //  expect(actual).toEqual(expected);
-  //});
+    expect(actual).toEqual(expected);
+  });
 
-  //test('only complete rectangles are counted', () => {
-  //  const expected = 1;
-  //  const actual = Rectangles.count([
-  //    '  +-+',
-  //    '    |',
-  //    '+-+-+',
-  //    '| | -',
-  //    '+-+-+',
-  //  ]);
+  test('only complete rectangles are counted', () => {
+    const expected = 1;
+    const actual = Rectangles.count([
+      '  +-+',
+      '    |',
+      '+-+-+',
+      '| | -',
+      '+-+-+',
+    ]);
 
-  //  expect(actual).toEqual(expected);
-  //});
+    expect(actual).toEqual(expected);
+  });
 
-  //test('rectangles can be of different sizes', () => {
-  //  const expected = 3;
-  //  const actual = Rectangles.count([
-  //    '+------+----+',
-  //    '|      |    |',
-  //    '+---+--+    |',
-  //    '|   |       |',
-  //    '+---+-------+',
-  //  ]);
+  test('rectangles can be of different sizes', () => {
+    const expected = 3;
+    const actual = Rectangles.count([
+      '+------+----+',
+      '|      |    |',
+      '+---+--+    |',
+      '|   |       |',
+      '+---+-------+',
+    ]);
 
-  //  expect(actual).toEqual(expected);
-  //});
+    expect(actual).toEqual(expected);
+  });
 
-  xtest('corner is required for a rectangle to be complete', () => {
+  test('corner is required for a rectangle to be complete', () => {
     const expected = 2;
     const actual = Rectangles.count([
       '+------+----+',
@@ -115,7 +115,7 @@ describe('Rectangles', () => {
     expect(actual).toEqual(expected);
   });
 
-  xtest('large input with many rectangles', () => {
+  test('large input with many rectangles', () => {
     const expected = 60;
     const actual = Rectangles.count([
       '+---+--+----+',
