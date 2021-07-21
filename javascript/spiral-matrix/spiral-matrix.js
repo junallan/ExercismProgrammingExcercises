dd271fea3e1903872b72b3ab0694f86dcce696fb
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-07-21 07:58:48 -0600

    Refactored using simplified algorithm to solve spiral matrix problem utilizing core populating edges of matrix algorithm.  First 2 tests passing.

--- javascript/spiral-matrix/spiral-matrix.js
+++ javascript/spiral-matrix/spiral-matrix.js
@@ -1,14 +1,38 @@
-export class SpiralMatrix {
-  static ofSize(matrixSize) {
-      let spiralMatrix = [];
+import { endianness } from "os";
 
-      for (let i = 0; i < matrixSize; i++) {
-          let row = []
-          for (let j = 0; j < matrixSize; j++) {
-              row.push(j+1);
-          }
-          spiralMatrix.push(row);
-      }
+export class SpiralMatrix {
+    static populateEdgesOfMatrix(matrix, startIndex, endIndex, value) {
+        if (startIndex === endIndex) {
+            matrix[startIndex][endIndex] = value;
+
+            return;
+        }
+
+        for (let i = startIndex; i <= endIndex; i++) {
+            matrix[startIndex][i] = value++;
+        }
+
+        for (let i = startIndex+1; i <= endIndex; i++) {
+            matrix[i][endIndex] = value++;
+        }
+
+        for (let i = endIndex - 1; i >= 0; i--) {
+            matrix[endIndex][i] = value++;
+        }
+
+        for (let i = endIndex - 1; i >= 0; i--) {
+            matrix[i][startIndex] = value++;
+        }
+
+        this.populateEdgesOfMatrix(matrix, startIndex + 1, endIndex - 1, value);
+    }
+
+    static ofSize(matrixSize) {
+      if (matrixSize === 0) return [];
+
+      let spiralMatrix = [...Array(matrixSize)].map(x => Array(matrixSize));
+
+      this.populateEdgesOfMatrix(spiralMatrix, 0, matrixSize-1, 1);
 
       return spiralMatrix;
   }
