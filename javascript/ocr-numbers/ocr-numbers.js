2b88a001feef4612a3cd76ff435cc505c7ba3585
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-07-23 08:26:22 -0600

    First passing test for exercism ocr numbers problem

---
+++ javascript/ocr-numbers/ocr-numbers.js
@@ -0,0 +1,5 @@
+export const convert = (input) => {
+	const parsedInput = input.split('\n');
+
+	if (parsedInput[0] === ' _ ' && parsedInput[1] === '| |' && parsedInput[2] === '|_|') return '0';
+};
