1a005b1faa9c1c5343f963d73c0d009dc4a18767
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-07-24 10:55:36 -0600

    'converts 123 456 789' test passing

--- javascript/ocr-numbers/ocr-numbers.js
+++ javascript/ocr-numbers/ocr-numbers.js
@@ -3,22 +3,40 @@ export const convert = (input) => {
 
 	const parsedInput = input.split('\n');
 
-	const firstLineNumberToProcess = parsedInput[0].substr(0, 3);
-	const secondLineNumberToProcess = parsedInput[1].substr(0, 3);
-	const thirdLineNumberToProcess = parsedInput[2].substr(0, 3);
+	const numbersToProcess = parsedInput[0].length / 3;
+	let convertedNumbers = '';
 
-	const nextNumberDataToProcess = parsedInput[0].length > 3 ? parsedInput[0].substr(3) + '\n' + parsedInput[1].substr(3) + '\n' + parsedInput[2].substr(3) + '\n' + '   ' : '';
+	for (let i = 0; i < numbersToProcess; i++) {
+		const startIndex = i * 3;
+		const firstLineNumberToProcess = parsedInput[0].substr(startIndex, 3);
+		const secondLineNumberToProcess = parsedInput[1].substr(startIndex, 3);
+		const thirdLineNumberToProcess = parsedInput[2].substr(startIndex, 3);
 
-	if (firstLineNumberToProcess === ' _ ' && secondLineNumberToProcess === '| |' && thirdLineNumberToProcess === '|_|') return '0' + convert(nextNumberDataToProcess);
-	if (firstLineNumberToProcess === '   ' && secondLineNumberToProcess === '  |' && thirdLineNumberToProcess === '  |') return '1' + convert(nextNumberDataToProcess);
-	if (firstLineNumberToProcess === ' _ ' && secondLineNumberToProcess === ' _|' && thirdLineNumberToProcess === '|_ ') return '2' + convert(nextNumberDataToProcess);;
-	if (firstLineNumberToProcess === ' _ ' && secondLineNumberToProcess === ' _|' && thirdLineNumberToProcess === ' _|') return '3' + convert(nextNumberDataToProcess);;
-	if (firstLineNumberToProcess === '   ' && secondLineNumberToProcess === '|_|' && thirdLineNumberToProcess === '  |') return '4' + convert(nextNumberDataToProcess);;
-	if (firstLineNumberToProcess === ' _ ' && secondLineNumberToProcess === '|_ ' && thirdLineNumberToProcess === ' _|') return '5' + convert(nextNumberDataToProcess);;
-	if (firstLineNumberToProcess === ' _ ' && secondLineNumberToProcess === '|_ ' && thirdLineNumberToProcess === '|_|') return '6' + convert(nextNumberDataToProcess);; 
-	if (firstLineNumberToProcess === ' _ ' && secondLineNumberToProcess === '  |' && thirdLineNumberToProcess === '  |') return '7' + convert(nextNumberDataToProcess);; 	 
-	if (firstLineNumberToProcess === ' _ ' && secondLineNumberToProcess === '|_|' && thirdLineNumberToProcess === '|_|') return '8' + convert(nextNumberDataToProcess);;
-	if (firstLineNumberToProcess === ' _ ' && secondLineNumberToProcess === '|_|' && thirdLineNumberToProcess === ' _|') return '9' + convert(nextNumberDataToProcess);;	 
+		if (firstLineNumberToProcess === ' _ ' && secondLineNumberToProcess === '| |' && thirdLineNumberToProcess === '|_|') convertedNumbers += '0';
+		else if (firstLineNumberToProcess === '   ' && secondLineNumberToProcess === '  |' && thirdLineNumberToProcess === '  |') convertedNumbers += '1';
+		else if (firstLineNumberToProcess === ' _ ' && secondLineNumberToProcess === ' _|' && thirdLineNumberToProcess === '|_ ') convertedNumbers += '2';
+		else if (firstLineNumberToProcess === ' _ ' && secondLineNumberToProcess === ' _|' && thirdLineNumberToProcess === ' _|') convertedNumbers += '3';
+		else if (firstLineNumberToProcess === '   ' && secondLineNumberToProcess === '|_|' && thirdLineNumberToProcess === '  |') convertedNumbers += '4';
+		else if (firstLineNumberToProcess === ' _ ' && secondLineNumberToProcess === '|_ ' && thirdLineNumberToProcess === ' _|') convertedNumbers += '5';
+		else if (firstLineNumberToProcess === ' _ ' && secondLineNumberToProcess === '|_ ' && thirdLineNumberToProcess === '|_|') convertedNumbers += '6';
+		else if (firstLineNumberToProcess === ' _ ' && secondLineNumberToProcess === '  |' && thirdLineNumberToProcess === '  |') convertedNumbers += '7';
+		else if (firstLineNumberToProcess === ' _ ' && secondLineNumberToProcess === '|_|' && thirdLineNumberToProcess === '|_|') convertedNumbers += '8';
+		else if (firstLineNumberToProcess === ' _ ' && secondLineNumberToProcess === '|_|' && thirdLineNumberToProcess === ' _|') convertedNumbers += '9';
+		else convertedNumbers += '?';	
+	}
 
-	return '?' + convert(nextNumberDataToProcess);
-};
+	const nextGroupOfNumbers = parsedInput.length / 4;
+
+	if (nextGroupOfNumbers > 1) convertedNumbers += ',';
+
+
+	let nextNumberDataToProcess = '';
+
+	for (let i = 1; i < nextGroupOfNumbers; i++) {
+		const nextNumberSetStart = i * 4;
+		nextNumberDataToProcess += parsedInput[nextNumberSetStart] + '\n' + parsedInput[nextNumberSetStart + 1] + '\n' + parsedInput[nextNumberSetStart + 2] + '\n' + parsedInput[nextNumberSetStart + 3] + '\n';
+	}
+	if (nextNumberDataToProcess.endsWith('\n')) nextNumberDataToProcess = nextNumberDataToProcess.slice(0,-1);
+
+	return convertedNumbers + convert(nextNumberDataToProcess);
+}; 
