b0b9ab5dec5f2fa2f56e07454062c0884ac36037
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-09-08 08:16:21 -0600

    Extracted lookup left side function

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -1,26 +1,5 @@
 export const rows = (numRows) => {
-	let triangle = [];
-
-	for (let i = 1; i <= numRows; i++) {
-		let lineValues = [];
-
-		for (let j = 1; j <= i; j++) {
-			let leftSideSum = 0;
-			let rightSideSum = 0;
-
-			if (isInitialPosition(i,j)) lineValues.push(1);
-			else {			
-				if (isLeftSideSum(j))		leftSideSum  = triangle[i - 2][j - 2];		
-				if (isRightSideSum(j,i))	rightSideSum = triangle[i - 2][j - 1];				
-
-				lineValues.push(leftSideSum + rightSideSum);
-			}	
-		}
-
-		triangle.push(lineValues)
-	}
-
-	function isInitialPosition(row, column){
+	function isInitialPosition(row, column) {
 		return row === 1 && column === 1;
 	}
 
@@ -32,7 +11,30 @@ export const rows = (numRows) => {
 		return column < columnEdge;
 	}
 
+	function lookupLeftSide(row, column) {
+		return triangle[row - 2][column - 2];
+	}
 
+	let triangle = [];
+
+	for (let i = 1; i <= numRows; i++) {
+		let lineValues = [];
+
+		for (let j = 1; j <= i; j++) {
+			let leftSideSum = 0;
+			let rightSideSum = 0;
+
+			if (isInitialPosition(i,j)) lineValues.push(1);
+			else {			
+				if (isLeftSideSum(j))		leftSideSum = lookupLeftSide(i ,j);
+				if (isRightSideSum(j,i))	rightSideSum = triangle[i - 2][j - 1];				
+
+				lineValues.push(leftSideSum + rightSideSum);
+			}	
+		}
+
+		triangle.push(lineValues)
+	}
 
 	return triangle;
 };
