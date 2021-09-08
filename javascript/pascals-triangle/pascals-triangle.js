a34a6f80a391b185ccbf61805381f752efafdefe
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-09-08 08:13:42 -0600

    Extracted left and right side sum check functions

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -10,8 +10,8 @@ export const rows = (numRows) => {
 
 			if (isInitialPosition(i,j)) lineValues.push(1);
 			else {			
-				if ((j - 1) > 0)	leftSideSum  = triangle[i - 2][j - 2];		
-				if (j < i)			rightSideSum = triangle[i - 2][j - 1];				
+				if (isLeftSideSum(j))		leftSideSum  = triangle[i - 2][j - 2];		
+				if (isRightSideSum(j,i))	rightSideSum = triangle[i - 2][j - 1];				
 
 				lineValues.push(leftSideSum + rightSideSum);
 			}	
@@ -23,6 +23,16 @@ export const rows = (numRows) => {
 	function isInitialPosition(row, column){
 		return row === 1 && column === 1;
 	}
+
+	function isLeftSideSum(column) {
+		return (column - 1) > 0;
+	}
+
+	function isRightSideSum(column, columnEdge) {
+		return column < columnEdge;
+	}
+
+
 
 	return triangle;
 };
