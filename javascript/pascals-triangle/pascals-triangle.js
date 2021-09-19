c062e25e21e259e21397015448f8ae831e968109
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-09-19 08:56:58 -0600

    Revised with destructuring pair edge elements

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -3,29 +3,21 @@ export const rows = (numRows) => {
 		let triangle = [];
 		
 		for (let i = 0; i < numRows; i++) {
+			//console.log('nextRow:' + nextRow(triangle.length === 0 ? [] : triangle[triangle.length - 1]));
 			triangle.push(nextRow(triangle.length === 0 ? [] : triangle[triangle.length - 1]));
 		}
 
+		/*console.log('triangle:' + triangle);*/
 		return triangle;
 	}
 
 	function pairs(priorRow) {
-		let rowPairs = [];
-
-		rowPairs.push([0, 1]);
-
-		for (let i = 1; i < priorRow.length; i++) {
-			rowPairs.push([priorRow[i - 1], priorRow[i]]);
-		}
-
-		rowPairs.push([1, 0]);
-
-		return rowPairs;
+		return [[0, 1], ...Array.from(Array(priorRow.length-1).keys()).map(i => [priorRow[i], priorRow[i + 1]]), [1, 0]];
 	}
-
+	
 	function nextRow(priorRow) {
 		if (priorRow.length === 0) return [1];
-
+		
 		return pairs(priorRow).map(([a, b]) => a + b);
 	}
 
