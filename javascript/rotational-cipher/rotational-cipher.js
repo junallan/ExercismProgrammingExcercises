b8948bacfa02e2c507d16baecbb21f3e7603fbc7
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-09-25 10:41:09 -0600

    All tests passing

--- javascript/rotational-cipher/rotational-cipher.js
+++ javascript/rotational-cipher/rotational-cipher.js
@@ -1,22 +1,16 @@
-const aCharCode = 97;
-const zCharCode = 122;
-const ACharCode = 65;
-const ZCharCode = 90;
-
-//console.log('a'.charCodeAt(0)); -> 97
-//console.log('z'.charCodeAt(0)); -> 122
-//console.log('A'.charCodeAt(0)); -> 65
-//console.log('Z'.charCodeAt(0)); -> 90
-
-//String.fromCharCode(97, 122, 65, 90) -> azAZ
-
 export class RotationalCipher {
     static rotate(data, key) {
         return [...data].map(d => this.doRotation(d, key)).join('');
     }
 
     static doRotation(data, key) {
+        const aCharCode = 97;
+        const zCharCode = 122;
+        const ACharCode = 65;
+        const ZCharCode = 90;
+
         const elementCode = data.charCodeAt(0);
+
         if (this.isCharacterCodeRange(elementCode, aCharCode, zCharCode)) {
             return this.getLetterRotation(elementCode, key, aCharCode, zCharCode);
         }
@@ -32,14 +26,6 @@ export class RotationalCipher {
     static isCharacterCodeRange(elementCode, lowerBoundCharacterCode, upperBoundCharacterCode) {
         return lowerBoundCharacterCode <= elementCode && elementCode <= upperBoundCharacterCode;
     }
-
-    //static isLowerCaseLetter(elementCode) {   
-    //    return aCharCode <= elementCode && elementCode <= zCharCode;
-    //}
-
-    //static isUpperCaseLetter(elementCode) {
-    //    return ACharCode <= elementCode && elementCode <= ZCharCode;
-    //}
 
     static getLetterRotation(elementCode, key, lowerBoundCharacter, upperBoundCharacter) {
         let letterRotationCode = elementCode + key;
