8bf94d172e5903c449a481c8439cb313c969d04c
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-09-25 10:52:50 -0600

    Refactored to call fromCharCode once

--- javascript/rotational-cipher/rotational-cipher.js
+++ javascript/rotational-cipher/rotational-cipher.js
@@ -1,6 +1,6 @@
 export class RotationalCipher {
     static rotate(data, key) {
-        return [...data].map(d => this.doRotation(d, key)).join('');
+        return String.fromCharCode(...[...data].map(d => this.doRotation(d, key)));
     }
 
     static doRotation(data, key) {
@@ -12,13 +12,13 @@ export class RotationalCipher {
         const elementCode = data.charCodeAt(0);
 
         if (this.isCharacterCodeRange(elementCode, aCharCode, zCharCode)) {
-            return this.getLetterRotation(elementCode, key, aCharCode, zCharCode);
+            return this.getLetterRotationCode(elementCode, key, aCharCode, zCharCode);
         }
         else if (this.isCharacterCodeRange(elementCode, ACharCode, ZCharCode)) {
-            return this.getLetterRotation(elementCode, key, ACharCode, ZCharCode);
+            return this.getLetterRotationCode(elementCode, key, ACharCode, ZCharCode);
         }
         else {
-            return data;
+            return elementCode;
         }
         
     }
@@ -27,10 +27,10 @@ export class RotationalCipher {
         return lowerBoundCharacterCode <= elementCode && elementCode <= upperBoundCharacterCode;
     }
 
-    static getLetterRotation(elementCode, key, lowerBoundCharacter, upperBoundCharacter) {
+    static getLetterRotationCode(elementCode, key, lowerBoundCharacter, upperBoundCharacter) {
         let letterRotationCode = elementCode + key;
 
-        return String.fromCharCode(((elementCode - lowerBoundCharacter) + key) <= (upperBoundCharacter - lowerBoundCharacter) ? letterRotationCode : (letterRotationCode % upperBoundCharacter) + (lowerBoundCharacter - 1));
+        return ((elementCode - lowerBoundCharacter) + key) <= (upperBoundCharacter - lowerBoundCharacter) ? letterRotationCode : (letterRotationCode % upperBoundCharacter) + (lowerBoundCharacter - 1);
     }
 }
 
