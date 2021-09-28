c7da3ba28c6efb86d338bb320f8a68d2020394b9
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-09-28 08:41:15 -0600

    Solution to exercism Lasagna exercise

---
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -0,0 +1,11 @@
+public class Lasagna
+{
+    public readonly int OvenTime = 40;
+    public readonly int LayerPrepTime = 2;
+    public int ExpectedMinutesInOven() => OvenTime;
+    public int RemainingMinutesInOven(int cookTime) => OvenTime - cookTime;
+
+    public int PreparationTimeInMinutes(int layers) => LayerPrepTime * layers;
+
+    public int ElapsedTimeInMinutes(int layers, int cookTime) => PreparationTimeInMinutes(layers) * cookTime;
+}
