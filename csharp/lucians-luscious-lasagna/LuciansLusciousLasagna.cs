e4cb1947d813dd647c9ffa03ba19d35e91d8b1ca
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-09-28 08:44:36 -0600

    Cleanup formatting

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -2,10 +2,9 @@ public class Lasagna
 {
     public readonly int OvenTime = 40;
     public readonly int LayerPrepTime = 2;
+
     public int ExpectedMinutesInOven() => OvenTime;
     public int RemainingMinutesInOven(int cookTime) => OvenTime - cookTime;
-
     public int PreparationTimeInMinutes(int layers) => LayerPrepTime * layers;
-
     public int ElapsedTimeInMinutes(int layers, int cookTime) => PreparationTimeInMinutes(layers) * cookTime;
 }
