f9e24f14f9025575ff369965cd553942dae2c4e5
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-09-29 08:33:20 -0600

    Variable and function renaming

--- javascript/rotational-cipher/rotational-cipher.js
+++ javascript/rotational-cipher/rotational-cipher.js
@@ -1,32 +1,28 @@
 export class RotationalCipher {
-    static rotate(data, key) {
-        return [...data].map(d => this.doRotation(d, key)).join('');
+    static rotate(data, shiftPosition) {
+        return [...data].map(d => this.doShift(d, shiftPosition)).join('');
     }
 
-    static doRotation(data, key) {
+    static doShift(data, shiftPosition) {
         if (!this.isCharacterALetter(data)) return data;
 
-        const lowerCaseLetterCode = data.toLowerCase().charCodeAt(0);
-        
-        const letterRotationCode = this.getLetterRotationCode(lowerCaseLetterCode, key);
-           
-        const isCharacterLowercase = data.toLowerCase() === data;
+        const letterRotationCode = this.getLetterRotationCode(data.toLowerCase().charCodeAt(0), shiftPosition);      
         const lowerCaseLetter = String.fromCharCode(letterRotationCode);
 
-        return isCharacterLowercase ? lowerCaseLetter : lowerCaseLetter.toUpperCase();
+        return data.toLowerCase() === data ? lowerCaseLetter : lowerCaseLetter.toUpperCase();
     }
 
     static isCharacterALetter(char) {
         return char.toLowerCase() !== char.toUpperCase()
     }
 
-    static getLetterRotationCode(elementCode, key) {
+    static getLetterRotationCode(elementCode, shiftPosition) {
         const zCharCode = 122;
         const alphabetBoundary = 25;
-        const letterRotationCode = elementCode + key;
         const aCharCode = zCharCode - alphabetBoundary;
+        const letterRotationCode = elementCode + shiftPosition;
 
-        return ((elementCode - aCharCode) + key) <= (zCharCode - aCharCode) ? letterRotationCode : (letterRotationCode % zCharCode) + (aCharCode - 1);
+        return ((elementCode - aCharCode) + shiftPosition) <= (zCharCode - aCharCode) ? letterRotationCode : (letterRotationCode % zCharCode) + (aCharCode - 1);
     }
 }
 
