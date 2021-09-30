cce72f870692358da8749ed64ef3dc7b0680951a
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-09-30 08:17:25 -0600

    Revised error in logic for ElapsedTimeInMinutes

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -6,5 +6,5 @@ public class Lasagna
     public int ExpectedMinutesInOven() => OvenTime;
     public int RemainingMinutesInOven(int cookTime) => OvenTime - cookTime;
     public int PreparationTimeInMinutes(int layers) => LayerPrepTime * layers;
-    public int ElapsedTimeInMinutes(int layers, int cookTime) => PreparationTimeInMinutes(layers) * cookTime;
+    public int ElapsedTimeInMinutes(int layers, int cookTime) => PreparationTimeInMinutes(layers) + cookTime;
 }
