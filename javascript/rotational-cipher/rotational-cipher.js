a16e3705ca83f9de2e6b722d13453eccd69b79d9
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-09-30 08:16:45 -0600

    Revised algorithm for to simplify letter rotation logic

--- javascript/rotational-cipher/rotational-cipher.js
+++ javascript/rotational-cipher/rotational-cipher.js
@@ -1,8 +1,8 @@
 export class RotationalCipher {
     static rotate(data, shiftPosition) {
         let dataTransformation = '';
-        for (let i = 0; i < data.length; i++) {
 
+        for (let i = 0; i < data.length; i++) {
            dataTransformation += this.doShift(data.charAt(i), shiftPosition);
         }
 
@@ -12,23 +12,21 @@ export class RotationalCipher {
     static doShift(dataElement, shiftPosition) {
         if (!this.isCharacterALetter(dataElement)) return dataElement;
 
-        const letterRotationCode = this.getLetterRotationCode(dataElement.toLowerCase().charCodeAt(0), shiftPosition);      
-        const lowerCaseLetter = String.fromCharCode(letterRotationCode);
-
+        const lowerCaseLetter = this.getLetterRotation(dataElement.toLowerCase(), shiftPosition);      
+    
         return dataElement.toLowerCase() === dataElement ? lowerCaseLetter : lowerCaseLetter.toUpperCase();
     }
 
     static isCharacterALetter(char) {
         return char.toLowerCase() !== char.toUpperCase()
     }
-
-    static getLetterRotationCode(elementCode, shiftPosition) {
-        const zCharCode = 122;
-        const alphabetBoundary = 25;
-        const aCharCode = zCharCode - alphabetBoundary;
-        const letterRotationCode = elementCode + shiftPosition;
-
-        return ((elementCode - aCharCode) + shiftPosition) <= (zCharCode - aCharCode) ? letterRotationCode : (letterRotationCode % zCharCode) + (aCharCode - 1);
+    
+    static getLetterRotation(dataElement, shiftPosition) {
+        const alphabet = "abcdefghijklmnopqrstuvwxyz";
+   
+        const elementPosition = (alphabet.indexOf(dataElement) + shiftPosition) % alphabet.length
+      
+        return alphabet.charAt(elementPosition);
     }
 }
 
