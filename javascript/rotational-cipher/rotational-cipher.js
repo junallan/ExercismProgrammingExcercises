3e7340b830a6464bfc68154a9660161b0ca3ad6b
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-02 16:08:59 -0600

    Instantiated object with constructor and member variables

--- javascript/rotational-cipher/rotational-cipher.js
+++ javascript/rotational-cipher/rotational-cipher.js
@@ -1,26 +1,34 @@
 export class RotationalCipher {
     static rotate(data, shiftPosition) {
-        return data.replace(/[a-z]/gi, (characterMatch) =>  this.doShift(characterMatch, shiftPosition) );
+        return new RotationalCipher(data).rotate(shiftPosition);
     }
 
-    static doShift(character, shiftPosition) {
-        const lowerCaseLetter = this.getLetterRotation(character.toLowerCase(), shiftPosition);      
+    constructor(data) {
+        this.data = data;
+        this.ACharacterCode = 97;
+        this.Alphabet = "abcdefghijklmnopqrstuvwxyz";
+    }
+
+    rotate(shiftPosition) {
+        return this.data.replace(/[a-z]/gi, (characterMatch) => this.doShift(characterMatch, shiftPosition));
+    }
+
+    doShift(character, shiftPosition) {
+        const lowerCaseLetter = this.getLetterRotation(character.toLowerCase(), shiftPosition);
         const isCharacterLowercase = character.toLowerCase() === character;
 
         return isCharacterLowercase ? lowerCaseLetter : lowerCaseLetter.toUpperCase();
     }
 
-    static indexOf(character) {
-        const aCharacterCode = 97;
-        return character.charCodeAt(0) - aCharacterCode;
+    indexOf(character) {
+        return character.charCodeAt(0) - this.ACharacterCode;
     }
-    
-    static getLetterRotation(character, shiftPosition) {
-        const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    getLetterRotation(character, shiftPosition) {
+        const elementPosition = (this.indexOf(character) + shiftPosition) % this.Alphabet.length
+
+        return this.Alphabet.charAt(elementPosition);
+    }
    
-        const elementPosition = (this.indexOf(character) + shiftPosition) % alphabet.length
-      
-        return alphabet.charAt(elementPosition);
-    }
 }
 
