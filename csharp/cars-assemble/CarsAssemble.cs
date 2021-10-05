01e1066a5d428c66235a2d2bd656a67dcfe6a0f5
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-04 21:20:51 -0600

    Extract success rate constants and created RawProductionRate helper function.

--- csharp/cars-assemble/CarsAssemble.cs
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -5,13 +5,19 @@ static class AssemblyLine
     private const int CarsProductionForSpeedMeasurement = 221;
     private const int MinutesPerHour = 60;
 
+    private const double FiveToEightCarSuccessRate = 0.9;
+    private const double NineCarSuccessRate = 0.8;
+    private const double TenCarSuccessRate = 0.77;
+
+    private static int RawProductionRate(int speed) => CarsProductionForSpeedMeasurement * speed;
+
     public static double ProductionRatePerHour(int speed)
     {
         if (speed == 0) return 0;
-        else if (speed < 5) return CarsProductionForSpeedMeasurement * speed;
-        else if(speed < 9) return (CarsProductionForSpeedMeasurement * speed * 0.9);
-        else if(speed < 10) return (CarsProductionForSpeedMeasurement * speed * 0.8);
-        else return (CarsProductionForSpeedMeasurement * speed * 0.77);
+        else if (speed < 5) return RawProductionRate(speed);
+        else if(speed < 9) return RawProductionRate(speed) * FiveToEightCarSuccessRate;
+        else if(speed < 10) return RawProductionRate(speed) * NineCarSuccessRate;
+        else return RawProductionRate(speed) * TenCarSuccessRate;
     }
 
     public static int WorkingItemsPerMinute(int speed) => (int)ProductionRatePerHour(speed) / MinutesPerHour;
