40f67780756e1650943bc9196bb2e258ade7c73c
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-07 07:42:23 -0600

    Refactored to extract Interest Rate Percentage helper method and formatting of Interest Rate switch statement

--- csharp/interest-is-interesting/InterestIsInteresting.cs
+++ csharp/interest-is-interesting/InterestIsInteresting.cs
@@ -2,14 +2,18 @@ using System;
 
 static class SavingsAccount
 {
-    public static float InterestRate(decimal balance) => balance switch { < 0 => -3.213f, < 1000 => 0.5f, < 5000 => 1.621f, _ => 2.475f };
+    private static decimal InterestRatePercentage(decimal balance) => (decimal)(InterestRate(balance) / 100);
 
-    public static decimal Interest(decimal balance)
-    {
-        decimal interestRatePercentage = (decimal)(InterestRate(balance) / 100);
-        
-        return interestRatePercentage * Math.Abs(balance);
-    }
+    public static float InterestRate(decimal balance) 
+                            => balance switch 
+                            { 
+                                < 0     => -3.213f, 
+                                < 1000  => 0.5f, 
+                                < 5000  => 1.621f, 
+                                _       => 2.475f 
+                            };
+
+    public static decimal Interest(decimal balance) => InterestRatePercentage(balance) * Math.Abs(balance);
 
     public static decimal AnnualBalanceUpdate(decimal balance) => balance + Interest(balance);
 
