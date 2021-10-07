069633754e466ee10405b27ea00fa697af0ef421
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-07 08:28:56 -0600

    Implemented GenerateSpellStrength function and all tests passing

--- csharp/roll-the-die/RollTheDie.cs
+++ csharp/roll-the-die/RollTheDie.cs
@@ -2,13 +2,11 @@ using System;
 
 public class Player
 {
-    private readonly Random _randomNumberGenerator = new Random();
+    private static readonly Random _randomNumberGenerator = new Random();
     private const int DieSides = 18;
+    private const int SpellMaxStrength = 100;
 
     public int RollDie() => _randomNumberGenerator.Next(DieSides) + 1;
 
-    public double GenerateSpellStrength()
-    {
-        throw new NotImplementedException("Please implement the Player.GenerateSpellStrength() method");
-    }
+    public double GenerateSpellStrength() => _randomNumberGenerator.NextDouble() * SpellMaxStrength;
 }
