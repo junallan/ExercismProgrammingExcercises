ac880f7534d5e9133daed2ef76a31a54bb095c2c
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-06 20:01:26 -0600

    Replaced algorithm for LogLevel to make it easier to read

--- csharp/log-levels/LogLevels.cs
+++ csharp/log-levels/LogLevels.cs
@@ -9,15 +9,11 @@ static class LogLine
         return logLine.Substring(endOfMessageLevelIdentifier).Trim();
     }
 
-    public static string LogLevel(string logLine)
-    {
-        int beginningMarkerForLogLevelIdentifier = logLine.IndexOf("[") + 1;
-        int endingMarkerForLogLevelIdentifier = logLine.IndexOf("]");
-
-        return logLine.Substring(beginningMarkerForLogLevelIdentifier, 
-                                 endingMarkerForLogLevelIdentifier - beginningMarkerForLogLevelIdentifier)
-                      .ToLower();
-    }
+    public static string LogLevel(string logLine) => logLine.Replace(Message(logLine), string.Empty)
+                                                            .Replace("[", string.Empty)
+                                                            .Replace("]: ", string.Empty)
+                                                            .TrimEnd()
+                                                            .ToLower();
 
     public static string Reformat(string logLine) => $"{Message(logLine)} ({LogLevel(logLine)})";
 }
