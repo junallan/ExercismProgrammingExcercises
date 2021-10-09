ecb92a983ab42fbe4de20726947bcb8356441225
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-09 10:51:37 -0600

    Changed naming of IsContainLetter function to ContainsLetter to be more readable.  Changed null check to use built in IsNullOrWhiteSpace method.  To eliminate reallocation of new string.

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -3,14 +3,14 @@ using System;
 public static class Bob
 {
     private static bool IsAskingQuestion(string statement)          => statement.TrimEnd().EndsWith("?");
-    private static bool IsShouting(string statement)                => statement == statement.ToUpper() && IsContainLetter(statement);
-    private static bool IsContainLetter(string statement)           => statement.ToUpper() != statement.ToLower();
+    private static bool IsShouting(string statement)                => statement == statement.ToUpper() && ContainsLetter(statement);
+    private static bool ContainsLetter(string statement)            => statement.ToUpper() != statement.ToLower();
 
     public static string Response(string statement)
     {
         return statement switch
         {
-           string val when val.Trim() == string.Empty               => "Fine. Be that way!",
+           string val when string.IsNullOrWhiteSpace(val)           => "Fine. Be that way!",
            string val when IsAskingQuestion(val) && IsShouting(val) => "Calm down, I know what I'm doing!",
            string val when IsAskingQuestion(val)                    => "Sure.",
            string val when IsShouting(val)                          => "Whoa, chill out!",
