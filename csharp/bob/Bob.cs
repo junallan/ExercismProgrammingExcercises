914a6d74cb68d161108e0424ee58eb89ec68a8e0
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-08 19:43:19 -0600

    Extract helper IsStatementQuestion function

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -2,11 +2,13 @@ using System;
 
 public static class Bob
 {
+    private static bool IsStatementQuestion(string statement) => statement.EndsWith("?");
+
     public static string Response(string statement)
     {
         return statement switch
         {
-           string val when val.EndsWith("?") => "Sure.",
+           string val when IsStatementQuestion(val) => "Sure.",
            string val when val == val.ToUpper() => "Whoa, chill out!",
            _ => "Whatever."
         };
