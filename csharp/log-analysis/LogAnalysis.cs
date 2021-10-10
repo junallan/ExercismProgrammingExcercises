c03b49b158bf82ce05be716b43f8192449c66e97
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-10 13:52:07 -0600

    Refactored SubstringBetween simplifying logic with string split method

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -2,18 +2,8 @@ using System;
 
 public static class LogAnalysis 
 {
-
-    private static int IndexAfterDelimeter(this string message, string delimeter) => message.IndexOf(delimeter) + delimeter.Length;
-    public static string SubstringAfter(this string message, string delimeter) => message.Split(delimeter)[1]; //message.Substring(message.IndexAfterDelimeter(delimeter));
-   
-    public static string SubstringBetween(this string message, string beginningDelimeter, string endingDelimeter)
-    {
-        int indexAfterStartDelimeter = message.IndexAfterDelimeter(beginningDelimeter);
-        int messageContentLength = message.IndexOf(endingDelimeter) - indexAfterStartDelimeter;
-
-        return message.Substring(indexAfterStartDelimeter, messageContentLength);
-    }
-
+    public static string SubstringAfter(this string message, string delimeter) => message.Split(delimeter)[1];
+    public static string SubstringBetween(this string message, string beginningDelimeter, string endingDelimeter) => message.Split(endingDelimeter)[0].Substring(1);
     public static string Message(this string message) => message.SubstringAfter(": ");
     public static string LogLevel(this string message) => message.SubstringBetween("[", "]");
 }
