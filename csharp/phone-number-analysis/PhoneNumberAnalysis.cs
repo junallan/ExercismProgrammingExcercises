35bdc53497603f2fe0033e7322217c3f7d36dae7
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-10 16:35:27 -0600

    Solution to exercism phone number problem

---
+++ csharp/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -0,0 +1,23 @@
+using System;
+
+public static class PhoneNumber
+{
+    const string NewYorkAreaCode = "212";
+    const string FakePrefixCode = "555";
+
+    private static bool IsNewYorkAreaCode(string areaCode) => areaCode == NewYorkAreaCode;
+    private static bool IsFakePrefixCode(string prefixCode) => prefixCode == FakePrefixCode;
+
+    public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
+    {
+        string[] phoneNumberSplit = phoneNumber.Split("-");
+        (string AreaCode, string PrefixCode, string LocalNumber) phoneNumberParsed = (phoneNumberSplit[0], phoneNumberSplit[1], phoneNumberSplit[2]);
+
+        bool isNewYorkAreaCode = IsNewYorkAreaCode(phoneNumberParsed.AreaCode);
+        (bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo = (isNewYorkAreaCode, IsFakePrefixCode(phoneNumberParsed.PrefixCode), phoneNumberParsed.LocalNumber);
+ 
+        return (isNewYorkAreaCode, IsFake(phoneNumberInfo), phoneNumberParsed.LocalNumber);
+    }
+
+    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo) => phoneNumberInfo.IsFake;
+}
