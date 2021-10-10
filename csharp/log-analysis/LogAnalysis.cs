303fb2639e4faae99f9329d06943b5df00843dfe
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-10 14:10:01 -0600

    Simplified SubstringBetween utilizing 2 splits

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Linq;
 
 public static class LogAnalysis 
 {
     public static string SubstringAfter(this string message, string delimeter) => message.Split(delimeter)[1];
-    public static string SubstringBetween(this string message, string beginningDelimeter, string endingDelimeter) => message.Split(endingDelimeter)[0].Substring(1);
+    public static string SubstringBetween(this string message, string beginningDelimeter, string endingDelimeter) => message.Split(beginningDelimeter)[1].Split(endingDelimeter)[0];
     public static string Message(this string message) => message.SubstringAfter(": ");
     public static string LogLevel(this string message) => message.SubstringBetween("[", "]");
 }
