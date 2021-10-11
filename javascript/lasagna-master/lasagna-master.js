a85ea6ca03d4f1437911c3c9d1c91611ec7dad3f
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-11 10:47:59 -0600

    Implemented quantities function

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -1,6 +1,9 @@
 /// <reference path="./global.d.ts" />
 // @ts-check
 
+const GRAMS_OF_NOODLES_PER_LAYER = 50;
+const LITERS_OF_SAUCE_PER_LAYER = 0.2;
+
 export function cookingStatus(remainingCookingTime) {
 	switch (remainingCookingTime) {
 		case undefined: return 'You forgot to set the timer.';
@@ -11,4 +14,16 @@ export function cookingStatus(remainingCookingTime) {
 
 export function preparationTime(layers, preparationTime=2) {
 	return layers.length * preparationTime;
+}
+
+export function quantities(layers) {
+	let noodleQuantity = 0;
+	let sauceQuantity = 0;
+
+	for (let i = 0; i < layers.length; i++) {
+		if (layers[i] === 'sauce') sauceQuantity += LITERS_OF_SAUCE_PER_LAYER;
+		else if (layers[i] === 'noodles') noodleQuantity += GRAMS_OF_NOODLES_PER_LAYER;
+	}
+
+	return { noodles: noodleQuantity, sauce: sauceQuantity};
 }
