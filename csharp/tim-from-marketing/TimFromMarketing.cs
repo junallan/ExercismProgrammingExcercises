495c20bb7a3bbb7adde4c8391aeb3d8b9cbecbfa
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-12 08:43:42 -0600

    Fixed type in get department label helper funciton

--- csharp/tim-from-marketing/TimFromMarketing.cs
+++ csharp/tim-from-marketing/TimFromMarketing.cs
@@ -3,6 +3,6 @@ using System;
 static class Badge
 {
     private static string GetIdentifierLabel(int? id) => id.HasValue ? $"[{id}] - " : string.Empty;
-    private static string GetDepartementLabel(string? department) => department?.ToUpper() ?? "OWNER";
-    public static string Print(int? id, string name, string? department) => $"{GetIdentifierLabel(id)}{name} - {GetDepartementLabel(department)}";
+    private static string GetDepartmentLabel(string? department) => department?.ToUpper() ?? "OWNER";
+    public static string Print(int? id, string name, string? department) => $"{GetIdentifierLabel(id)}{name} - {GetDepartmentLabel(department)}";
 }
