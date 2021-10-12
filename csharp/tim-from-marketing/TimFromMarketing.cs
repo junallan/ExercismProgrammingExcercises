2a0bdedd06c65141aaae4d12e9b8a3ddcdb78be9
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-11 23:28:40 -0600

    should return a function passing test on translate2d function

--- csharp/tim-from-marketing/TimFromMarketing.cs
+++ csharp/tim-from-marketing/TimFromMarketing.cs
@@ -2,5 +2,7 @@ using System;
 
 static class Badge
 {
-    public static string Print(int? id, string name, string? department) => $"{(id.HasValue ? $"[{id}] - " : string.Empty)}{name} - {department?.ToUpper() ?? "OWNER"}";
+    private static string GetIdentifierLabel(int? id) => id.HasValue ? $"[{id}] - " : string.Empty;
+    private static string GetDepartementLabel(string? department) => department?.ToUpper() ?? "OWNER";
+    public static string Print(int? id, string name, string? department) => $"{GetIdentifierLabel(id)}{name} - {GetDepartementLabel(department)}";
 }
