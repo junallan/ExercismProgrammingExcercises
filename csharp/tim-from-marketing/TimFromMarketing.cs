02900e7d89b446dfc69c083fffeca3a29213add4
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-11 22:51:21 -0600

    Use expression bodied method

--- csharp/tim-from-marketing/TimFromMarketing.cs
+++ csharp/tim-from-marketing/TimFromMarketing.cs
@@ -2,8 +2,5 @@ using System;
 
 static class Badge
 {
-    public static string Print(int? id, string name, string? department)
-    {
-        return $"{(id.HasValue ? $"[{id}] - " : string.Empty)}{name} - {department?.ToUpper() ?? "OWNER"}";
-    }
+    public static string Print(int? id, string name, string? department) => $"{(id.HasValue ? $"[{id}] - " : string.Empty)}{name} - {department?.ToUpper() ?? "OWNER"}";
 }
