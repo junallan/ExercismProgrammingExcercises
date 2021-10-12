e9329b65d99b8a1a2e3548e7cd09570290ba1dee
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-11 22:40:41 -0600

    Null coalenscing operator to simplify departementIdentifier expression.

--- csharp/tim-from-marketing/TimFromMarketing.cs
+++ csharp/tim-from-marketing/TimFromMarketing.cs
@@ -5,7 +5,7 @@ static class Badge
     public static string Print(int? id, string name, string? department)
     {
         string idIdentifier = id.HasValue ? $"[{id}] - " : string.Empty;
-        string departementIdentifier = department != null ? $" - {department.ToUpper()}" : " - OWNER";
+        string departementIdentifier = $" - {department?.ToUpper() ?? "OWNER"}";
 
         return $"{idIdentifier}{name}{departementIdentifier}";
     }
