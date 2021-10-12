970647607468a40f912792c1b43cb52e4be92d9b
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-11 22:50:01 -0600

    Get rid of intermediate variables

--- csharp/tim-from-marketing/TimFromMarketing.cs
+++ csharp/tim-from-marketing/TimFromMarketing.cs
@@ -4,9 +4,6 @@ static class Badge
 {
     public static string Print(int? id, string name, string? department)
     {
-        string idIdentifier = id.HasValue ? $"[{id}] - " : string.Empty;
-        string departementIdentifier = $" - {department?.ToUpper() ?? "OWNER"}";
-
-        return $"{idIdentifier}{name}{departementIdentifier}";
+        return $"{(id.HasValue ? $"[{id}] - " : string.Empty)}{name} - {department?.ToUpper() ?? "OWNER"}";
     }
 }
