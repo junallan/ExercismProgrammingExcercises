782928bfa07ad1e70857cc70ee813a4d8465d82a
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-14 11:57:41 -0600

    Implemented discardTopCard function

--- javascript/elyses-destructured-enchantments/enchantments.js
+++ javascript/elyses-destructured-enchantments/enchantments.js
@@ -49,7 +49,9 @@ export function swapTopTwoCards(deck) {
  * deck and a new deck containing all the other cards
  */
 export function discardTopCard(deck) {
-  throw new Error('Implement the discardTopCard function');
+	const [firstCard, ...remainingCards] = deck;
+
+	return [firstCard, remainingCards];
 }
 
 /** @type Card[] **/
