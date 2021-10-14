be6cb59bd6badd040717aea99c51d095038ff041
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-14 12:23:13 -0600

    Implemented getFirstOddCard function

--- javascript/elyses-analytic-enchantments/enchantments.js
+++ javascript/elyses-analytic-enchantments/enchantments.js
@@ -60,7 +60,7 @@ export function doesStackIncludeOddCard(stack) {
  * @returns {number} the first odd value
  */
 export function getFirstOddCard(stack) {
-  throw new Error('Implement the getFirstOddCard function');
+	return stack.find((cardNumber) => isOddCardNumber(cardNumber));
 }
 
 /**
