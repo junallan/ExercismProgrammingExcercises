ed5998ff40bee08da940b81d74672fb4ba9f12d4
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-18 11:20:03 -0600

    Changed HasPassed to compare date's with > operator to be more expressive

--- csharp/booking-up-for-beauty/BookingUpForBeauty.cs
+++ csharp/booking-up-for-beauty/BookingUpForBeauty.cs
@@ -4,8 +4,8 @@ static class Appointment
 {
     public static DateTime Schedule(string appointmentDateDescription) => DateTime.Parse(appointmentDateDescription);
 
-    public static bool HasPassed(DateTime appointmentDate) => DateTime.Now.CompareTo(appointmentDate) > 0;
-
+    public static bool HasPassed(DateTime appointmentDate) => DateTime.Now > appointmentDate;
+    
     public static bool IsAfternoonAppointment(DateTime appointmentDate) => 12 <= appointmentDate.Hour && appointmentDate.Hour < 18;
 
     public static string Description(DateTime appointmentDate) => $"You have an appointment on {appointmentDate.ToString("M/d/yyyy h:mm:ss tt")}.";
