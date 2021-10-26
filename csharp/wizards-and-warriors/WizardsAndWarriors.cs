e20d300507c17154d0fa7fe2827306430da53628
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-10-26 14:13:43 -0600

    Implemented ToString function

---
+++ csharp/wizards-and-warriors/WizardsAndWarriors.cs
@@ -0,0 +1,52 @@
+using System;
+
+abstract class Character
+{
+    private string _characterType;
+
+    protected Character(string characterType)
+    {
+        _characterType = characterType;
+    }
+
+    public abstract int DamagePoints(Character target);
+
+    public virtual bool Vulnerable()
+    {
+        throw new NotImplementedException("Please implement the Character.Vulnerable() method");
+    }
+
+    public override string ToString()
+    {
+        return $"Character is a {_characterType}";
+    }
+}
+
+class Warrior : Character
+{
+    public Warrior() : base("Warrior")
+    {
+    }
+
+    public override int DamagePoints(Character target)
+    {
+        throw new NotImplementedException("Please implement the Warrior.DamagePoints() method");
+    }
+}
+
+class Wizard : Character
+{
+    public Wizard() : base("Wizard")
+    {
+    }
+
+    public override int DamagePoints(Character target)
+    {
+        throw new NotImplementedException("Please implement the Wizard.DamagePoints() method");
+    }
+
+    public void PrepareSpell()
+    {
+        throw new NotImplementedException("Please implement the Wizard.PrepareSpell() method");
+    }
+}
