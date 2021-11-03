8c811ee5cb37d62928f2f02685ce2cf5a29c9925
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-11-03 08:44:01 -0600

    Extract utility functions to make code more readable

--- csharp/squeaky-clean/SqueakyClean.cs
+++ csharp/squeaky-clean/SqueakyClean.cs
@@ -4,25 +4,23 @@ using System.Text;
 
 public static class Identifier
 {
-    public static string Clean(string identifier)
-    {
-        //var identifierFiltered = identifier.Replace(' ', '_').Replace("\0", "CTRL");
+    private static bool IsGreekLetter(char letter) => letter is >= 'α' and <= 'ω';
+    private static bool IsConvertToKebabCase(string identifier, int i) => (i > 0) && identifier[i - 1] == '-';
 
+    public static string Clean(string identifier)
+    { 
         StringBuilder identifierFiltered = new StringBuilder();
 
         for(int i=0; i< identifier.Length; i++)
         {
             if (identifier[i] == ' ') identifierFiltered.Append("_");
             else if (identifier[i] == '\0') identifierFiltered.Append("CTRL");
-            else if ((i > 0) && identifier[i - 1] == '-')
-            {
-                identifierFiltered.Append(Char.ToUpper(identifier[i]));
-            }
-            else if (Char.IsLetter(identifier[i]) && !(identifier[i] >= 'α' && identifier[i] <= 'ω')) identifierFiltered.Append(identifier[i]);
-
-        
+            else if (IsConvertToKebabCase(identifier, i)) identifierFiltered.Append(Char.ToUpper(identifier[i]));    
+            else if (Char.IsLetter(identifier[i]) && !IsGreekLetter(identifier[i])) identifierFiltered.Append(identifier[i]);
         }
 
         return identifierFiltered.ToString();
     }
+
+  
 }
