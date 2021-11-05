5e2486e1f3e1258857d1e7e8a4317369c719b865
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-11-05 08:14:08 -0600

    Removed unecessary using statements and renamed helper function and variable declaration for better readability.

--- csharp/squeaky-clean/SqueakyClean.cs
+++ csharp/squeaky-clean/SqueakyClean.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Linq;
 using System.Text;
 
 public static class Identifier
 {
     private static bool IsGreekLetter(char letter) => letter is >= 'α' and <= 'ω';
-    private static bool IsConvertToKebabCase(string identifier, int i) => (i > 0) && identifier[i - 1] == '-';
+    private static bool ShouldConvertToKebabCase(string identifier, int i) => (i > 0) && identifier[i - 1] == '-';
 
     public static string Clean(string identifier)
     { 
-        StringBuilder identifierFiltered = new StringBuilder();
+        var identifierFiltered = new StringBuilder();
 
         for(int i=0; i< identifier.Length; i++)
         {
@@ -17,7 +16,7 @@ public static class Identifier
             {
                 Char value when value == ' ' => "_",
                 Char value when value == '\0' => "CTRL",
-                Char value when IsConvertToKebabCase(identifier, i)  => Char.ToUpper(value).ToString(),
+                Char value when ShouldConvertToKebabCase(identifier, i)  => Char.ToUpper(value).ToString(),
                 Char value when Char.IsLetter(value) && !IsGreekLetter(value) => value,
                 _ => ""
             });
