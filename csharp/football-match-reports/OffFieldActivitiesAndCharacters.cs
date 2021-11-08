1f23c9e76f86b9f0bc5352d29931670bd585b291
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-11-08 07:33:35 -0700

    Implemented AnalyzeOnField function

---
+++ csharp/football-match-reports/OffFieldActivitiesAndCharacters.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class PlayAnalyzer
+{ 
+    public static string AnalyzeOnField(int position)
+    {
+        switch (position)
+        {
+            case 1: return "goalie";
+            case 2: return "left back";
+            case 3:
+            case 4: return "center back";
+            case 5: return "right back";
+            case 6:
+            case 7:
+            case 8: return "midfielder";
+            case 9: return "left wing";
+            case 10: return "striker";
+            case 11: return "right wing";
+            default: throw new ArgumentOutOfRangeException();
+        }
+    }
+}
+
+public class Manager
+{
+    public string Name { get; }
+
+    public string? Club { get; }
+
+    public Manager(string name, string? club)
+    {
+        this.Name = name;
+        this.Club = club;
+    }
+}
+
+public class Incident
+{
+    public virtual string GetDescription() => "An incident happened.";
+}
+
+public class Foul : Incident
+{
+    public override string GetDescription() => "The referee deemed a foul.";
+}
+
+public class Injury : Incident
+{
+    private readonly int player;
+
+    public Injury(int player)
+    {
+        this.player = player;
+    }
+
+    public override string GetDescription() => $"Player {player} is injured.";
+}
