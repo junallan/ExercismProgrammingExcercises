c7742a255afcc9c443eb2f8cb033f9a5375460b7
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-11-10 06:12:46 -0700

    Extracted PlayAnalyzer class to FootbalMatchReport.cs file

--- csharp/football-match-reports/FootballMatchReportsTests.cs
+++ csharp/football-match-reports/FootballMatchReportsTests.cs
@@ -1,5 +1,6 @@
 using System;
 using Exercism.Tests;
+using FootballMatchReports;
 using Xunit;
 
 public class FootballMatchReportsTests
--- csharp/football-match-reports/OffFieldActivitiesAndCharacters.cs
+++ csharp/football-match-reports/OffFieldActivitiesAndCharacters.cs
@@ -1,33 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class PlayAnalyzer
-{ 
-    public static string AnalyzeOnField(int position)
-    {
-        switch (position)
-        {
-            case 1: return "goalie";
-            case 2: return "left back";
-            case 3 or 4: return "center back";
-            case 5: return "right back";
-            case 6 or 7 or 8: return "midfielder";
-            case 9: return "left wing";
-            case 10: return "striker";
-            case 11: return "right wing";
-            default: throw new ArgumentOutOfRangeException();
-        }
-    }
 
-    public static string AnalyzeOffField(object criteria) => criteria switch
-    {
-        int supporters => $"There are {supporters} supporters at the match.",
-        string commentary => commentary,
-        Incident incident => incident.GetDescription(),
-        Manager manager => manager.ToString(),
-        _ => throw new ArgumentException()
-    };
-}
 
 public class Manager
 {
