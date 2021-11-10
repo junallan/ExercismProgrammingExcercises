5ec5b418ba14b2a57c5b655ea72d57771a90e023
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-11-10 07:15:38 -0700

    Handle empty string encode condition

---
+++ csharp/run-length-encoding/RunLengthEncoding.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class RunLengthEncoding
+{
+    public static string Encode(string input)
+    {
+        if (input.Length == 0) return string.Empty;
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public static string Decode(string input)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
