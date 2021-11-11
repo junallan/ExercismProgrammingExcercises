9a207e93dee6241b229cb178eb847554f30f01d6
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-11-11 08:19:08 -0700

    Removed unecessary boilerplate code

--- csharp/run-length-encoding/RunLengthEncoding.cs
+++ csharp/run-length-encoding/RunLengthEncoding.cs
@@ -14,32 +14,21 @@ public static class RunLengthEncoding
         for(int i=1; i<input.Length; i++)
         {
             if(input[i-1] == input[i])
-            {
                 numberOfRepeatedInput++;
-
-                //if (i == input.Length - 1) encodedInput.Append($"{numberOfRepeatedInput}{input[i]}");
-            }
             else
             {              
                 encodedInput.Append($"{((numberOfRepeatedInput == 1) ? string.Empty : numberOfRepeatedInput)}{input[i-1]}");
-
-                //if (i == input.Length - 1) encodedInput.Append(input[i].ToString());
 
                 numberOfRepeatedInput = 1;
             }
         }
 
         if(input.ElementAt(input.Length -2) == input.Last())
-        {
             encodedInput.Append($"{numberOfRepeatedInput}{input.Last()}");
-        }
         else
-        {
             encodedInput.Append(input.Last());
-        }
 
         return encodedInput.ToString();
-        //return input.Aggregate(string.Empty, (encodedInput, element) => encodedInput.Last() == element ? )
     }
 
     private static bool IsLetterOrWhiteSpace(this char input) => Char.IsLetter(input) || Char.IsWhiteSpace(input);
@@ -55,8 +44,7 @@ public static class RunLengthEncoding
         if (input.First().IsLetterOrWhiteSpace() && input[1].IsLetterOrWhiteSpace()) decodedInput.Append(input.First());
         
         for(int i=0; i<input.Length-1; i++)
-        {
-           
+        {    
             if(input[i].IsLetterOrWhiteSpace() && input[i+1].IsLetterOrWhiteSpace())
             {
                 decodedInput.Append(input[i+1]);
@@ -67,13 +55,9 @@ public static class RunLengthEncoding
                 int numberOfRepetitionOfLetter = int.Parse(input.Substring(startPositionOfNumber, i - startPositionOfNumber + 1));
 
                 if(numberOfRepetitionOfLetter == 0)
-                {
                     decodedInput.Insert(decodedInput.Length, input[i + 1].ToString(), int.Parse(input[i].ToString()));
-                }
                 else
-                {
                     decodedInput.Insert(decodedInput.Length, input[i + 1].ToString(), numberOfRepetitionOfLetter);
-                }
 
                 startPositionOfNumber = i + 2;
             }
