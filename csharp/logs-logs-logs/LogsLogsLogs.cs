fa09e96a0c2b74743a2ec3cb6d9cd45d3e852b1f
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-11-14 07:28:01 -0700

    ParseLogLevel function handling Warning and unknown case

---
+++ csharp/logs-logs-logs/LogsLogsLogs.cs
@@ -0,0 +1,29 @@
+using System;
+
+public enum LogLevel
+{ 
+    Trace,
+    Debug,
+    Info,
+    Warning,
+    Error,
+    Fatal,
+    Unknown
+}
+
+static class LogLine
+{
+    public static LogLevel ParseLogLevel(string logLine)
+    {
+        return logLine.TrimStart() switch
+        {
+            string lineInfo when lineInfo.StartsWith("[WRN]") => LogLevel.Warning,
+            _ => LogLevel.Unknown
+        };
+    }
+
+    public static string OutputForShortLog(LogLevel logLevel, string message)
+    {
+        throw new NotImplementedException("Please implement the (static) LogLine.OutputForShortLog() method");
+    }
+}
