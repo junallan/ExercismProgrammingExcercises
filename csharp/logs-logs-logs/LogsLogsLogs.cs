89b5dc0de0d4850f45a5b36db1bfa7bf1666b43a
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-11-14 07:40:38 -0700

    Implemented OutputForShortLog function

--- csharp/logs-logs-logs/LogsLogsLogs.cs
+++ csharp/logs-logs-logs/LogsLogsLogs.cs
@@ -2,13 +2,13 @@ using System;
 
 public enum LogLevel
 { 
-    Trace,
-    Debug,
-    Info,
-    Warning,
-    Error,
-    Fatal,
-    Unknown
+    Trace = 1,
+    Debug = 2,
+    Info = 4,
+    Warning = 5,
+    Error = 6,
+    Fatal = 42,
+    Unknown = 0
 }
 
 static class LogLine
@@ -17,14 +17,12 @@ static class LogLine
     {
         string lineInfo when lineInfo.StartsWith("[TRC]") => LogLevel.Trace,
         string lineInfo when lineInfo.StartsWith("[DBG]") => LogLevel.Debug,
+        string lineInfo when lineInfo.StartsWith("[INF]") => LogLevel.Info,
         string lineInfo when lineInfo.StartsWith("[WRN]") => LogLevel.Warning,
         string lineInfo when lineInfo.StartsWith("[ERR]") => LogLevel.Error,
         string lineInfo when lineInfo.StartsWith("[FTL]") => LogLevel.Fatal,
         _ => LogLevel.Unknown
     };
 
-    public static string OutputForShortLog(LogLevel logLevel, string message)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.OutputForShortLog() method");
-    }
+    public static string OutputForShortLog(LogLevel logLevel, string message) => $"{(int)logLevel}:{message}";
 }
