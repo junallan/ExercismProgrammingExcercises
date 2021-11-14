fcfcf28fc6745e8c8acb3390d5991e1c5e3f3fd7
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-11-14 07:32:25 -0700

    ParseLogLevel handling Trace case

--- csharp/logs-logs-logs/LogsLogsLogs.cs
+++ csharp/logs-logs-logs/LogsLogsLogs.cs
@@ -15,6 +15,7 @@ static class LogLine
 {
     public static LogLevel ParseLogLevel(string logLine) => logLine.TrimStart() switch
     {
+        string lineInfo when lineInfo.StartsWith("[TRC]") => LogLevel.Trace,
         string lineInfo when lineInfo.StartsWith("[WRN]") => LogLevel.Warning,
         string lineInfo when lineInfo.StartsWith("[ERR]") => LogLevel.Error,
         string lineInfo when lineInfo.StartsWith("[FTL]") => LogLevel.Fatal,
