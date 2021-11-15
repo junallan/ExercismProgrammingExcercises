4f13f02aacb6732edee428d2116856cd322d0b83
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-11-15 07:23:08 -0700

    Added constants to remove majic numbers

--- csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
+++ csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
@@ -3,29 +3,34 @@ using System.Collections.Generic;
 
 public static class TelemetryBuffer
 {
+    private const int BitsInByte = 256;
+    private const int ShortByteCount = 2;
+    private const int IntByteCount = 4;
+    private const int LongByteCount = 8;
+
     public static byte[] ToBuffer(long reading)
     {
         byte[] result = new byte[9];
 
-        if (UInt32.MaxValue < reading && reading <= Int64.MaxValue) result[0] = 256 - 8;
-        else if(Int32.MaxValue < reading) result[0] = 4;
-        else if(UInt16.MaxValue < reading) result[0] = 256 - 4;
-        else if(Int16.MaxValue < reading) result[0] =2;
+        if (UInt32.MaxValue < reading && reading <= Int64.MaxValue) result[0] = BitsInByte - LongByteCount;
+        else if(Int32.MaxValue < reading) result[0] = IntByteCount;
+        else if(UInt16.MaxValue < reading) result[0] = BitsInByte - IntByteCount;
+        else if(Int16.MaxValue < reading) result[0] = ShortByteCount;
         else if(Int16.MinValue <= reading)
         {
-            result[0] = 256 - 2;
+            result[0] = BitsInByte - ShortByteCount;
             var payload = BitConverter.GetBytes((short) reading);
             payload.CopyTo(result, 1);
         }
         else if(Int32.MinValue <= reading)
         {
-            result[0] = 256 - 4;
+            result[0] = BitsInByte - IntByteCount;
             var payload = BitConverter.GetBytes((int)reading);
             payload.CopyTo(result, 1);
         }
         else
         {
-            result[0] = 256 - 8;
+            result[0] = BitsInByte - LongByteCount;
             var payload = BitConverter.GetBytes((long)reading);
             payload.CopyTo(result, 1);
         }
