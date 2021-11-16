5c31f38a04310a0b6781043ce879eb1c65a235ac
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-11-15 17:44:05 -0700

    Extract signed prefeix byte helper field variables

--- csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
+++ csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
@@ -9,34 +9,38 @@ public static class TelemetryBuffer
     private const int IntByteCount = 4;
     private const int LongByteCount = 8;
 
+    private static byte ShortSignedPrefixByte = BitsInByte - ShortByteCount;
+    private static byte IntSignedPrefixByte = BitsInByte - IntByteCount;
+    private static byte LongSignedPrefixByte = BitsInByte - LongByteCount;
+
     public static byte[] ToBuffer(long reading)
     {
         byte[] result = new byte[9];
 
-        if (UInt32.MaxValue < reading && reading <= Int64.MaxValue) result[0] = BitsInByte - LongByteCount;
+        if (UInt32.MaxValue < reading && reading <= Int64.MaxValue) result[0] = LongSignedPrefixByte;
         else if(Int32.MaxValue < reading) result[0] = IntByteCount;
-        else if(UInt16.MaxValue < reading) result[0] = BitsInByte - IntByteCount;
+        else if(UInt16.MaxValue < reading) result[0] = IntSignedPrefixByte;
         else if(Int16.MaxValue < reading) result[0] = ShortByteCount;
         else if(Int16.MinValue <= reading)
         {
-            result[0] = BitsInByte - ShortByteCount;
+            result[0] = ShortSignedPrefixByte;
             var payload = BitConverter.GetBytes((short) reading);
             payload.CopyTo(result, 1);
         }
         else if(Int32.MinValue <= reading)
         {
-            result[0] = BitsInByte - IntByteCount;
+            result[0] = IntSignedPrefixByte;
             var payload = BitConverter.GetBytes((int)reading);
             payload.CopyTo(result, 1);
         }
         else
         {
-            result[0] = BitsInByte - LongByteCount;
+            result[0] = LongSignedPrefixByte;
             var payload = BitConverter.GetBytes((long)reading);
             payload.CopyTo(result, 1);
         }
-   
-        if(reading >= 0)
+
+        if (reading >= 0)
         {
             var payload = BitConverter.GetBytes(reading);
             payload.CopyTo(result, 1);
@@ -45,38 +49,19 @@ public static class TelemetryBuffer
         
         return result;
     }
+   
 
     public static long FromBuffer(byte[] buffer)
     {
-        var allowedPrefixes = new List<int>{ BitsInByte - LongByteCount, BitsInByte - IntByteCount, BitsInByte - ShortByteCount, ShortByteCount, IntByteCount, 0 };
-        var prefixBuffer = (int)buffer[0];
-        if (!allowedPrefixes.Contains(prefixBuffer))
-        {
-            return 0;
-        }
-
-        //buffer[0] = 0;
-        //buffer[0] = 0xff;
-        //     buffer[1] = 0;
-        //buffer[2] = 0;
-        //buffer[3] = 0;
-        //buffer[4] = 0;
-        //buffer[5] = 0;
-        //buffer[6] = 0;
-        //buffer[7] = 0;
-        //buffer[8] = 0;
-        //Array.Reverse(buffer);
-
-        if ((BitsInByte - ShortByteCount) == prefixBuffer)
-            return BitConverter.ToInt16(buffer, 1);
-        else if ((BitsInByte - IntByteCount) == prefixBuffer)
-            return BitConverter.ToInt32(buffer, 1);
+        var allowedPrefixes = new List<byte> { LongSignedPrefixByte, IntSignedPrefixByte, ShortSignedPrefixByte, ShortByteCount, IntByteCount, 0 };
+       
+        if (!allowedPrefixes.Contains(buffer[0])) return 0;
+        else if (ShortSignedPrefixByte == buffer[0]) return BitConverter.ToInt16(buffer, 1);
+        else if (IntSignedPrefixByte == buffer[0]) return BitConverter.ToInt32(buffer, 1);
         else return BitConverter.ToInt64(buffer, 1);
-
-        // reading = reading >>1;
-        //if// (UInt32.MinValue < reading && reading <= Int64.MaxValue) //result[0] = BitsInByte - LongByteCount;
-        //    return 5; //+ LongByteCount;;
-
-        // return reading;
     }
+
+  
+
+  
 }
