7552ade352bac24bddf4e0cd7bde09120a8bc579
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-11-16 17:30:24 -0700

    Handled * case

--- csharp/calculator-conundrum/CalculatorConundrum.cs
+++ csharp/calculator-conundrum/CalculatorConundrum.cs
@@ -4,9 +4,12 @@ public static class SimpleCalculator
 {
     public static string Calculate(int operand1, int operand2, string operation)
     {
+        var expression = $"{operand1} {operation} {operand2}";
+
         return operation switch
         {
-            "+" => $"{operand1} {operation} {operand2} = {operand1 + operand2}",
+            "+" => $"{expression} = {operand1 + operand2}",
+            "*" => $"{expression} = {operand1 * operand2}"
         };
     }
 }
