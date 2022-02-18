8b6a9a076eddcfb60d5649853039986df148222b
Jun Allan Parreno <junallan@telus.net>
2022-02-17 22:59:49 -0700

    Simplified code using => notation, eliminating unecessary else conditions, and remove unecessary setter in property

--- csharp/weighing-machine/WeighingMachine.cs
+++ csharp/weighing-machine/WeighingMachine.cs
@@ -5,17 +5,18 @@ class WeighingMachine
     private double weight;
     private string displayWeight;
 
-    public int Precision { get;set; }
+    public int Precision { get; }
     public double Weight {
-        get { return weight; }
+        get => weight; 
         set { 
             if(value < 0) throw new ArgumentOutOfRangeException(); 
-            else weight = value; }
+            weight = value; 
         }
-    public double TareAdjustment { get;set; } = 5;
-    public string DisplayWeight { 
-        get { return $"{(Weight - TareAdjustment).ToString($"F{Precision}")} kg"; } 
     }
+
+    public double TareAdjustment { get;set; } = 5;
+    public string DisplayWeight => $"{(Weight - TareAdjustment).ToString($"F{Precision}")} kg";  
+    
 
     public WeighingMachine(int precision)
     {
