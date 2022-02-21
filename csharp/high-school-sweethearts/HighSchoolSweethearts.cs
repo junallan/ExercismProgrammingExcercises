6c4f3f3a44057009068433fa3d4f75212085ee7e
Jun Allan Parreno <junallan@telus.net>
2022-02-21 15:48:39 -0700

    DisplayBanner expression body function

--- csharp/high-school-sweethearts/HighSchoolSweethearts.cs
+++ csharp/high-school-sweethearts/HighSchoolSweethearts.cs
@@ -4,22 +4,12 @@ public static class HighSchoolSweethearts
 {
     private const int LinePaddingLength = 29;
     private const int BannerPaddingLength = 10;
-
-    private static string LineContents(string studentA, string studentB)
-    {
-        return $"**{studentA,BannerPaddingLength}  +  {studentB,-BannerPaddingLength}**";
-    }
      
 
     public static string DisplaySingleLine(string studentA, string studentB) => $"{studentA,LinePaddingLength} ♡ {studentB,-LinePaddingLength}";
 
-    public static string DisplayBanner(string studentA, string studentB)
-    {
-        var test = LineContents(studentA.Trim(), studentB.Trim());
-        // var test2 = $"{studentA.TrimEnd(),BannerPaddingLength}  +  {studentB.TrimStart(),-BannerPaddingLength}";
-        // var test3 = $"**{test2,12}**";
-           
-return $@"
+
+    public static string DisplayBanner(string studentA, string studentB) => $@"
      ******       ******
    **      **   **      **
  **         ** **         **
@@ -35,7 +25,6 @@ return $@"
              ***
               *
 ";
-    }
 
     public static string DisplayGermanExchangeStudents(string studentA
         , string studentB, DateTime start, float hours)
