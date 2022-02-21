3a09e72a53d4af60aa457d5e0046d6a429689264
Jun Allan Parreno <junallan@telus.net>
2022-02-21 14:12:58 -0700

    DisplaySingleLine test passing

---
+++ csharp/high-school-sweethearts/HighSchoolSweethearts.cs
@@ -0,0 +1,20 @@
+using System;
+
+public static class HighSchoolSweethearts
+{
+    public static string DisplaySingleLine(string studentA, string studentB)
+    {
+        return $"{studentA, 29} ♡ {studentB, -29}";
+    }
+
+    public static string DisplayBanner(string studentA, string studentB)
+    {
+        throw new NotImplementedException($"Please implement the (static) HighSchoolSweethearts.DisplayBanner() method");
+    }
+
+    public static string DisplayGermanExchangeStudents(string studentA
+        , string studentB, DateTime start, float hours)
+    {
+        throw new NotImplementedException($"Please implement the (static) HighSchoolSweethearts.DisplayGermanExchangeStudents() method");
+    }
+}
