c718f8d0b7c9b87f684f2c8555ddb8658e5893ea
Jun Allan Parreno <junallan@telus.net>
2022-03-19 13:43:56 -0600

    Implemented IComparable interface and TestTrack object

--- csharp/remote-control-competition/RemoteControlCompetition.cs
+++ csharp/remote-control-competition/RemoteControlCompetition.cs
@@ -8,37 +8,26 @@ public interface IRemoteControlCar
     void Drive();
 }
 
-public class ProductionRemoteControlCar : IRemoteControlCar
+public class ProductionRemoteControlCar : IRemoteControlCar, IComparable<ProductionRemoteControlCar>
 {
     public int DistanceTravelled { get; private set; }
     public int NumberOfVictories { get; set; }
 
-    public void Drive()
-    {
-        DistanceTravelled += 10;
-    }
+    public int CompareTo(ProductionRemoteControlCar other) => this.NumberOfVictories.CompareTo(other.NumberOfVictories);
+
+    public void Drive() => DistanceTravelled += 10;
 }
 
 public class ExperimentalRemoteControlCar : IRemoteControlCar
 {
     public int DistanceTravelled { get; private set; }
 
-    public void Drive()
-    {
-        DistanceTravelled += 20;
-    }
+    public void Drive() => DistanceTravelled += 20;
 }
 
 public static class TestTrack
 {
-    public static void Race(IRemoteControlCar car)
-    {
-        car.Drive();
-    }
-
-    public static List<ProductionRemoteControlCar> GetRankedCars(ProductionRemoteControlCar prc1,
-        ProductionRemoteControlCar prc2)
-    {
-        throw new NotImplementedException($"Please implement the (static) TestTrack.GetRankedCars() method");
-    }
+    public static void Race(IRemoteControlCar car) => car.Drive();
+    public static List<ProductionRemoteControlCar> GetRankedCars(ProductionRemoteControlCar prc1, ProductionRemoteControlCar prc2) 
+                                                        => prc1.CompareTo(prc2) < 0 ? new List<ProductionRemoteControlCar> { prc1, prc2 } : new List<ProductionRemoteControlCar> { prc2, prc1 };
 }
