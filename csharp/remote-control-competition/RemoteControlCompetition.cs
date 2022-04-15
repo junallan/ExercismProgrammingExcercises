bc2ae61788e1e6fcb76c893efc5e4ad79cae9c52
Jun Allan Parreno <junallan@telus.net>
2022-04-15 10:44:31 -0600

    Removed comment

--- csharp/remote-control-competition/RemoteControlCompetition.cs
+++ csharp/remote-control-competition/RemoteControlCompetition.cs
@@ -30,7 +30,7 @@ public static class TestTrack
     public static void Race(IRemoteControlCar car) => car.Drive();
     public static List<ProductionRemoteControlCar> GetRankedCars(ProductionRemoteControlCar prc1, ProductionRemoteControlCar prc2) 
     {
-        var productionRemoteControlCars = new List<ProductionRemoteControlCar>{prc1, prc2}; //prc1.CompareTo(prc2) < 0 ? new List<ProductionRemoteControlCar> { prc1, prc2 } : new List<ProductionRemoteControlCar> { prc2, prc1 };
+        var productionRemoteControlCars = new List<ProductionRemoteControlCar>{prc1, prc2}; 
         
         productionRemoteControlCars.Sort();
         
