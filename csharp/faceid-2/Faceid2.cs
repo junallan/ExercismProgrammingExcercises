9c16a7ffcb217ce9d6b6843e2ddb032afe5e8d6f
Jun Allan Parreno <junallan@telus.net>
2022-10-12 18:03:21 -0600

    Implemented IsAdmin function on Authentiator class

--- csharp/faceid-2/Faceid2.cs
+++ csharp/faceid-2/Faceid2.cs
@@ -30,10 +30,7 @@ public class Authenticator
 {
     public static bool AreSameFace(FacialFeatures faceA, FacialFeatures faceB) => faceA.EyeColor.Equals(faceB.EyeColor) && faceA.PhiltrumWidth.Equals(faceB.PhiltrumWidth);
 
-    public bool IsAdmin(Identity identity)
-    {
-        throw new NotImplementedException("Please implement the Authenticator.IsAdmin() method");
-    }
+    public bool IsAdmin(Identity identity) => identity.Email.Equals("admin@exerc.ism") && AreSameFace(identity.FacialFeatures, new FacialFeatures("green", 0.9m));
 
     public bool Register(Identity identity)
     {
