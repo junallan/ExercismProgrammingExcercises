0e248847ccb20ccd12b4010104f3ad45df9dff66
Jun Allan Parreno <junallan@telus.net>
2022-10-18 08:59:27 -0600

    Describe a character traveling to a destination implementation

--- csharp/wizards-and-warriors-2/WizardsAndWarriors2.cs
+++ csharp/wizards-and-warriors-2/WizardsAndWarriors2.cs
@@ -11,10 +11,7 @@ static class GameMaster
         
         return $"You're traveling to your destination {methodOfTravel} {travelMethod.ToString().ToLower()}.";
     }
-    public static string Describe(Character character, Destination destination, TravelMethod travelMethod)
-    {
-        throw new NotImplementedException("Please implement the (static) GameMaster.Describe(Character, Destination, TravelMethod) method");
-    }
+    public static string Describe(Character character, Destination destination, TravelMethod travelMethod) => $"{Describe(character)} {Describe(travelMethod)} {Describe(destination)}";
 
     public static string Describe(Character character, Destination destination)
     {
