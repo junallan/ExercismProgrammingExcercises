3434be2ec68d6bdeaf2058d8ded3abbd849fd3dd
Jun Allan Parreno <junallan@telus.net>
2022-11-02 06:52:12 -0600

    Implemented GetClaimWithLongestSide and revised IsLastClaim

--- csharp/land-grab-in-space/LandGrabInSpace.cs
+++ csharp/land-grab-in-space/LandGrabInSpace.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public struct Coord
 {
@@ -30,23 +32,112 @@ public struct Plot
 
 public class ClaimsHandler
 {
-    public Plot Plot{ get; set; }
+    public List<Plot> Plots{ get; set; } = new List<Plot>();
 
     public void StakeClaim(Plot plot)
     {
-        Plot = plot;
+        Plots.Add(plot);
     }
 
-    public bool IsClaimStaked(Plot plot) => 
-        Plot.C1.X == plot.C1.X && Plot.C1.Y == plot.C1.Y && 
-        Plot.C2.X == plot.C2.X && Plot.C2.Y == plot.C2.Y && 
-        Plot.C3.X == plot.C3.X && Plot.C3.Y == plot.C3.Y &&
-        Plot.C4.X == plot.C4.X && Plot.C4.Y == plot.C4.Y;
+    public bool IsClaimStaked(Plot plot)
+    {
+        bool isStaked = false;
 
-    public bool IsLastClaim(Plot plot) => IsClaimStaked(plot);
+        foreach(var section in Plots)
+        {
+            if(section.C1.X == plot.C1.X && section.C1.Y == plot.C1.Y && 
+                section.C2.X == plot.C2.X && section.C2.Y == plot.C2.Y && 
+                section.C3.X == plot.C3.X && section.C3.Y == plot.C3.Y &&
+                section.C4.X == plot.C4.X && section.C4.Y == plot.C4.Y)
+            {
+                isStaked = true;
+                break;
+            }
+        }
+
+        return isStaked;
+    }
+        
+
+    public bool IsLastClaim(Plot plot)
+    {
+        var lastPlotItem = Plots.Last();
+        return GetCoordinateLength(lastPlotItem.C1, plot.C1) == 0 && GetCoordinateLength(lastPlotItem.C2, plot.C2) == 0
+             && GetCoordinateLength(lastPlotItem.C3, plot.C3) == 0 && GetCoordinateLength(lastPlotItem.C4, plot.C4) == 0;
+    } 
 
     public Plot GetClaimWithLongestSide()
+    {  
+        int longestSideIndex = 0;
+        double currentLongestLength = 0;
+
+        for(int i=0; i<Plots.Count; i++)
+        {
+            if(currentLongestLength == 0) currentLongestLength = GetCoordinateLength(Plots[i].C1, Plots[i].C2);
+            
+            double coordinateLength = GetCoordinateLength(Plots[i].C1, Plots[i].C2);
+            
+            if(currentLongestLength < coordinateLength)
+            {
+                currentLongestLength = coordinateLength;
+                longestSideIndex = i;
+                continue;
+            }
+
+            coordinateLength = GetCoordinateLength(Plots[i].C1, Plots[i].C3);
+
+            if(currentLongestLength < coordinateLength)
+            {
+                currentLongestLength = coordinateLength;
+                longestSideIndex = i;
+                continue;
+            }
+
+            coordinateLength = GetCoordinateLength(Plots[i].C1, Plots[i].C4);
+
+            if(currentLongestLength < coordinateLength)
+            {
+                currentLongestLength = coordinateLength;
+                longestSideIndex = i;
+                continue;
+            }
+
+            coordinateLength = GetCoordinateLength(Plots[i].C2, Plots[i].C3);
+
+            if(currentLongestLength < coordinateLength)
+            {
+                currentLongestLength = coordinateLength;
+                longestSideIndex = i;
+                continue;
+            }
+
+            coordinateLength = GetCoordinateLength(Plots[i].C2, Plots[i].C4);
+
+            if(currentLongestLength < coordinateLength)
+            {
+                currentLongestLength = coordinateLength;
+                longestSideIndex = i;
+                continue;
+            }
+
+            coordinateLength = GetCoordinateLength(Plots[i].C3, Plots[i].C4);
+
+            if(currentLongestLength < coordinateLength)
+            {
+                currentLongestLength = coordinateLength;
+                longestSideIndex = i;
+                continue;
+            }
+        }
+       
+        return Plots[longestSideIndex];
+    }
+
+    private double GetCoordinateLength(Coord coord1, Coord coord2)
     {
-        throw new NotImplementedException("Please implement the ClaimsHandler.GetClaimWithLongestSide() method");
+        int xPlane = coord2.X-coord1.X;
+        int yPlane = coord2.Y-coord1.Y;
+
+        return Math.Sqrt(Math.Pow(xPlane, 2) + Math.Pow(yPlane, 2));
     }
 }
