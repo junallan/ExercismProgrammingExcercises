bdf364025200156f40eb9290b32a0eb74f5b6bd9
Jun Allan Parreno <junallan@telus.net>
2023-02-04 19:16:39 -0700

    Rate the activity

--- fsharp/valentines-day/ValentinesDay.fs
+++ fsharp/valentines-day/ValentinesDay.fs
@@ -22,4 +22,14 @@ type Activity =
     | Restaurant of Cuisine
     | Walk of int
 
-let rateActivity (activity: Activity): Approval = failwith "Please implement the 'rateActivity' function"
+let rateActivity (activity: Activity): Approval = 
+    match activity with
+    | Activity.BoardGame                  -> Approval.No
+    | Activity.Chill                      -> Approval.No
+    | Activity.Movie Genre.Romance        -> Approval.Yes
+    | Activity.Movie _                    -> Approval.No
+    | Activity.Restaurant Cuisine.Korean  -> Approval.Yes
+    | Activity.Restaurant Cuisine.Turkish -> Approval.Maybe
+    | Activity.Walk a when a < 3          -> Approval.Yes
+    | Activity.Walk a when a < 5          -> Approval.Maybe
+    | _                                   -> Approval.No 
