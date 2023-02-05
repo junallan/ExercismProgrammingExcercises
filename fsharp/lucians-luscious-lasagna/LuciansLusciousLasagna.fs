3e929a30ae9f72e6a9a1daa2cda0b39667c42f60
Jun Allan Parreno <junallan@telus.net>
2023-02-04 17:56:19 -0700

    Define the expected oven time in minutes

---
+++ fsharp/lucians-luscious-lasagna/LuciansLusciousLasagna.fs
@@ -0,0 +1,10 @@
+module LuciansLusciousLasagna
+
+// TODO: define the 'expectedMinutesInOven' binding
+let expectedMinutesInOven = 40
+// TODO: define the 'remainingMinutesInOven' function
+let remainingMinutesInOven = 0
+// TODO: define the 'preparationTimeInMinutes' function
+let preparationTimeInMinutes = 0
+// TODO: define the 'elapsedTimeInMinutes' function
+let elapsedTimeInMinutes = 0
