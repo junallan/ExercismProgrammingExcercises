eaf40e33368a42d9c97e6f8b809835c271bfdae5
Jun Allan Parreno <junallan@telus.net>
2023-02-04 18:33:07 -0700

    Define the approval

---
+++ fsharp/valentines-day/ValentinesDay.fs
@@ -0,0 +1,16 @@
+module ValentinesDay
+
+// TODO: please define the 'Approval' discriminated union type
+type Approval =
+    | Yes
+    | No
+    | Maybe
+
+
+// TODO: please define the 'Cuisine' discriminated union type
+
+// TODO: please define the 'Genre' discriminated union type
+
+// TODO: please define the 'Activity' discriminated union type
+
+let rateActivity (activity: Activity): Approval = failwith "Please implement the 'rateActivity' function"
