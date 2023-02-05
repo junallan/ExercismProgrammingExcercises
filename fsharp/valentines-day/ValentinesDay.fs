9ec40934b9544124d7fd700c9a1a8d145f8df4dd
Jun Allan Parreno <junallan@telus.net>
2023-02-04 18:38:25 -0700

    Define the activity

--- fsharp/valentines-day/ValentinesDay.fs
+++ fsharp/valentines-day/ValentinesDay.fs
@@ -9,13 +9,17 @@ type Cuisine =
     | Korean
     | Turkish
 
-// TODO: please define the 'Genre' discriminated union type
 type Genre =
     | Crime
     | Horror
     | Romance
     | Thriller
 
-// TODO: please define the 'Activity' discriminated union type
+type Activity =
+    | BoardGame
+    | Chill
+    | Movie of Genre
+    | Restaurant of Cuisine
+    | Walk of int
 
 let rateActivity (activity: Activity): Approval = failwith "Please implement the 'rateActivity' function"
