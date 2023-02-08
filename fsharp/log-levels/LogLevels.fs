6aa3c9b60fd36453a94d821492dbc1304ddbd29c
Jun Allan Parreno <junallan@telus.net>
2023-02-07 21:03:18 -0700

    Fix broken tests

--- fsharp/log-levels/LogLevels.fs
+++ fsharp/log-levels/LogLevels.fs
@@ -1,7 +1,7 @@
 module LogLevels
 
-let message (logLine: string): string = (logLine.Split ':'[1]).Trim()
+let message (logLine: string) = logLine.Split(':').[1].Trim()
 
-let logLevel(logLine: string): string = (logLine.Split ':'[0]).Replace("[","").Replace("]","").Trim().ToLower()
+let logLevel(logLine: string) = logLine.Split(':').[0].Replace("[","").Replace("]","").Trim().ToLower()
 
-let reformat(logLine: string): string = message(logLine) + " (" + logLevel(logLine) + ")" 
+let reformat(logLine: string) = message logLine + " (" + logLevel logLine + ")" 
