d95ea6bb0111db6ad606549033688df2810716f9
Jun Allan Parreno <junallan@telus.net>
2023-02-11 12:10:11 -0700

    HandleErrorWitOutParam

--- csharp/error-handling/ErrorHandling.cs
+++ csharp/error-handling/ErrorHandling.cs
@@ -6,7 +6,11 @@ public static class ErrorHandling
     public static int? HandleErrorByReturningNullableType(string input) => Int32.TryParse(input, out int result) ? result : null;
     public static bool HandleErrorWithOutParam(string input, out int result)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        result = 0;
+        
+        var isSuccess = Int32.TryParse(input, out result);
+       
+        return isSuccess;
     }
 
     public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
