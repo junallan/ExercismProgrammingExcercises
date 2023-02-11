1259100f7cfdcd3795a111c3f7fca03a89b0560c
Jun Allan Parreno <junallan@telus.net>
2023-02-11 12:03:45 -0700

    HandleErrorByReturningNullableType

--- csharp/error-handling/ErrorHandling.cs
+++ csharp/error-handling/ErrorHandling.cs
@@ -2,16 +2,8 @@ using System;
 
 public static class ErrorHandling
 {
-    public static void HandleErrorByThrowingException()
-    {
-        throw new Exception();
-    }
-
-    public static int? HandleErrorByReturningNullableType(string input)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-
+    public static void HandleErrorByThrowingException() => throw new Exception();
+    public static int? HandleErrorByReturningNullableType(string input) => Int32.TryParse(input, out int result) ? result : null;
     public static bool HandleErrorWithOutParam(string input, out int result)
     {
         throw new NotImplementedException("You need to implement this function.");
