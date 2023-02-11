52e024eacfdd2b9db373f2ec12646703bfa0e2cf
Jun Allan Parreno <junallan@telus.net>
2023-02-11 11:47:34 -0700

    recursive solution

--- csharp/robot-simulator/RobotSimulator.cs
+++ csharp/robot-simulator/RobotSimulator.cs
@@ -47,24 +47,27 @@ public class RobotSimulator
 
     public void Move(string instructions)
     {
-        foreach(char movement in instructions)
-        {
-            (_direction, _x, _y) = movement switch{
-                'R' when _direction == Direction.North  =>  (Direction.East, _x, _y),
-                'R' when _direction == Direction.East   => (Direction.South, _x, _y),
-                'R' when _direction == Direction.South  =>  (Direction.West, _x, _y),
-                'R' when _direction == Direction.West   =>  (Direction.North, _x, _y),
-                'L' when _direction == Direction.North  =>  (Direction.West, _x, _y),
-                'L' when _direction == Direction.East   =>  (Direction.North, _x, _y),
-                'L' when _direction == Direction.South  =>  (Direction.East, _x, _y),
-                'L' when _direction == Direction.West   =>  (Direction.South, _x, _y),
-                'A' when _direction == Direction.North  => (_direction, _x, _y + 1),
-                'A' when _direction == Direction.South  => (_direction, _x, _y - 1),
-                'A' when _direction == Direction.East   => (_direction, _x + 1, _y),
-                'A' when _direction == Direction.West   => (_direction, _x - 1, _y),
-                _                                       =>  (_direction, _x, _y)           
-            };
+        if(instructions.Length == 0) return;
 
-        }
+        (_direction, _x, _y) = MoveDirection(instructions[0]);
+
+        if(instructions.Length > 1) Move(instructions.Substring(1));
     }
+
+    private (Direction, int, int) MoveDirection(char movement) =>  movement switch
+    {
+        'R' when _direction == Direction.North => (Direction.East, _x, _y),
+        'R' when _direction == Direction.East => (Direction.South, _x, _y),
+        'R' when _direction == Direction.South => (Direction.West, _x, _y),
+        'R' when _direction == Direction.West => (Direction.North, _x, _y),
+        'L' when _direction == Direction.North => (Direction.West, _x, _y),
+        'L' when _direction == Direction.East => (Direction.North, _x, _y),
+        'L' when _direction == Direction.South => (Direction.East, _x, _y),
+        'L' when _direction == Direction.West => (Direction.South, _x, _y),
+        'A' when _direction == Direction.North => (_direction, _x, _y + 1),
+        'A' when _direction == Direction.South => (_direction, _x, _y - 1),
+        'A' when _direction == Direction.East => (_direction, _x + 1, _y),
+        'A' when _direction == Direction.West => (_direction, _x - 1, _y),
+        _ => (_direction, _x, _y)
+    };
 }
