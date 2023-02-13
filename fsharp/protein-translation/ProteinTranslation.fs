49aeccd98845b5aeba72e2ca4b319162d36dcf68
Jun Allan Parreno <junallan@telus.net>
2023-02-12 18:05:25 -0700

    Shorthand for pattern matching and fix misspelled variable

--- fsharp/protein-translation/ProteinTranslation.fs
+++ fsharp/protein-translation/ProteinTranslation.fs
@@ -1,13 +1,13 @@
 module ProteinTranslation
 
-let matchFound rna = match rna with | "AUG" -> ["Methionine"]
-                                    | "UUU" | "UUC" -> ["Phenylalanine"]
-                                    | "UUA" | "UUG" -> ["Leucine"]
-                                    | "UCU" | "UCC" | "UCA" | "UCG" -> ["Serine"]
-                                    | "UAU" | "UAC" -> ["Tyrosine"]
-                                    | "UGU" | "UGC" -> ["Cysteine"]
-                                    | "UGG" -> ["Tryptophan"]
-                                    | _ -> []
+let matchFound = function   | "AUG" -> ["Methionine"]
+                            | "UUU" | "UUC" -> ["Phenylalanine"]
+                            | "UUA" | "UUG" -> ["Leucine"]
+                            | "UCU" | "UCC" | "UCA" | "UCG" -> ["Serine"]
+                            | "UAU" | "UAC" -> ["Tyrosine"]
+                            | "UGU" | "UGC" -> ["Cysteine"]
+                            | "UGG" -> ["Tryptophan"]
+                            | _ -> []
 
 let rec proteins (rna : string) =
     if rna.Equals("") then
@@ -15,9 +15,9 @@ let rec proteins (rna : string) =
     else        
         let startElement = rna.[..2]
         let endElements = rna.[3..]
-        let protienResult = matchFound startElement
-        if List.isEmpty protienResult then
+        let proteinResult = matchFound startElement
+        if List.isEmpty proteinResult then
             []
         else
-            protienResult @ proteins endElements
+            proteinResult @ proteins endElements
 
