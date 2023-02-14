04f0d7c1bf034821389a6cfff819d5f623752a0b
Jun Allan Parreno <junallan@telus.net>
2023-02-13 20:44:47 -0700

    Solve specifc name case

--- fsharp/two-fer/TwoFer.fs
+++ fsharp/two-fer/TwoFer.fs
@@ -1,3 +1,6 @@
 module TwoFer
 
-let twoFer (input: string option): string = "One for you, one for me."
+let twoFer (input: string option): string =
+  match input with
+  | Some(input)  -> "One for " + input + ", one for me." 
+  | _            -> "One for you, one for me."                      
