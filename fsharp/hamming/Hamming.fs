7acabd70aeda799996596969df3affb77eaa7716
Jun Allan Parreno <junallan@telus.net>
2023-02-15 21:08:34 -0700

    Changed equivalient case

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -2,6 +2,6 @@ module Hamming
 
 let distance (strand1: string) (strand2: string): int option = 
   match strand1, strand2 with
-  | s1, s2 when String.length s1 = String.length s2 -> Some 0
+  | s1, s2 when s1 = s2 -> Some 0
   | s1, s2 when String.length s1 <> String.length s2 -> None
   | _ -> failwith "You need to implement this function."
