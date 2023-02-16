f8ba1850884ebc24296cf6d2e44f8e79e249ac48
Jun Allan Parreno <junallan@telus.net>
2023-02-15 20:59:32 -0700

    Generalized equal scenario and scenario where strings are unequal in length

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -2,5 +2,6 @@ module Hamming
 
 let distance (strand1: string) (strand2: string): int option = 
   match strand1, strand2 with
-  | s1, s2 when String.length s1 = 0 && String.length s2 = 0 -> Some 0
+  | s1, s2 when String.length s1 = String.length s2 -> Some 0
+  | s1, s2 when String.length s1 <> String.length s2 -> None
   | _ -> failwith "You need to implement this function."
