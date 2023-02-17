0f39c15d9055b1fc67a937f970afb604bba2dbbb
Jun Allan Parreno <junallan@telus.net>
2023-02-16 19:58:34 -0700

    Seq zip fold solution

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -1,7 +1,7 @@
 module Hamming
 
-let accumulateDifferencs firstStrand secondStrand =
-    (0, firstStrand, secondStrand) |||> Seq.fold2 (fun acc a b ->
+let accumulateDifferencs strandCombination =
+    (0, strandCombination) ||> Seq.fold (fun acc (a, b) ->
         match (a, b) with
         | a, b when a <> b -> acc + 1
         | _ -> acc)
@@ -10,7 +10,5 @@ let distance (strand1: string) (strand2: string): int option =
   match strand1, strand2 with
   | s1, s2 when s1 = s2 -> Some 0
   | s1, s2 when String.length s1 <> String.length s2 -> None
-  | s1, s2 when String.length s1 = String.length s2 -> let s1Elements = Seq.toList s1
-                                                       let s2Elements = Seq.toList s2
-                                                       Some (accumulateDifferencs s1Elements s2Elements)
+  | s1, s2 when String.length s1 = String.length s2 -> Some (accumulateDifferencs (Seq.zip s1 s2))
   | _ -> None                                                   
