ca0692c1e08a5e2fc7c2d87c1f4b9336d69f6b39
Jun Allan Parreno <junallan@telus.net>
2023-02-16 20:28:08 -0700

    Zip and sumBy implementation

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -1,10 +1,9 @@
 module Hamming
 
 let accumulateDifferencs strandCombination =
-    (0, strandCombination) ||> Seq.fold (fun acc (a, b) ->
-        match (a, b) with
-        | a, b when a <> b -> acc + 1
-        | _ -> acc)
+    strandCombination |> Seq.sumBy (fun (a,b) -> match a, b with
+                                                 | a, b when a <> b -> 1
+                                                 | _ -> 0)
 
 let distance (strand1: string) (strand2: string): int option = 
   match strand1, strand2 with
