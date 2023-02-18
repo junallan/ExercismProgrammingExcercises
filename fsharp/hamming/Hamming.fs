7c48d52257d7a86253034dcd6336b6afd1fb96ba
Jun Allan Parreno <junallan@telus.net>
2023-02-17 20:45:46 -0700

    Eliminated unecessary pattern match and used if else instead

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -3,7 +3,8 @@ module Hamming
 let accumulateDifferencs strandCombination =
     strandCombination |> Seq.sumBy (fun (a,b) -> if a <> b then 1 else 0)
 
-let distance (strand1: string) (strand2: string): int option = 
-  match strand1, strand2 with
-  | s1, s2 when String.length s1 = String.length s2 -> Some (Seq.zip s1 s2 |> accumulateDifferencs)
-  | _ -> None                                                   
+let distance (strand1: string) (strand2: string): int option =
+   if strand1.Length = strand2.Length then
+     Some (Seq.zip strand1 strand2 |> accumulateDifferencs)
+   else
+     None                                                   
