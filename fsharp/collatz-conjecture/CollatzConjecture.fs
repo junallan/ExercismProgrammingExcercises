8f21984abcdd77c489641ebb2b5587fab9fea89b
Jun Allan Parreno <junallan@telus.net>
2023-02-18 09:29:50 -0700

    Revised match to include _

--- fsharp/collatz-conjecture/CollatzConjecture.fs
+++ fsharp/collatz-conjecture/CollatzConjecture.fs
@@ -2,12 +2,9 @@ module CollatzConjecture
 
 let steps (number: int): int option =
     let rec computeSteps number steps =
-        if number <= 0 then
-            None
-        elif number = 1 then
-            Some steps
-        elif (number % 2 = 0) then
-            computeSteps (number / 2) (steps + 1)
-        else
-            computeSteps (3 * number + 1) (steps + 1)
+        match number with
+        | _ when number <= 0 -> None
+        | _ when number = 1 -> Some steps
+        | n when n % 2 = 0 -> computeSteps (n / 2) (steps + 1)
+        | _ -> computeSteps (3 * number + 1) (steps + 1)
     computeSteps number 0
