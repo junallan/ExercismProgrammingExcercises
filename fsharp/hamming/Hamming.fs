21b9bdabdb5b44bfbd3c4712fbd534c775d3b595
Jun Allan Parreno <junallan@telus.net>
2023-02-17 20:36:37 -0700

    Revised sum by using simple if condition

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -1,11 +1,9 @@
 module Hamming
 
-let distance (strand1: string) (strand2: string): int option =
-    let rec calculateDistance (strand1: char list) (strand2: char list) (distance: int): int option =
-        match strand1, strand2 with
-        | [], [] -> Some distance
-        | fs, [] -> None
-        | [], ss -> None
-        | fsH::fsT, ssH::ssT when fsH <> ssH -> calculateDistance fsT ssT (distance+1)
-        | _::fsT, _::ssT -> calculateDistance fsT ssT distance
-    calculateDistance (Seq.toList strand1) (Seq.toList strand2) 0
+let accumulateDifferencs strandCombination =
+    strandCombination |> Seq.sumBy (fun (a,b) -> if a <> b then 1 else 0)
+
+let distance (strand1: string) (strand2: string): int option = 
+  match strand1, strand2 with
+  | s1, s2 when String.length s1 = String.length s2 -> Some (Seq.zip s1 s2 |> accumulateDifferencs)
+  | _ -> None                                                   
