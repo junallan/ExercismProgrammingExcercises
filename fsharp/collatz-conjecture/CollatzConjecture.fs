7c17fe7c55c92571ea7cc955525fd3464c96e341
Jun Allan Parreno <junallan@telus.net>
2023-02-17 21:21:44 -0700

    Solution for CollatzConjecture

--- fsharp/collatz-conjecture/CollatzConjecture.fs
+++ fsharp/collatz-conjecture/CollatzConjecture.fs
@@ -1,7 +1,13 @@
 module CollatzConjecture
 
 let steps (number: int): int option =
-    if number = 1 then
-        Some 0
-    else
-        None
+    let rec computeSteps number steps =
+        if number = 0 then
+            None
+        elif number = 1 then
+            Some steps
+        elif (number % 2 = 0) then
+            computeSteps (number / 2) (steps + 1)
+        else
+            computeSteps (3 * number + 1) (steps + 1)
+    computeSteps number 0
