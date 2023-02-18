4dee2235b92d5769dde744f2f9d87582af459712
Jun Allan Parreno <junallan@telus.net>
2023-02-18 10:47:50 -0700

    Sequence expression solution

--- fsharp/collatz-conjecture/CollatzConjecture.fs
+++ fsharp/collatz-conjecture/CollatzConjecture.fs
@@ -1,20 +1,32 @@
 module CollatzConjecture
 
 let steps (number: int): int option =
-    if number = 1 then
-        Some 0
-    else
-        number |> Seq.unfold(fun number ->
-            match number with
-            | _ when number <= 1 -> None
-            | n when n % 2 = 0 -> Some (n, n / 2)
-            | _ -> Some (number, 3 * number + 1))
-        |> fun calculatedSteps ->
-            let numberOfSteps = Seq.length calculatedSteps
-            if numberOfSteps = 0 then
-                None 
-            else
-                Some numberOfSteps
+    let rec calculatedSteps n =
+        seq {
+            if n > 1 then
+                yield n
+                match n with
+                | n when n % 2 = 0 -> yield! calculatedSteps (n / 2) 
+                | _ -> yield! calculatedSteps (3 * n + 1) 
+        }
+
+    if number < 1 then None
+    else calculatedSteps number |> Seq.length |> Some
+
+    //if number = 1 then
+    //    Some 0
+    //else
+    //    number |> Seq.unfold(fun number ->
+    //        match number with
+    //        | _ when number <= 1 -> None
+    //        | n when n % 2 = 0 -> Some (n, n / 2)
+    //        | _ -> Some (number, 3 * number + 1))
+    //    |> fun calculatedSteps ->
+    //        let numberOfSteps = Seq.length calculatedSteps
+    //        if numberOfSteps = 0 then
+    //            None 
+    //        else
+    //            Some numberOfSteps
                          
     //let rec computeSteps number steps =
     //    match number with
