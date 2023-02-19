2ffedc33b9ad3953d5fedd59cdbf09b9ae003316
Jun Allan Parreno <junallan@telus.net>
2023-02-19 16:19:02 -0700

    Implemented isAfternoonAppointment function

--- fsharp/booking-up-for-beauty/BookingUpForBeauty.fs
+++ fsharp/booking-up-for-beauty/BookingUpForBeauty.fs
@@ -7,8 +7,7 @@ let schedule (appointmentDateDescription: string): DateTime = DateTime.Parse app
 
 let hasPassed (appointmentDate: DateTime): bool = appointmentDate.CompareTo DateTime.Now < 0
 
-let isAfternoonAppointment (appointmentDate: DateTime): bool =
-    failwith "Please implement the 'isAfternoonAppointment' function"
+let isAfternoonAppointment (appointmentDate: DateTime): bool = 12 <= appointmentDate.Hour && appointmentDate.Hour < 18
 
 let description (appointmentDate: DateTime): string = failwith "Please implement the 'description' function"
 
