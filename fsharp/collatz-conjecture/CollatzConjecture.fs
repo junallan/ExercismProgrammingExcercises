7050c48339c5711f72a70fff8f20cc73286cb054
Jun Allan Parreno <junallan@telus.net>
2023-02-18 17:38:43 -0700

    Simplified unfold solution

--- fsharp/collatz-conjecture/CollatzConjecture.fs
+++ fsharp/collatz-conjecture/CollatzConjecture.fs
@@ -1,33 +1,33 @@
 module CollatzConjecture
 
 let steps (number: int): int option =
-    let rec calculatedSteps n =
-        seq {
-            if n > 1 then
-                yield n
-                match n with
-                | n when n % 2 = 0 -> yield! calculatedSteps (n / 2) 
-                | _ -> yield! calculatedSteps (3 * n + 1) 
-        }
+    if number = 1 then
+        Some 0
+    else
+        number
+        |> Seq.unfold(fun number ->
+            match number with
+               | _ when number <= 1 -> None
+               | n when n % 2 = 0 -> Some (n, n / 2)
+               | _ -> Some (number, 3 * number + 1))
+        |> Seq.length
+        |> function
+        | 0 -> None
+        | n -> Some n
 
-    if number < 1 then None
-    else calculatedSteps number |> Seq.length |> Some
+//let steps (number: int): int option =
+//    let rec calculatedSteps n =
+//        seq {
+//            if n > 1 then
+//                yield n
+//                match n with
+//                | n when n % 2 = 0 -> yield! calculatedSteps (n / 2) 
+//                | _ -> yield! calculatedSteps (3 * n + 1) 
+//        }
 
-    //if number = 1 then
-    //    Some 0
-    //else
-    //    number |> Seq.unfold(fun number ->
-    //        match number with
-    //        | _ when number <= 1 -> None
-    //        | n when n % 2 = 0 -> Some (n, n / 2)
-    //        | _ -> Some (number, 3 * number + 1))
-    //    |> fun calculatedSteps ->
-    //        let numberOfSteps = Seq.length calculatedSteps
-    //        if numberOfSteps = 0 then
-    //            None 
-    //        else
-    //            Some numberOfSteps
-                         
+//    if number < 1 then None
+//    else calculatedSteps number |> Seq.length |> Some
+                  
     //let rec computeSteps number steps =
     //    match number with
     //    | _ when number <= 0 -> None
