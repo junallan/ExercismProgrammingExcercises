0028dc51af77945262d1fe0030bedebcf77c9331
Jun Allan Parreno <junallan@telus.net>
2023-02-18 18:34:50 -0700

    Handle full house scenario

--- csharp/yacht/Yacht.cs
+++ csharp/yacht/Yacht.cs
@@ -25,14 +25,32 @@ public static class YachtGame
         {
             YachtCategory.Yacht => YachtScore(dice),
             YachtCategory.Ones or YachtCategory.Twos or YachtCategory.Threes or YachtCategory.Fours or YachtCategory.Fives or YachtCategory.Sixes  => NumberScore(dice, category),
+            YachtCategory.FullHouse => FullHouseScore(dice),
             _ => 0
         };
 
         return result;
     }
 
-    private static int YachtScore(int[] dice)=> dice.Distinct().Count() == 1 ? 50 : 0;
+    private static int YachtScore(int[] dice) => dice.Distinct().Count() == 1 ? 50 : 0;
 
     private static int NumberScore(int[] dice, YachtCategory category) => dice.Where(diceNumber => diceNumber == (int)category).Sum();
+
+    private static int FullHouseScore(int[] dice)
+    {
+        var diceNumberVariations = dice.Distinct();
+
+        if (diceNumberVariations.Count() != 2) return 0;
+
+        var firstDistictNumberScore = diceNumberVariations.First();
+        var secondDistinctNumberScore = diceNumberVariations.Last();
+        var firstDistinctNumberScoreCount = dice.Count(diceNumber => diceNumber == firstDistictNumberScore);
+        var secondDistinctNumberScoreCount = dice.Count(diceNumber => diceNumber == secondDistinctNumberScore);
+
+        if ((firstDistinctNumberScoreCount == 2 && secondDistinctNumberScoreCount == 3) || (firstDistinctNumberScoreCount == 3 && secondDistinctNumberScoreCount == 5))
+            return firstDistictNumberScore * firstDistinctNumberScoreCount + secondDistinctNumberScore * secondDistinctNumberScoreCount;
+        else
+            return 0;
+    }
 }
 
