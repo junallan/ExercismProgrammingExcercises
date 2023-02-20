62c23958f74335dbcd207aaf7fef194a2ef6d926
Jun Allan Parreno <junallan@telus.net>
2023-02-20 11:30:54 -0700

    Implemented existingList function

---
+++ fsharp/tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.fs
@@ -0,0 +1,14 @@
+module TracksOnTracksOnTracks
+
+let newList: string list = []
+
+let existingList: string list = ["F#"; "Clojure"; "Haskell"]
+
+let addLanguage (language: string) (languages: string list): string list =
+    failwith "Please implement the 'addLanguage' function"
+
+let countLanguages (languages: string list): int = failwith "Please implement the 'countLanguages' function"
+
+let reverseList(languages: string list): string list = failwith "Please implement the 'reverseList' function"
+
+let excitingList (languages: string list): bool = failwith "Please implement the 'excitingList' function"
