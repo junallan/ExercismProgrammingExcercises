da40b5c0e98aad0ea066278a08645b88dd5da106
Jun Allan Parreno <junallan@telus.net>
2023-02-20 15:06:00 -0700

    Implemented list exciting logic

--- fsharp/tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.fs
+++ fsharp/tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.fs
@@ -10,4 +10,8 @@ let countLanguages (languages: string list): int = languages.Length
 
 let reverseList(languages: string list): string list = List.rev languages
 
-let excitingList (languages: string list): bool = failwith "Please implement the 'excitingList' function"
+let excitingList (languages: string list): bool =
+    match languages with
+    | l when l.Length > 0 && l.Head = "F#" -> true
+    | [_; "F#"; _] | [_; "F#";] -> true
+    | _ -> false
