510e322f4b2ef93e48c1074564fbe4f49c55ff0a
Jun Allan Parreno <junallan@telus.net>
2023-02-20 08:17:29 -0700

    Implemented last week method

---
+++ fsharp/bird-watcher/BirdWatcher.fs
@@ -0,0 +1,18 @@
+module BirdWatcher
+
+let lastWeek: int[] = [| 0; 2; 5; 3; 7; 8; 4 |]
+
+let yesterday(counts: int[]): int =
+  failwith "Please implement the 'yesterday' function"
+
+let total(counts: int[]): int =
+  failwith "Please implement the 'total' function"
+
+let dayWithoutBirds(counts: int[]): bool =
+  failwith "Please implement the 'dayWithoutBirds' function"
+
+let incrementTodaysCount(counts: int[]): int[] =
+ failwith "Please implement the 'incrementTodaysCount' function"
+
+let oddWeek(counts: int[]): bool =
+  failwith "Please implement the 'oddWeek' function"
