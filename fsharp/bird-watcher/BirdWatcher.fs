e0dc6486fe1d2914ae0405dee818f86546a055a2
Jun Allan Parreno <junallan@telus.net>
2023-02-20 08:33:56 -0700

    Implemented dayWithouBirds

--- fsharp/bird-watcher/BirdWatcher.fs
+++ fsharp/bird-watcher/BirdWatcher.fs
@@ -4,11 +4,9 @@ let lastWeek: int[] = [| 0; 2; 5; 3; 7; 8; 4 |]
 
 let yesterday(counts: int[]): int = counts.[counts.Length - 2]
 
-let total(counts: int[]): int =
-  failwith "Please implement the 'total' function"
+let total(counts: int[]): int = Array.sum counts
 
-let dayWithoutBirds(counts: int[]): bool =
-  failwith "Please implement the 'dayWithoutBirds' function"
+let dayWithoutBirds(counts: int[]): bool = counts |> Array.exists((=) 0)
 
 let incrementTodaysCount(counts: int[]): int[] =
  failwith "Please implement the 'incrementTodaysCount' function"
