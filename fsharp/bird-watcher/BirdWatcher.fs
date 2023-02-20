b3c3cb155ff11c7e92ac4d7da031e75c80090546
Jun Allan Parreno <junallan@telus.net>
2023-02-20 16:27:06 -0700

    Simplified oddWeek dayWithoutBirs and incrementTodaysCount functions

--- fsharp/bird-watcher/BirdWatcher.fs
+++ fsharp/bird-watcher/BirdWatcher.fs
@@ -6,16 +6,14 @@ let yesterday(counts: int[]): int = counts.[counts.Length - 2]
 
 let total(counts: int[]): int = Array.sum counts
 
-let dayWithoutBirds(counts: int[]): bool = counts |> Array.exists((=) 0)
-
+let dayWithoutBirds(counts: int[]): bool = counts |> Array.contains 0
 let incrementTodaysCount(counts: int[]): int[] =
-    let reversedCounts = Array.rev counts
-    let todaysCountIncremented = Array.head reversedCounts + 1
-    [| todaysCountIncremented; yield! Array.tail reversedCounts |] |> Array.rev
+    counts.[6] <- counts.[6] + 1
+    counts
 
 let oddWeek(counts: int[]): bool =
   match counts with
-  | [| _; 0; _; 0; _; 0; _; |] -> true
-  | [| _; 10; _; 10; _; 10; _; |] -> true
+  | [| _; 0; _; 0; _; 0; _; |] 
+  | [| _; 10; _; 10; _; 10; _; |] 
   | [| 5; _; 5; _; 5; _; 5; |] -> true
   | _ -> false
