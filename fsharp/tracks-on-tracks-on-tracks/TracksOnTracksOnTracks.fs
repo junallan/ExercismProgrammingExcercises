7ff7bad84e0a63d5c2537f2e5fb0cca6612fee7d
Jun Allan Parreno <junallan@telus.net>
2023-02-20 14:50:12 -0700

    Implemented countLanguages

--- fsharp/tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.fs
+++ fsharp/tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.fs
@@ -4,10 +4,9 @@ let newList: string list = []
 
 let existingList: string list = ["F#"; "Clojure"; "Haskell"]
 
-let addLanguage (language: string) (languages: string list): string list =
-    failwith "Please implement the 'addLanguage' function"
+let addLanguage (language: string) (languages: string list): string list = language :: languages
 
-let countLanguages (languages: string list): int = failwith "Please implement the 'countLanguages' function"
+let countLanguages (languages: string list): int = languages.Length
 
 let reverseList(languages: string list): string list = failwith "Please implement the 'reverseList' function"
 
