c22bd132295d05f749ec22d556ca8883a4ad847c
Jun Allan Parreno <junallan@telus.net>
2023-02-25 11:21:44 -0700

    Handle unecessary variable assignment

--- csharp/error-handling/ErrorHandling.cs
+++ csharp/error-handling/ErrorHandling.cs
@@ -4,15 +4,8 @@ public static class ErrorHandling
 {
     public static void HandleErrorByThrowingException() => throw new Exception();
     public static int? HandleErrorByReturningNullableType(string input) => Int32.TryParse(input, out int result) ? result : null;
-    public static bool HandleErrorWithOutParam(string input, out int result)
-    {
-        result = 0;
-        
-        var isSuccess = Int32.TryParse(input, out result);
+    public static bool HandleErrorWithOutParam(string input, out int result) => Int32.TryParse(input, out result);
        
-        return isSuccess;
-    }
-
     public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
     {
         disposableObject.Dispose();
