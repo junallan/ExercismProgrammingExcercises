0d1cac77ea2edb15e8289aa8d59c08dd737b2972
Jun Allan Parreno <junallan@telus.net>
2023-02-24 19:02:01 -0700

    Revised pattern match

--- fsharp/bandwagoner/Bandwagoner.fs
+++ fsharp/bandwagoner/Bandwagoner.fs
@@ -4,30 +4,19 @@ type Coach = { Name: string; FormerPlayer: bool; }
 type Stats = { Wins: int; Losses: int }
 type Team = { Name: string; Coach: Coach; Stats: Stats }
 
-let createCoach (name: string) (formerPlayer: bool): Coach =
-    let coach: Coach = { Name = name; FormerPlayer = formerPlayer }
-    coach
+let createCoach (name: string) (formerPlayer: bool): Coach = { Name = name; FormerPlayer = formerPlayer }
 
-let createStats(wins: int) (losses: int): Stats =
-   let stats: Stats = { Wins = wins; Losses = losses }
-   stats
+let createStats(wins: int) (losses: int): Stats = { Wins = wins; Losses = losses }
 
-let createTeam(name: string) (coach: Coach)(stats: Stats): Team =
-  let team: Team = { Name = name; Coach = coach; Stats = stats }
-  team
-
-let replaceCoach(team: Team) (coach: Coach): Team =
-   let team: Team = { team with Coach = coach }
-   team
-
-let isSameTeam(homeTeam: Team) (awayTeam: Team): bool =
-   homeTeam = awayTeam
+let createTeam(name: string) (coach: Coach)(stats: Stats): Team = { Name = name; Coach = coach; Stats = stats }
+  
+let replaceCoach(team: Team) (coach: Coach): Team = { team with Coach = coach }
+   
+let isSameTeam(homeTeam: Team) (awayTeam: Team): bool = homeTeam = awayTeam
 
 let rootForTeam(team: Team): bool =
    match team with
    | { Coach = {Name = "Gregg Popovich"} } -> true
    | { Coach = { FormerPlayer = true } } -> true
-   | { Name = "Chicago Bulls" } -> true
-   | _ when team.Stats.Wins >= 60 -> true
-   | _ when team.Stats.Wins < team.Stats.Losses -> true
-   | _ -> false
+   | { Stats = { Wins = wins; Losses = losses }} -> wins >= 60 || wins < losses
+   
