e42bff4f0407a5b5ced383248a2585397d2338ce
Jun Allan Parreno <junallan@telus.net>
2023-02-24 19:15:01 -0700

    Implemented getCoordinate function

---
+++ fsharp/tisbury-treasure-hunt/TisburyTreasureHunt.fs
@@ -0,0 +1,12 @@
+module TisburyTreasureHunt
+
+let getCoordinate (line: string * string): string = snd line
+
+let convertCoordinate (coordinate: string): int * char = 
+    failwith "Please implement the 'convertCoordinate' function"
+
+let compareRecords (azarasData: string * string) (ruisData: string * (int * char) * string) : bool = 
+    failwith "Please implement the 'compareRecords' function"
+
+let createRecord (azarasData: string * string) (ruisData: string * (int * char) * string) : (string * string * string * string) =
+    failwith "Please implement the 'createRecord' function"
