8a69dae73e83a582fb560b2213b3f30912f83888
Jun Allan Parreno <junallan@telus.net>
2023-02-25 11:00:44 -0700

    Largest and Smallest functions implemented with first two testst passing

---
+++ csharp/palindrome-products/PalindromeProducts.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class PalindromeProducts
+{
+    public static (int, IEnumerable<(int,int)>) Largest(int minFactor, int maxFactor)
+    {
+        int? largestPalindromeProduct = null;
+        var palindromeProducts = new List<(int FirstNumber, int SecondNumber)>();
+
+        for (int i = maxFactor; i >= minFactor; i--)
+        {
+            for (int j = maxFactor; j >= minFactor; j--)
+            {
+                if (IsPalindromProduct(j, i))
+                {
+                    if (!largestPalindromeProduct.HasValue) largestPalindromeProduct = i * j;
+                    else if (largestPalindromeProduct.Value != i * j) continue;
+
+                    if (!palindromeProducts.Any(n => (n.FirstNumber == i && n.SecondNumber == j) || (n.FirstNumber == j && n.SecondNumber == i)))
+                        palindromeProducts.Add((j, i));
+                }
+            }
+        }
+
+        return (largestPalindromeProduct.Value, palindromeProducts);
+    }
+
+    public static (int, IEnumerable<(int,int)>) Smallest(int minFactor, int maxFactor)
+    {
+        int? smallestPalindromeProduct = null;
+        var palindromeProducts = new List<(int, int)>();
+
+        for (int i = minFactor; i <= maxFactor; i++)
+        {
+            for (int j = i; j <= maxFactor; j++)
+            {
+                if (IsPalindromProduct(i, j))
+                {
+                    if (smallestPalindromeProduct.HasValue && smallestPalindromeProduct.Value != i * j) return (smallestPalindromeProduct.Value, palindromeProducts);
+
+                    smallestPalindromeProduct = i * j;
+                    maxFactor = smallestPalindromeProduct.Value;
+                    palindromeProducts.Add((i, j));
+                }
+            }
+        }
+
+        return (smallestPalindromeProduct.Value, palindromeProducts);
+    }
+
+    private static bool IsPalindromProduct(int firstNumber, int secondNumber)
+    {
+        var product = firstNumber * secondNumber;
+        var parsedProduct = product.ToString().ToCharArray();
+        Array.Reverse(parsedProduct);
+
+        return product.ToString() == new String(parsedProduct);    
+    }
+}
