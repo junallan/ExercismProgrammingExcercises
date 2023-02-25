426fadf6e35e556d7a7257d0949588b408f5e9e8
Jun Allan Parreno <junallan@telus.net>
2023-02-25 09:15:52 -0700

    Simplified convertCoordinate function

--- fsharp/tisbury-treasure-hunt/TisburyTreasureHunt.fs
+++ fsharp/tisbury-treasure-hunt/TisburyTreasureHunt.fs
@@ -3,11 +3,7 @@ module TisburyTreasureHunt
 let getCoordinate (line: string * string): string = snd line
 
 let convertCoordinate (coordinate: string): int * char =
-    let coordinateParsed = Seq.toList coordinate
-    let coordinatesLastIndex = List.length coordinateParsed - 1
-    let letterCoordinate = coordinateParsed.[coordinatesLastIndex]
-    let numberCoordinate = List.take coordinatesLastIndex coordinateParsed
-    (int (string numberCoordinate.[0]),letterCoordinate)
+    string coordinate.[0] |> int, coordinate.[1]
 
 let compareRecords (azarasData: string * string) (ruisData: string * (int * char) * string) : bool = 
     let (_, coordinatesAzara) = azarasData
