41144703a3da75c047c4c6228f2509e74463b163
Jun Allan Parreno <junallan@telus.net>
2023-02-24 19:46:06 -0700

    Implemented convertCoordinate

--- fsharp/tisbury-treasure-hunt/TisburyTreasureHunt.fs
+++ fsharp/tisbury-treasure-hunt/TisburyTreasureHunt.fs
@@ -2,8 +2,14 @@ module TisburyTreasureHunt
 
 let getCoordinate (line: string * string): string = snd line
 
-let convertCoordinate (coordinate: string): int * char = 
-    failwith "Please implement the 'convertCoordinate' function"
+let convertCoordinate (coordinate: string): int * char =
+    let coordinateParsed = Seq.toList coordinate
+    let coordinatesLastIndex = List.length coordinateParsed - 1
+    let letterCoordinate = coordinateParsed.[coordinatesLastIndex]
+    let numberCoordinate = List.take coordinatesLastIndex coordinateParsed
+    (int (string numberCoordinate.[0]),letterCoordinate)
+
+    
 
 let compareRecords (azarasData: string * string) (ruisData: string * (int * char) * string) : bool = 
     failwith "Please implement the 'compareRecords' function"
