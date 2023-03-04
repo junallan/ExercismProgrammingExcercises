e41cb42655c399b9e40f0b48f2b5bcaebdad3bbe
Jun Allan Parreno <junallan@telus.net>
2023-03-04 08:21:15 -0700

    Format code with go fmt

--- go/cars-assemble/cars_assemble.go
+++ go/cars-assemble/cars_assemble.go
@@ -1,28 +1,28 @@
 package cars
 
 const (
-	NumberOfMinutesInHour = 60
+	NumberOfMinutesInHour   = 60
 	CarCountForGroupPackage = 10
-	PackageCarCost = 95000
-	IndividualCarCost = 10000
+	PackageCarCost          = 9_5000
+	IndividualCarCost       = 10_000
 )
 
 // CalculateWorkingCarsPerHour calculates how many working cars are
 // produced by the assembly line every hour.
 func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
-	return float64 (productionRate) * (successRate / float64 (100));
+	return float64(productionRate) * (successRate / float64(100))
 }
 
 // CalculateWorkingCarsPerMinute calculates how many working cars are
 // produced by the assembly line every minute.
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
-	return int(CalculateWorkingCarsPerHour(productionRate, successRate) / NumberOfMinutesInHour);
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate) / NumberOfMinutesInHour)
 }
 
 // CalculateCost works out the cost of producing the given number of cars.
 func CalculateCost(carsCount int) uint {
-	groupPackageCount := uint (carsCount / CarCountForGroupPackage);
-	individualCarCount := uint (carsCount % CarCountForGroupPackage);
+	groupPackageCount := uint(carsCount / CarCountForGroupPackage)
+	individualCarCount := uint(carsCount % CarCountForGroupPackage)
 
-	return uint(groupPackageCount) * PackageCarCost + (individualCarCount * IndividualCarCost);
+	return uint(groupPackageCount)*PackageCarCost + (individualCarCount * IndividualCarCost)
 }
