9076abb9d3f24605a2f52d87bff6c0e76291c52e
Jun Allan Parreno <junallan@telus.net>
2023-03-07 23:09:45 -0700

    Simplified Largest function utilizing utitlity function GeneratePairs

--- csharp/palindrome-products/PalindromeProducts.cs
+++ csharp/palindrome-products/PalindromeProducts.cs
@@ -9,27 +9,30 @@ public static class PalindromeProducts
         var largestPalindromeProduct = int.MinValue;
         var palindromeProducts = new List<(int FirstNumber, int SecondNumber)>();
 
-        for (int i = maxFactor; i >= minFactor; i--)
+        //for (int i = maxFactor; i >= minFactor; i--)
+        //{
+        //    for (int j = maxFactor; j >= minFactor; j--)
+        //    {
+        foreach (var (i, j) in GeneratePairs(minFactor, maxFactor))
         {
-            for (int j = maxFactor; j >= minFactor; j--)
+            if (!IsPalindrome(j * i)) continue;
+
+            var product = i * j;
+
+            if (largestPalindromeProduct < product)
             {
-                if (!IsPalindrome(j * i)) continue;
-
-                var product = i * j;
-
-                if (largestPalindromeProduct < product)
-                { 
-                    largestPalindromeProduct = product;
-                    palindromeProducts.Clear();
+                largestPalindromeProduct = product;
+                palindromeProducts.Clear();
+                palindromeProducts.Add((i, j));
+            }
+            else if (largestPalindromeProduct == product)
+            {
+                if (!palindromeProducts.Any(n => (n.FirstNumber == i && n.SecondNumber == j) || (n.FirstNumber == j && n.SecondNumber == i)))
                     palindromeProducts.Add((j, i));
-                }
-                else if (largestPalindromeProduct == product)
-                {
-                    if (!palindromeProducts.Any(n => (n.FirstNumber == i && n.SecondNumber == j) || (n.FirstNumber == j && n.SecondNumber == i)))
-                        palindromeProducts.Add((j, i));
-                }
             }
         }
+        //    }
+        //}
 
         if (largestPalindromeProduct == int.MinValue) throw new ArgumentException();
 
@@ -41,20 +44,17 @@ public static class PalindromeProducts
         var smallestPalindromeProduct = int.MaxValue;
         var palindromeProducts = new List<(int FirstNumber, int SecondNumber)>();
 
-        for (int i = minFactor; i <= maxFactor; i++)
+        foreach (var (i, j) in GeneratePairs(minFactor, maxFactor))
         {
-            for (int j = i; j <= maxFactor; j++)
-            {
-                if (!IsPalindrome( i * j)) continue;
+            if (!IsPalindrome(i * j)) continue;
 
-                var product = i * j;
+            var product = i * j;
 
-                if (smallestPalindromeProduct < product) return (smallestPalindromeProduct, palindromeProducts);
-                if (smallestPalindromeProduct == int.MaxValue) smallestPalindromeProduct = product;
-   
-                if (!palindromeProducts.Any(n => (n.FirstNumber == i && n.SecondNumber == j) || (n.FirstNumber == j && n.SecondNumber == i)))
-                    palindromeProducts.Add((j, i));           
-            }
+            if (smallestPalindromeProduct < product) return (smallestPalindromeProduct, palindromeProducts);
+            if (smallestPalindromeProduct == int.MaxValue) smallestPalindromeProduct = product;
+
+            if (!palindromeProducts.Any(n => (n.FirstNumber == i && n.SecondNumber == j) || (n.FirstNumber == j && n.SecondNumber == i)))
+                palindromeProducts.Add((j, i));
         }
 
         if (smallestPalindromeProduct == int.MaxValue) throw new ArgumentException();
@@ -65,5 +65,16 @@ public static class PalindromeProducts
     {
         var numberFormatted = number.ToString();
         return numberFormatted == new String(numberFormatted.Reverse().ToArray());
+    }
+
+    private static IEnumerable<(int, int)> GeneratePairs(int minFactor, int maxFactor)
+    {
+        for (int i = minFactor; i <= maxFactor; i++)
+        {
+            for (int j = i; j <= maxFactor; j++)
+            {
+                yield return (i, j);
+            }
+        }
     }
 }
