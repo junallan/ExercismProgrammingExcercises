b24736773de0f3a4dcf3df57c1b324844c9c7a3f
Jun Allan Parreno <junallan@telus.net>
2023-03-11 16:22:01 -0700

    Palindrome products variation methods with benchmarking

---
+++ csharp/PalindromeProductsBenchMarking/PalindromeProductsBenchMarking.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace PalindromeProductsBenchMarking
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class PalindromeProductsBenchMarking
+    {
+        //[Benchmark]
+        //public void MeasureSmallest()
+        //{
+        //    PalindromeProducts.Smallest(100, 999);
+        //}
+
+        //[Benchmark]
+        //public void MeasureSmallestBetweenSquareRange()
+        //{
+        //    PalindromeProducts.SmallestVariationBetweenSquareRange(100, 999);
+        //}
+
+        [Benchmark]
+        public void MeasureLargest()
+        {
+            PalindromeProducts.Largest(100, 999);
+        }
+
+        [Benchmark]
+        public void MeasureLargestBetweenSquareRangeV1()
+        {
+           PalindromeProducts.LargestVariationBetweenSquareRangeV1(100, 999);
+        }
+
+        [Benchmark]
+        public void MeasureLargestBetweenSquareRangeV2()
+        {
+            PalindromeProducts.LargestVariationBetweenSquareRangeV2(100, 999);
+        }
+    }
+}
--- csharp/palindrome-products/PalindromeProducts.cs
+++ csharp/palindrome-products/PalindromeProducts.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 public static class PalindromeProducts
 {
-    public static (int, IEnumerable<(int,int)>) Largest(int minFactor, int maxFactor)
+    public static (int, IEnumerable<(int, int)>) Largest(int minFactor, int maxFactor)
     {
         var largestPalindromeProduct = int.MinValue;
         var palindromeProducts = new List<(int FirstNumber, int SecondNumber)>();
@@ -33,26 +33,160 @@ public static class PalindromeProducts
         return (largestPalindromeProduct, palindromeProducts);
     }
 
-    public static (int, IEnumerable<(int,int)>) Smallest(int minFactor, int maxFactor)
+    public static (int, IEnumerable<(int, int)>) LargestVariationBetweenSquareRangeV1(int minFactor, int maxFactor)
+    //public static (int, IEnumerable<(int, int)>) Largest(int minFactor, int maxFactor)
     {
-        var smallestPalindromeProduct = int.MaxValue;
+        if (minFactor > maxFactor) throw new ArgumentException();
+
+        var smallestProduct = minFactor * minFactor;
+
+        var largestProduct = maxFactor * maxFactor;
+
         var palindromeProducts = new List<(int FirstNumber, int SecondNumber)>();
+        var palindromesInRange = PalindromesInRange(smallestProduct, largestProduct);
 
-        foreach (var (i, j) in GeneratePairs(minFactor, maxFactor))
-        {
-            var product = i * j;
+        int currentPalindrome = int.MinValue;
 
-            if (!IsPalindrome(product)) continue;
+        foreach (var palindrome in palindromesInRange)
+        { 
+            for (int i = maxFactor; minFactor <= i; i--)
+            {
+                var remainder = palindrome / i;
+                var carryover = palindrome % i;
 
-            if (smallestPalindromeProduct < product) return (smallestPalindromeProduct, palindromeProducts);
-            if (smallestPalindromeProduct == int.MaxValue) smallestPalindromeProduct = product;
+                if (currentPalindrome > remainder * i) continue;
 
-            if (!palindromeProducts.Any(n => (n.FirstNumber == i && n.SecondNumber == j) || (n.FirstNumber == j && n.SecondNumber == i)))
-                palindromeProducts.Add((j, i));
+                if ((minFactor <= remainder && remainder <= maxFactor) && carryover == 0)
+                {
+                    if (currentPalindrome != palindrome) palindromeProducts.Clear();
+                    currentPalindrome = palindrome;
+
+                    if (palindromeProducts.Contains((i, remainder))) continue;
+
+                    palindromeProducts.Add((remainder, i));
+                }
+            }
+
+            //if (palindromeProducts.Any()) break;
         }
 
-        if (smallestPalindromeProduct == int.MaxValue) throw new ArgumentException();
-        return (smallestPalindromeProduct, palindromeProducts);
+        if (!palindromeProducts.Any()) throw new ArgumentException();
+
+        return (currentPalindrome, palindromeProducts);
+    }
+
+    public static (int, IEnumerable<(int, int)>) LargestVariationBetweenSquareRangeV2(int minFactor, int maxFactor)
+    //public static (int, IEnumerable<(int, int)>) Largest(int minFactor, int maxFactor)
+    {
+        if (minFactor > maxFactor) throw new ArgumentException();
+
+        var smallestProduct = minFactor * minFactor;
+
+        var largestProduct = maxFactor * maxFactor;
+
+        var palindromeProducts = new List<(int FirstNumber, int SecondNumber)>();
+        var palindromesInRange = PalindromesInRange(smallestProduct, largestProduct).OrderByDescending(p => p);
+
+        int currentPalindrome = int.MinValue;
+
+        foreach (var palindrome in palindromesInRange)
+        {
+            var lowerFactorCurrent = minFactor;
+           
+            for (int i = maxFactor; lowerFactorCurrent <= i; i--)
+            {
+                var remainder = palindrome / i;
+                var carryover = palindrome % i;
+
+                if (currentPalindrome > remainder * i) continue;
+
+                if ((minFactor <= remainder && remainder <= maxFactor) && carryover == 0)
+                {
+                    currentPalindrome = palindrome;
+
+                    if (palindromeProducts.Contains((i, remainder))) break;
+
+                    palindromeProducts.Add((remainder, i));
+                }
+
+                if(lowerFactorCurrent == i)
+                {
+                    lowerFactorCurrent++;
+                    i = maxFactor;
+                }
+            }
+
+            if (palindromeProducts.Any()) break;
+        }
+
+        if (!palindromeProducts.Any()) throw new ArgumentException();
+
+        return (currentPalindrome, palindromeProducts);
+    }
+
+    //public static (int, IEnumerable<(int,int)>) Smallest(int minFactor, int maxFactor)
+    //{
+    //    var smallestPalindromeProduct = int.MaxValue;
+    //    var palindromeProducts = new List<(int FirstNumber, int SecondNumber)>();
+
+    //    foreach (var (i, j) in GeneratePairs(minFactor, maxFactor))
+    //    {
+    //        var product = i * j;
+
+    //        if (!IsPalindrome(product)) continue;
+
+    //        if (smallestPalindromeProduct < product) return (smallestPalindromeProduct, palindromeProducts);
+    //        if (smallestPalindromeProduct == int.MaxValue) smallestPalindromeProduct = product;
+
+    //        if (!palindromeProducts.Any(n => (n.FirstNumber == i && n.SecondNumber == j) || (n.FirstNumber == j && n.SecondNumber == i)))
+    //            palindromeProducts.Add((j, i));
+    //    }
+
+    //    if (smallestPalindromeProduct == int.MaxValue) throw new ArgumentException();
+    //    return (smallestPalindromeProduct, palindromeProducts);
+    //}
+
+    //public static (int, IEnumerable<(int,int)>) SmallestVariationBetweenSquareRange(int minFactor, int maxFactor)
+    public static (int, IEnumerable<(int, int)>) Smallest(int minFactor, int maxFactor)
+    {
+        if (minFactor > maxFactor) throw new ArgumentException();
+      
+        var smallestProduct = minFactor * minFactor;
+        
+        var largestProduct = maxFactor * maxFactor;
+        
+        var palindromeProducts = new List<(int FirstNumber, int SecondNumber)>();
+        var palindromesInRange = PalindromesInRange(smallestProduct, largestProduct);
+        
+        int currentPalindrome = int.MaxValue;
+        
+        foreach (var palindrome in palindromesInRange)
+        {
+            for (int i = minFactor; i <= maxFactor; i++)
+            {
+                var remainder = palindrome / i;
+                var carryover = palindrome % i;
+        
+                if (currentPalindrome < remainder * i) break;
+        
+                if ((minFactor <= remainder && remainder <= maxFactor) && carryover == 0)
+                {
+                    currentPalindrome = palindrome;
+                    palindromeProducts.Add((remainder, i));
+                }
+            }
+        
+            if (palindromeProducts.Any()) break;
+        }
+        
+        if (!palindromeProducts.Any()) throw new ArgumentException();
+        
+        return (currentPalindrome, palindromeProducts);
+    }
+
+    private static IEnumerable<int> PalindromesInRange(int lowerRange, int upperRange)
+    {
+        for (int i = lowerRange; i <= upperRange; i++) if (IsPalindrome(i)) yield return i;
     }
 
     private static bool IsPalindrome(int number)
