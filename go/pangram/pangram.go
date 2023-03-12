6c1356d28122f040280aa40f60247da316ad3de0
Jun Allan Parreno <junallan@telus.net>
2023-03-11 19:24:53 -0700

    Bit shift solution variation

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -6,17 +6,35 @@ import (
 )
 
 func IsPangram(input string) bool {
-	alphabet := "abcdefghijklmnopqrstuvwxyz"
-	inputLower := strings.ToLower(input)
+	const alphabetMask int = 0b11111111111111111111111111
+	inputMask := 0
+	length := len(input)
 
-	for _, char := range alphabet {
-		if !strings.ContainsRune(inputLower, char) {
-			return false
+	for i := 0; i < length; i++ {
+		asciiChar := input[i]
+
+		if 65 <= asciiChar && asciiChar <= 90 {
+			inputMask |= 1 << (asciiChar - 65)
+		} else if 97 <= asciiChar && asciiChar <= 122 {
+			inputMask |= 1 << (asciiChar - 97)
 		}
 	}
 
-	return true
+	return alphabetMask == inputMask
 }
+
+// func IsPangramStringRuneComparisonVariation(input string) bool {
+// 	alphabet := "abcdefghijklmnopqrstuvwxyz"
+// 	inputLower := strings.ToLower(input)
+
+// 	for _, char := range alphabet {
+// 		if !strings.ContainsRune(inputLower, char) {
+// 			return false
+// 		}
+// 	}
+
+// 	return true
+// }
 
 func IsPangramSortAndCountLettersVariation(input string) bool {
 	parsedInputToSortedOrder := strings.Split(strings.ToLower(input), "")
