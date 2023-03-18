77db47dd88df860ce0d69fe9f3f7720eb8c6f540
Jun Allan Parreno <junallan@telus.net>
2023-03-18 10:03:25 -0600

    Linked list initialize implemented

---
+++ go/linked-list/linked_list.go
@@ -0,0 +1,71 @@
+package linkedlist
+
+// Define List and Node types here.
+// Note: The tests expect Node type to include an exported field with name Value to pass.
+type List struct {
+	first *Node
+	last  *Node
+}
+
+type Node struct {
+	prev  *Node
+	next  *Node
+	Value interface{}
+}
+
+func NewList(args ...interface{}) *List {
+	list := &List{}
+	// items := List{head: Node{value: 1}, tail: Node{value: 2}}
+
+	for _, item := range args {
+		list.Push(item)
+	}
+
+	return list
+}
+
+func (n *Node) Next() *Node {
+	return n.next
+}
+
+func (n *Node) Prev() *Node {
+	return n.prev
+}
+
+func (l *List) Unshift(v interface{}) {
+	panic("Please implement the Unshift function")
+}
+
+func (l *List) Push(v interface{}) {
+	node := &Node{Value: v}
+
+	if l.first == nil {
+		l.first = node
+		l.last = node
+	} else {
+		previousTail := l.last
+		l.last = node
+		previousTail.next = l.last
+		l.last.prev = previousTail
+	}
+}
+
+func (l *List) Shift() (interface{}, error) {
+	panic("Please implement the Shift function")
+}
+
+func (l *List) Pop() (interface{}, error) {
+	panic("Please implement the Pop function")
+}
+
+func (l *List) Reverse() {
+	panic("Please implement the Reverse function")
+}
+
+func (l *List) First() *Node {
+	return l.first
+}
+
+func (l *List) Last() *Node {
+	return l.last
+}
