f50a4279c50a46ca43599b9e1773cf7ba654d3a3
junallan <junallan@telus.net>
2023-04-07 12:52:49 -0600

    Added error handling on square function

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,9 @@
 grain_count = 0;
 
 def square(number):
+    if number < 1 or 64 < number:
+        raise ValueError("square must be between 1 and 64")
+    
     if number == 1:
         return 1
     else:
