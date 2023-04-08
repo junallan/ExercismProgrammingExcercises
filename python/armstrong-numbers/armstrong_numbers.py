6b4af6ce4ca8d9b0b22ecddf39636032bc5690a5
junallan <junallan@telus.net>
2023-04-07 22:38:56 -0600

    Remove unecessary variable assignment

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,10 +1,9 @@
 def is_armstrong_number(number):
     number_as_string = str(number)
+    
     length = len(number_as_string)
     
-    accumulated_total = sum(
-        int(digit) ** length
-        for digit in number_as_string
+    return number == sum(
+       int(digit) ** length
+       for digit in number_as_string 
     )
-
-    return number == accumulated_total
