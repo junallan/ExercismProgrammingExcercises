cfe202457754b7c5dd85b5a9686d2d89a7e3fe6a
junallan <junallan@telus.net>
2023-04-08 12:51:38 -0600

    Fix bug of only capitalizing first word of sentence instead of every word

--- python/little-sisters-essay/string_methods.py
+++ python/little-sisters-essay/string_methods.py
@@ -8,7 +8,7 @@ def capitalize_title(title):
     :return: str - title string in title case (first letters capitalized).
     """
 
-    return title.capitalize()
+    return title.title()
 
 
 def check_sentence_ending(sentence):
