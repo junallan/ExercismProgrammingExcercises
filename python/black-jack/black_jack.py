f867588444a23d9cdfa08381614688aeb00b1119
junallan <junallan@telus.net>
2023-04-08 11:33:16 -0600

    Implemented can_double_down

--- python/black-jack/black_jack.py
+++ python/black-jack/black_jack.py
@@ -112,4 +112,4 @@ def can_double_down(card_one, card_two):
     :return: bool - can the hand can be doubled down? (i.e. totals 9, 10 or 11 points).
     """
 
-    pass
+    return 9 <= (value_of_card(card_one) + value_of_card(card_two)) <= 11
