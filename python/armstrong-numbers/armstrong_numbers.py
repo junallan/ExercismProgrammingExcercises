07c68483aa8a66169a8692dfbd1b5639db256d98
junallan <junallan@telus.net>
2023-04-07 22:36:53 -0600

    List comprehension instead of look for calculating total

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -2,9 +2,9 @@ def is_armstrong_number(number):
     number_as_string = str(number)
     length = len(number_as_string)
     
-    accumulated_total = 0
-
-    for digit in number_as_string:
-        accumulated_total += int(digit) ** length
+    accumulated_total = sum(
+        int(digit) ** length
+        for digit in number_as_string
+    )
 
     return number == accumulated_total
