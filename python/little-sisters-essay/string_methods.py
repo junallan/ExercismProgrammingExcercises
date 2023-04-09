50a45976331216749da95b9779b5e66812787aa5
junallan <junallan@telus.net>
2023-04-08 18:40:52 -0600

    Fix bug in replace_word_choice to determine if a full word instead of replacing substring of a word

--- python/little-sisters-essay/string_methods.py
+++ python/little-sisters-essay/string_methods.py
@@ -1,3 +1,5 @@
+import string
+
 """Functions to help edit essay homework using string manipulation."""
 
 
@@ -40,4 +42,9 @@ def replace_word_choice(sentence, old_word, new_word):
     :return: str - input sentence with new words in place of old words.
     """
 
-    return sentence.replace(old_word, new_word)
+    return (" ".join(
+                word if word != old_word else new_word 
+                for word 
+                in sentence.strip(string.punctuation)
+                            .split()) + "."
+            )
