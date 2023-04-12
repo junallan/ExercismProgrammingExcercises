93d2925a9631fa33368681dab3960a424f3aa48f
junallan <junallan@telus.net>
2023-04-11 20:57:15 -0600

    Implemented square_of_sum

---
+++ python/difference-of-squares/difference_of_squares.py
@@ -0,0 +1,15 @@
+def square_of_sum(number):
+    sum = 0
+
+    for i in range(number):
+        sum += i + 1
+    
+    return sum ** 2
+
+
+def sum_of_squares(number):
+    pass
+
+
+def difference_of_squares(number):
+    pass
