eda8d2d561e52df647c4593ff1055bdd13b8d8d7
junallan <junallan@telus.net>
2023-04-11 21:00:48 -0600

    Explicitly specify types

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,4 +1,4 @@
-def square_of_sum(number):
+def square_of_sum(number: int) -> int:
     sum = 0
 
     for i in range(number):
@@ -7,7 +7,7 @@ def square_of_sum(number):
     return sum ** 2
 
 
-def sum_of_squares(number):
+def sum_of_squares(number: int) -> int:
     sum = 0
 
     for i in range(number):
@@ -16,5 +16,5 @@ def sum_of_squares(number):
     return sum
 
 
-def difference_of_squares(number):
+def difference_of_squares(number: int) -> int:
     return square_of_sum(number) - sum_of_squares(number)
