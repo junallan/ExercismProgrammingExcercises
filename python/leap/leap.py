b505a3e34355de187e6099e8980976bfebf1a9eb
junallan <junallan@telus.net>
2023-04-13 22:58:51 -0600

    Condensed one conditional

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -2,11 +2,8 @@ def leap_year(year: int) -> bool:
     if year % 4 != 0:
         return False
 
-    if year % 100 != 0:
+    if year % 100 != 0 or year % 400 == 0:
         return True
-
-    if year % 400 == 0:    
-        return True     
    
     return False
 
