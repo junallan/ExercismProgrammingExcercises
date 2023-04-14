936611ab402daacca05c75cddedcb522e3c4d095
junallan <junallan@telus.net>
2023-04-13 23:26:42 -0600

    extract has_0_side utility function

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,12 +1,12 @@
 def equilateral(sides: list) -> bool:
-    if 0 in sides:
+    if has_0_side(sides):
         return False
     
     return sides[0] == sides[1] == sides[2]
 
 
 def isosceles(sides: list) -> bool:
-    if 0 in sides:
+    if has_0_side(sides):
         return False
     
     distinct_side_count = len(set(sides))
@@ -17,10 +17,19 @@ def isosceles(sides: list) -> bool:
 def scalene(sides: list) -> bool:
     pass
 
+
+def has_0_side(sides: list) -> bool:
+    return 0 in sides
+
+
 def is_triangle(sides: list) -> bool:
     return (two_sides_greater_than_one(sides[0], sides[1], sides[2]) 
             and two_sides_greater_than_one(sides[0], sides[2], sides[1])
             and two_sides_greater_than_one(sides[1], sides[2], sides[0]))
 
-def two_sides_greater_than_one(side1: int | float, side2: int | float, side3: int | float ) -> bool:
+
+def two_sides_greater_than_one(
+        side1: int | float, 
+        side2: int | float, 
+        side3: int | float ) -> bool:
     return side1 + side2 >= side3
