e26ad6c3d395e943b6e68c65f3c7720ed70085b0
junallan <junallan@telus.net>
2023-04-13 23:02:47 -0600

    Condensed logic to a single statment

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,9 +1,4 @@
 def leap_year(year: int) -> bool:
-    if year % 4 != 0:
-        return False
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
-    if year % 100 != 0 or year % 400 == 0:
-        return True
-   
-    return False
 
