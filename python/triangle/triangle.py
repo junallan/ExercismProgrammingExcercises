dc7df7ec0ddec0782ac89a33cb3611bcef2e6cd7
junallan <junallan@telus.net>
2023-04-13 23:12:11 -0600

    Implemented equilateral function

---
+++ python/triangle/triangle.py
@@ -0,0 +1,13 @@
+def equilateral(sides):
+    if 0 in sides:
+        return False
+    
+    return sides[0] == sides[1] == sides[2]
+
+
+def isosceles(sides):
+    pass
+
+
+def scalene(sides):
+    pass
