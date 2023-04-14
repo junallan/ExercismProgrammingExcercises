c8589df0b342ab8dd1fa696e04a92bae408aa8e1
junallan <junallan@telus.net>
2023-04-13 21:20:26 -0600

    Removed one nested conditional

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,11 +1,12 @@
 def leap_year(year: int) -> bool:
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:    
-                return True     
-            else:
-                return False
+    if year % 4 != 0:
+        return False
+
+    if year % 100 == 0:
+        if year % 400 == 0:    
+            return True     
         else:
-            return True
+            return False
+    else:
+        return True
     
-    return False
