dd5071539794bce8a63d8d88fadc6844550d667e
junallan <junallan@telus.net>
2023-04-15 10:20:25 -0600

    Revised is_triangle utility function to simplify code

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,20 +1,18 @@
 def equilateral(sides: list) -> bool: 
-    return not has_0_side(sides) and sides[0] == sides[1] == sides[2]
+    return is_triangle(sides) and sides[0] == sides[1] == sides[2]
 
 
 def isosceles(sides: list) -> bool: 
-    return not has_0_side(sides) and (1 <= len(set(sides)) <= 2) and is_triangle(sides)
+    return is_triangle(sides) and (1 <= len(set(sides)) <= 2) and is_triangle(sides)
 
 
 def scalene(sides: list) -> bool:  
-    return not has_0_side(sides) and len(set(sides)) == 3 and is_triangle(sides)
-    
-
-def has_0_side(sides: list) -> bool:
-    return 0 in sides
+    return is_triangle(sides) and len(set(sides)) == 3 and is_triangle(sides)
 
 
 def is_triangle(sides: list) -> bool:
-    return ((sides[0] + sides[1] >= sides[2]) 
+    return (not 0 in sides
+            and len(sides) == 3
+            and (sides[0] + sides[1] >= sides[2]) 
             and (sides[0] + sides[2] >= sides[1])
             and (sides[1] + sides[2] >= sides[0]))
