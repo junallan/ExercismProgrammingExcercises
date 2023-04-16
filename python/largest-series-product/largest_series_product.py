2f2dc6eafb0afc20848e182344b109cbac3cee7f
junallan <junallan@telus.net>
2023-04-15 19:31:45 -0600

    Handle non digit case

--- python/largest-series-product/largest_series_product.py
+++ python/largest-series-product/largest_series_product.py
@@ -3,6 +3,11 @@ import functools
 def largest_product(series: str, size: int) -> int:
     if len(series) < size:
         raise ValueError("span must be smaller than string length")
+    
+    if not series.isdigit():
+        raise ValueError("digits input must only contain digits");
+
+
     if len(series) == size:
         digits = [int(number) for number in series] 
         product = functools.reduce(lambda d1, d2: d1 * d2, digits)
