967500d3e9f02fbefed23d39f4b081083129f2ed
junallan <junallan@telus.net>
2023-04-15 19:27:19 -0600

    Handle error condition when span is larger than string length

--- python/largest-series-product/largest_series_product.py
+++ python/largest-series-product/largest_series_product.py
@@ -1,6 +1,8 @@
 import functools
 
 def largest_product(series: str, size: int) -> int:
+    if len(series) < size:
+        raise ValueError("span must be smaller than string length")
     if len(series) == size:
         digits = [int(number) for number in series] 
         product = functools.reduce(lambda d1, d2: d1 * d2, digits)
