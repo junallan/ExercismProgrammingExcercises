90253dff157182bbe6ed577a405d073d165a9332
junallan <junallan@telus.net>
2023-04-16 22:37:31 -0600

    Handle full house case

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -7,7 +7,7 @@ THREES = 3
 FOURS = 4
 FIVES = 5
 SIXES = 6
-FULL_HOUSE = None
+FULL_HOUSE = 7
 FOUR_OF_A_KIND = None
 LITTLE_STRAIGHT = None
 BIG_STRAIGHT = None
@@ -22,4 +22,18 @@ def score(dice, category):
             return 0
     elif ONES <= category <= SIXES:
         return dice.count(category) * category     
+    elif category == FULL_HOUSE:
+        distinct_dices = list(set(dice))
+
+        if len(distinct_dices) != 2:
+            return 0
+        else:
+            count_first_number = dice.count(distinct_dices[0])
+            count_second_number = dice.count(distinct_dices[1])
+
+            if ((count_first_number == 3 and count_second_number == 2) or
+                (count_first_number == 2 and count_second_number == 3)):
+                return sum(dice)
+            else:
+                return 0
     pass
