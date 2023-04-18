6a8c1a7acf426f9e61480600ca7c32cfea664e02
junallan <junallan@telus.net>
2023-04-17 21:18:06 -0600

    Fixed square bug

--- python/square-root/square_root.py
+++ python/square-root/square_root.py
@@ -1,13 +1,11 @@
 def square_root(number: int) -> int:
-    number_squared = number * number
-
-    if number_squared == number:
+    if number * number == number:
         return number
     
     number_guess = number // 2
 
     # Babylonian method to find square root
-    while number_squared != number:
+    while number_guess * number_guess != number:
         number_guess = (number_guess + (number // number_guess)) / 2
 
     return number_guess
