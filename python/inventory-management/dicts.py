352193a051c32a1be20e3d0ed77160a3bed06e02
junallan <junallan@telus.net>
2023-04-17 19:42:24 -0600

    Implemented create_inventory

---
+++ python/inventory-management/dicts.py
@@ -0,0 +1,59 @@
+"""Functions to keep track and alter inventory."""
+
+
+def create_inventory(items: list[str]) -> dict[str,int]:
+    """Create a dict that tracks the amount (count) of each element on the `items` list.
+
+    :param items: list - list of items to create an inventory from.
+    :return: dict - the inventory dictionary.
+    """
+    
+    inventory = {}
+
+    for element in items:
+        inventory[element] = inventory[element] + 1 if element in inventory else 1
+
+    return inventory
+
+
+def add_items(inventory, items):
+    """Add or increment items in inventory using elements from the items `list`.
+
+    :param inventory: dict - dictionary of existing inventory.
+    :param items: list - list of items to update the inventory with.
+    :return: dict - the inventory updated with the new items.
+    """
+
+    pass
+
+
+def decrement_items(inventory, items):
+    """Decrement items in inventory using elements from the `items` list.
+
+    :param inventory: dict - inventory dictionary.
+    :param items: list - list of items to decrement from the inventory.
+    :return: dict - updated inventory with items decremented.
+    """
+
+    pass
+
+
+def remove_item(inventory, item):
+    """Remove item from inventory if it matches `item` string.
+
+    :param inventory: dict - inventory dictionary.
+    :param item: str - item to remove from the inventory.
+    :return: dict - updated inventory with item removed. Current inventory if item does not match.
+    """
+
+    pass
+
+
+def list_inventory(inventory):
+    """Create a list containing all (item_name, item_count) pairs in inventory.
+
+    :param inventory: dict - an inventory dictionary.
+    :return: list of tuples - list of key, value pairs from the inventory dictionary.
+    """
+
+    pass
