11ed3c97392b0ce68cdb716b28fb199ca95ec475
junallan <junallan@telus.net>
2023-04-17 19:17:54 -0600

    Eliminated built in function operators

--- python/square-root/square_root.py
+++ python/square-root/square_root.py
@@ -1,11 +1,13 @@
 def square_root(number: int) -> int:
-    if number ** 2 == number:
+    number_squared = number * number
+
+    if number_squared == number:
         return number
     
     number_guess = number // 2
 
     # Babylonian method to find square root
-    while number_guess * number_guess != number:
+    while number_squared != number:
         number_guess = (number_guess + (number // number_guess)) / 2
 
     return number_guess
