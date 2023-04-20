56fd45dfb838ef3108335f4788d3cb6807565d55
junallan <junallan@telus.net>
2023-04-20 17:21:24 -0600

    Refactored straight logic to sort and test list content

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -12,15 +12,14 @@ FULL_HOUSE = (lambda dice: sum(dice)
     if len(distinct_numbers := set(dice)) == 2 
         and dice.count(distinct_numbers.pop()) in [2,3] 
     else 0)
-
 def FOUR_OF_A_KIND(dice: list[int]) -> int:
     number_that_repeats_most = max(dice, key = lambda d: dice.count(d))
-    count_of_most_repeated_number = dice.count(number_that_repeats_most)
- 
+
     return (number_that_repeats_most * 4 
-        if count_of_most_repeated_number >= 4 else 0)
-LITTLE_STRAIGHT = lambda dice: straight(dice, 1, 5)
-BIG_STRAIGHT = lambda dice: straight(dice, 2, 6)
+        if dice.count(number_that_repeats_most) >= 4 
+        else 0)
+LITTLE_STRAIGHT = lambda dice: 30 if sorted(dice) == [1, 2, 3, 4, 5] else 0
+BIG_STRAIGHT = lambda dice: 30 if sorted(dice) == [2, 3, 4, 5, 6] else 0
 CHOICE = sum
 
 def score(dice: list, category:Callable[[List[int]], int]) -> int:
@@ -28,13 +27,3 @@ def score(dice: list, category:Callable[[List[int]], int]) -> int:
 
 def count(dice: list, number: int) -> int:
     return dice.count(number) * number
-
-def straight(dice: list[int], min_number: int, max_number: int) -> int:
-    distinct_dices = list(set(dice))
-
-    if len(distinct_dices) != 5:
-        return 0
-    elif min(dice) == min_number and max(dice) == max_number:
-        return 30
-    else:
-        return 0 
