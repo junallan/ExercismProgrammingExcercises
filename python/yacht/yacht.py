a404ac5c8ab9e23035b1d859fde15585f8ed01ff
junallan <junallan@telus.net>
2023-04-19 18:11:42 -0600

    Refactored count scores to call common utitlity method to remove duplicated logic

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,14 +1,13 @@
 from typing import Callable, List
 
 # Score categories.
-# Change the values as you see fit.
 YACHT = lambda dice: 50 if all(d == dice[0] for d in dice) else 0
-ONES = lambda dice: dice.count(1)
-TWOS = lambda dice: dice.count(2) * 2
-THREES = lambda dice: dice.count(3) * 3
-FOURS = lambda dice: dice.count(4) * 4
-FIVES = lambda dice: dice.count(5) * 5
-SIXES = lambda dice: dice.count(6) * 6
+ONES = lambda dice: count(dice, 1)
+TWOS = lambda dice: count(dice, 2)
+THREES = lambda dice: count(dice, 3)
+FOURS = lambda dice: count(dice, 4)
+FIVES = lambda dice: count(dice, 5)
+SIXES = lambda dice: count(dice, 6)
 def FULL_HOUSE(dice: list[int]) -> int: 
     distinct_dices = list(set(dice))
 
@@ -28,6 +27,7 @@ def FOUR_OF_A_KIND(dice: list[int]) -> int:
 
     if len(distinct_dices) > 2:
         return 0
+    
     if len(distinct_dices) == 1:
         return distinct_dices[0] * 4
     else:
@@ -46,6 +46,9 @@ CHOICE = lambda dice: sum(dice)
 
 def score(dice: list, category:Callable[[List[int]], int]) -> int:
     return category(dice)
+
+def count(dice: list, number: int) -> int:
+    return dice.count(number) * number
 
 def straight(dice: list[int], min_number: int, max_number: int) -> int:
     distinct_dices = list(set(dice))
