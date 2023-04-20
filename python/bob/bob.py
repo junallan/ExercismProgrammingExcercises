297270fc0405a12894fac691ae86365718131025
junallan <junallan@telus.net>
2023-04-19 19:00:19 -0600

    Hanled yelling with question

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,7 +1,10 @@
 def response(hey_bob: str) -> str:
-    if hey_bob.isupper():
+    is_yelling = hey_bob.isupper()
+    is_asking_a_question = hey_bob.endswith("?")
+    if is_yelling and is_asking_a_question:
+        return "Calm down, I know what I'm doing!"
+    elif is_yelling:
         return "Whoa, chill out!"
-    elif hey_bob.endswith("?"):
+    elif is_asking_a_question:
         return "Sure."
-    #"Calm down, I know what I'm doing"
     pass
