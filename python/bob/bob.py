a8746915255dabed899482a98fee0b7cdca0aa5e
junallan <junallan@telus.net>
2023-04-19 21:04:00 -0600

    Handled space and default case

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,10 +1,14 @@
 def response(hey_bob: str) -> str:
     is_yelling = hey_bob.isupper()
-    is_asking_a_question = hey_bob.endswith("?")
+    is_asking_a_question = hey_bob.strip().endswith("?")
+    
     if is_yelling and is_asking_a_question:
         return "Calm down, I know what I'm doing!"
     elif is_yelling:
         return "Whoa, chill out!"
     elif is_asking_a_question:
         return "Sure."
-    pass
+    elif hey_bob.isspace() or not hey_bob:
+        return "Fine. Be that way!"
+    else:
+        return "Whatever."
