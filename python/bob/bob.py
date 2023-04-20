6264edd7d2efd04e99ac45c6be8b7c39067762bc
junallan <junallan@telus.net>
2023-04-19 18:57:44 -0600

    Handle question case

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,4 +1,7 @@
 def response(hey_bob: str) -> str:
     if hey_bob.isupper():
         return "Whoa, chill out!"
+    elif hey_bob.endswith("?"):
+        return "Sure."
+    #"Calm down, I know what I'm doing"
     pass
