092298ff8bb1960885898e52a772553758410e48
junallan <junallan@telus.net>
2023-04-20 21:33:11 -0600

    Refactored utilizing dictionary comprehension

--- python/etl/etl.py
+++ python/etl/etl.py
@@ -1,9 +1,6 @@
 def transform(legacy_data: dict[int, list[str]]) -> dict[str, int]:
-    transformed_data = {}
-    for item in legacy_data:
-        for element in legacy_data[item]:
-            transformed_data[element.lower()] = item
-
-    return transformed_data        
-    #return {legacy_data[item]: item for item in legacy_data}
+    return {element.lower(): key 
+            for key, values in legacy_data.items() 
+            for element in values}
+ 
 
