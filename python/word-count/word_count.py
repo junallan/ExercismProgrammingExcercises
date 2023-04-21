f0aecc27bf0da48461c5f862cd63d05f3547bbf4
junallan <junallan@telus.net>
2023-04-21 17:29:17 -0600

    Eliminate newline and tabs in string and check for whitespace

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,10 +1,11 @@
+import re
 import string
 
 def count_words(sentence: str) -> dict[str, int]:
-    translator = str.maketrans("", "", string.punctuation)
-    words = sentence.translate(translator).lower().split()
     word_counts = {}
-
+    delimeters = string.punctuation.replace("'", "") + " "
+    words = [w.replace("\n", "").replace("\t", "").strip(string.punctuation).lower() for w in re.split(f"[{delimeters}]+", sentence) if not w.isspace() and w]
+#    print("words " + words)
     for w in words:
         if w in word_counts:
             word_counts[w] += 1
@@ -12,3 +13,15 @@ def count_words(sentence: str) -> dict[str, int]:
             word_counts[w] = 1
 
     return word_counts
+
+    # translator = str.maketrans("", "", string.punctuation)
+    # words = sentence.translate(translator).lower().split()
+    # word_counts = {}
+
+    # for w in words:
+    #     if w in word_counts:
+    #         word_counts[w] += 1
+    #     else:
+    #         word_counts[w] = 1
+
+    # return word_counts
