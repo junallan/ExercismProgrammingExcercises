81e49beaa106b0230108277760240ab02af03d16
junallan <junallan@telus.net>
2023-04-21 17:02:58 -0600

    Removed unecessary ( and refactored list comprehension utilizing dictionary .items() built in function

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,8 +1,10 @@
 RAINDROPS = {3: "Pling", 5: "Plang", 7: "Plong"}
 
+
 def convert(number: int) -> str:
-    sounds = ([RAINDROPS[factor]          
-        for factor in RAINDROPS if not number % factor])
+    sounds = [sound         
+        for factor, sound in RAINDROPS.items() 
+        if not number % factor]
 
     return "".join(sounds) or str(number)
 
