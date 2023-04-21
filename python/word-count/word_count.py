780122c2b6aeb223808449c15e085ef9851efb0e
junallan <junallan@telus.net>
2023-04-21 17:39:57 -0600

    Handled ' check

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -4,8 +4,10 @@ import string
 def count_words(sentence: str) -> dict[str, int]:
     word_counts = {}
     delimeters = string.punctuation.replace("'", "") + "\n\t "
-    words = [w.strip(string.punctuation).lower() for w in re.split(f"[{delimeters}]+", sentence) if not w.isspace() and w]
-#    print("words " + words)
+    words = [w.strip(string.punctuation).lower() 
+        for w in re.split(f"[{delimeters}]+", sentence) 
+        if not w.isspace() and w != "'" and w]
+
     for w in words:
         if w in word_counts:
             word_counts[w] += 1
@@ -13,15 +15,3 @@ def count_words(sentence: str) -> dict[str, int]:
             word_counts[w] = 1
 
     return word_counts
-
-    # translator = str.maketrans("", "", string.punctuation)
-    # words = sentence.translate(translator).lower().split()
-    # word_counts = {}
-
-    # for w in words:
-    #     if w in word_counts:
-    #         word_counts[w] += 1
-    #     else:
-    #         word_counts[w] = 1
-
-    # return word_counts
