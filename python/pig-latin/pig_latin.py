c8e5916506fa714b4f6826831353f8ae633897ca
junallan <junallan@telus.net>
2023-04-23 15:58:19 -0600

    Refactored to utilize index of instead of string replace

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -9,7 +9,7 @@ def translate(text: str) -> str:
 
 def translate_word(word: str) -> str:
     if word[0] == "y" and not get_constant_cluster(word[1:]): 
-        return f"{word.replace(word[0], '')}{word[0]}ay"
+        return f"{word[1:]}{word[0]}ay"
 
     beginning_constants = get_constant_cluster(word)
 
@@ -17,13 +17,13 @@ def translate_word(word: str) -> str:
         return f"{word}ay"
     
     if word.startswith("qu"):
-        return f"{word.replace('qu', '')}quay"
+        return f"{word[2:]}quay"
     
     if len(word) >= 3 and word[1:3] == "qu":
-        return f"{word.replace(word[0:3], '', 1)}{word[0:3]}ay"
+        return f"{word[3:]}{word[0:3]}ay"
 
     if beginning_constants:
-        return f"{word.replace(beginning_constants, '', 1)}{beginning_constants}ay"
+        return f"{word[len(beginning_constants):]}{beginning_constants}ay"
 
 
 def get_constant_cluster(text: str) -> str:
