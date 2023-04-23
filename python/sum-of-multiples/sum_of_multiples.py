ed6915e8775c44a537e9b522b2eb168519d5c834
junallan <junallan@telus.net>
2023-04-23 16:40:28 -0600

    Handled distinct sum multiples

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,4 +1,18 @@
 def sum_of_multiples(limit: int, multiples: list[int]) -> int:
     if multiples and limit < multiples[0]: return 0
 
-    pass
+    multiples_less_than_or_equal_limit = [base for base in multiples if base <= limit]
+
+    multiples_tracked = set()
+
+    for base in multiples_less_than_or_equal_limit:
+        counter = 1
+        multiple = base * counter
+        
+        while limit >= multiple:
+            multiples_tracked.add(multiple)
+            counter += 1
+            multiple = base * counter
+
+    return sum(multiples_tracked)
+
