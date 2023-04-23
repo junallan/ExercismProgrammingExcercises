d36487f1bb27f8b51200bd690fae1b2fe00ce2f0
junallan <junallan@telus.net>
2023-04-23 17:03:55 -0600

    Fixed bug accumulating 1 factor

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,18 +1,11 @@
 def sum_of_multiples(limit: int, multiples: list[int]) -> int:
-    if multiples and limit < multiples[0]: return 0
-
-    multiples_less_than_or_equal_limit = [base for base in multiples if base <= limit]
+    if not multiples: return 0
 
     multiples_tracked = set()
 
-    for base in multiples_less_than_or_equal_limit:
-        counter = 1
-        multiple = base * counter
-        
-        while limit >= multiple:
-            multiples_tracked.add(multiple)
-            counter += 1
-            multiple = base * counter
+    for num in multiples:
+        if num != 0:
+            multiples_tracked.update(range(num, limit, num))
 
     return sum(multiples_tracked)
 
