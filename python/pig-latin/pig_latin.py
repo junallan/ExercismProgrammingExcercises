998f598adecc9351899d540380186a82e58ac405
junallan <junallan@telus.net>
2023-04-23 09:43:05 -0600

    Handle constant cluster case

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,3 +1,6 @@
+from re import match
+
+
 def translate(text: str) -> str:
     if not text: return ""
 
@@ -5,4 +8,14 @@ def translate(text: str) -> str:
 
     if first_character in "aeiou":
         return f"{text}ay"
-    
+
+    if (beginning_constants:= get_constant_cluster(text)):
+        return f"{text.replace(beginning_constants, '', 1)}{beginning_constants}ay"
+
+def get_constant_cluster(text: str) -> str:
+    matching = match(r'^[^aeiou]+', text)
+
+    if matching:
+        return matching.group(0)
+    else:
+        return ""
