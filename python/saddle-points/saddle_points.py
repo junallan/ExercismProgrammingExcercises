90749c5db639728ee5151b94edf7f64680831f2d
junallan <junallan@telus.net>
2023-04-23 18:42:01 -0600

    Handle irregular matrix case

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,2 +1,7 @@
 def saddle_points(matrix: list[list[int]]) -> list[dict[str, int]]:
     if len(matrix) == 0: return []
+
+    row_items_length = map(len, matrix)
+
+    if not all(length == len(matrix[0]) for length in row_items_length):
+        raise ValueError("irregular matrix")
