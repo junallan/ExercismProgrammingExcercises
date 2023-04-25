5b457dd3bc09afd5fd1f55e102298b0ca2ca6448
junallan <junallan@telus.net>
2023-04-24 20:18:25 -0600

    Replaced % with range instead

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,7 +1,11 @@
 def sum_of_multiples(limit: int, multiples: list[int]) -> int:
-    return sum({num for num in range(limit) 
-        for factor in multiples 
-        if factor != 0 and num % factor == 0})
+    return sum({num for factor in multiples 
+                if factor != 0 
+                for num in range(factor, limit, factor)})
+
+    # return sum({num for num in range(limit) 
+    #     for factor in multiples 
+    #     if factor != 0 and num % factor == 0})
 
     #if not multiples: return 0
 
