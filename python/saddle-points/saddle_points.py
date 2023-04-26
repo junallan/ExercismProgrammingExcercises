1b98bba6df0e4c97ed231b2666965c0f4714d4d2
junallan <junallan@telus.net>
2023-04-26 17:50:13 -0600

    Revised to list comprehension

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -6,15 +6,7 @@ def saddle_points(matrix: list[list[int]]) -> list[dict[str, int]]:
     if not all(length == len(matrix[0]) for length in row_items_length):
         raise ValueError("irregular matrix")
     
-    matching_saddle_points = []
-
-    for row_index, row in enumerate(matrix):
-        max_row = max(row)
-        
-        for column_index in range(len(row)):
-            min_column = min(row[column_index] for row in matrix)   
-        
-            if max_row == min_column:
-                matching_saddle_points.append({"row": row_index + 1, "column": column_index + 1})
-
-    return matching_saddle_points
+    return [{"row": row_index + 1, "column": column_index + 1} 
+            for row_index, row in enumerate(matrix) 
+            for column_index in range(len(row)) 
+            if max(row) == min(row[column_index] for row in matrix)]
