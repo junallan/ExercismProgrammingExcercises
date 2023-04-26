e4a13305760c30a364cad78481603fc010820a31
junallan <junallan@telus.net>
2023-04-25 22:04:56 -0600

    Simplified logic only checking for vowels

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -11,16 +11,7 @@ def translate_word(word: str) -> str:
     if word[0] in "aeiou" or word.startswith("xr") or word.startswith("yt"): 
         return f"{word}ay"
 
-    beginning_constants = "y" if word[0] == "y" else "" + get_constant_cluster(word)
-
-    if beginning_constants:
-        length_beginning_constants = len(beginning_constants)
-        word_split_index = length_beginning_constants
-        if word[length_beginning_constants] == "u" and word[length_beginning_constants-1] == "q":
-            word_split_index += 1
-    
-        return f"{word[word_split_index:]}{word[:word_split_index]}ay"
-
-
-def get_constant_cluster(text: str) -> str:
-    return matching.group() if (matching:= match(r'^[^aeiouy]+', text)) else ""
+    for pos in range(1, len(word)):
+        if word[pos] in "aeiouy":
+            pos += 1 if word[pos] == "u" and word[pos - 1] == "q" else 0    
+            return f"{word[pos:]}{word[:pos]}ay"
