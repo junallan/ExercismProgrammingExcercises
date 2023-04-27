058ae92e6cb3e89911423552a47571ed37de36dd
junallan <junallan@telus.net>
2023-04-26 18:52:23 -0600

    Fix class docstring placement

--- python/ellens-alien-game/classes.py
+++ python/ellens-alien-game/classes.py
@@ -1,10 +1,16 @@
 """Solution to Ellen's Alien Game exercise."""
-
-
 from typing import List
 
 
 class Alien:
+    """
+    Represents an alien object in Ellen's Alien Game.
+
+    Attributes:
+        x_coordinate (int): The x-coordinate of the alien's current position.
+        y_coordinate (int): The y-coordinate of the alien's current position.
+        health (int): The current health of the alien.
+    """
     total_aliens_created = 0
 
     def __init__(self, x_coordinate: int, y_coordinate: int):
@@ -12,6 +18,7 @@ class Alien:
         self.y_coordinate = y_coordinate
         self.health = 3
         Alien.total_aliens_created += 1
+
 
     # hit(): Decrement Alien health by one point.
     def hit(self):
@@ -21,16 +28,18 @@ class Alien:
     # is_alive(): Return a boolean for if Alien is alive (if health is > 0).
     def is_alive(self) -> bool:
         return self.health > 0
-    
+
 
     # teleport(new_x_coordinate, new_y_coordinate): Move Alien object to new coordinates.
     def teleport(self, x_coordinate: int, y_coordinate: int):
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
 
+
     # collision_detection(other): Implementation TBD.
     def collision_detection(self, other):
         pass
+
 
 
 def new_aliens_collection(coordinates: List[tuple[int,int]]) -> List[Alien]:
