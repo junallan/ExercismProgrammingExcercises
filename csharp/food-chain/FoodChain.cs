b5644fd99787b78bc434e9a76aa756692eb9efaa
junallan <junallan@telus.net>
2023-04-27 18:50:37 -0600

    Revised initial song Fly case building up using repeated sentences and animals

--- csharp/food-chain/FoodChain.cs
+++ csharp/food-chain/FoodChain.cs
@@ -1,26 +1,69 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography;
 using System.Text;
 
 public static class FoodChain
 {
     public static string Recite(int verseNumber)
     {
-        var animals = new List<string>() { "fly" };
-
         var song = new StringBuilder();
 
-        for(var i=0; i< verseNumber; i++)
+        // Store lookup of common sentence chunks
+        var sentenceChunks = new List<string>
         {
-            song.Append($"I know an old lady who swallowed a {animals[i]}.\n");
-            song.Append($"I don't know why she swallowed the {animals[i]}. ");
-            song.Append("Perhaps she'll die.");
+            "I know an old lady who swallowed a",
+            "It wriggled and jiggled and tickled inside her.",
+            "She swallowed the",
+            "to catch the",
+            "I don't know why she swallowed the",
+        };
 
+        // Store lookup of common animal words
+        var animals = new List<string>
+        {
+            "fly",
+            "spider"
+        };
+
+        for (var index=0; index < verseNumber; index++)
+        {
+            song.Append($"{sentenceChunks[index]} {animals[index]}.\n");
+         
         }
-        //        "I know an old lady who swallowed a fly.\n" +
-        //        "I don't know why she swallowed the fly. Perhaps she'll die.";
+
+        song.Append($"{sentenceChunks[sentenceChunks.Count-1]} {animals[0]}. ");
+        song.Append("Perhaps she'll die.");
 
         return song.ToString();
+
+        // Find pattern and loop through assembling song with sentence
+        // chunks and animal words
+
+
+        //var firstPhrase = "I know an old lady who swallowed a";
+        ////var lastTwoSentences = 
+        //var animals = new List<string>() { "fly" };
+
+        //var song = new StringBuilder();
+
+        //for(var i=0; i< verseNumber; i++)
+        //{
+        //    song.Append($"I know an old lady who swallowed a {animals[i]}.\n");
+        //    song.Append($"I don't know why she swallowed the {animals[i]}. ");
+        //    song.Append("Perhaps she'll die.");
+
+        //}
+
+
+        //   "I know an old lady who swallowed a spider.\n" +
+        //"It wriggled and jiggled and tickled inside her.\n" +
+        //"She swallowed the spider to catch the fly.\n" +
+        //"I don't know why she swallowed the fly. Perhaps she'll die.";
+        // return song.ToString();
     }
 
     public static string Recite(int startVerse, int endVerse)
