e8640c377c7f33aa6ab823c9b9d363b861efa20f
junallan <junallan@telus.net>
2023-04-29 00:20:35 -0600

    Refactored extracting match file function

--- csharp/grep/Grep.cs
+++ csharp/grep/Grep.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
+
 public static class Grep
 {
     public static string Match(string pattern, string flags, string[] files)
@@ -16,56 +17,75 @@ public static class Grep
 
         foreach (var fileName in files)
         {
-            var fileContents = File.ReadAllText(fileName)
-                .Split("\n")
-                .Where(x => x != string.Empty)
-                .ToArray();
+            var matchedLine = FileMatch(pattern, fileName, files, matchOptions, match);
 
-            for (int lineNumber = 1; lineNumber <= fileContents.Length; lineNumber++)
+            if (!string.IsNullOrEmpty(matchedLine))
             {
-                var isMatch = false;
-
-                var lineContent = fileContents[lineNumber - 1];
-
-                if (match.IsCaseInsensitiveMatch && lineContent.ToLower().Contains(pattern.ToLower()))
-                    isMatch = true;
-                else if (match.IsWholeLineMatch && lineContent == pattern)
-                    isMatch = true;
-                else if(!(match.IsCaseInsensitiveMatch || match.IsWholeLineMatch))
-                    isMatch = lineContent.Contains(pattern);
-
-                if (match.IsInvertedMatch)
-                    isMatch = !isMatch;
-
-                if (!isMatch) continue;
-
                 if (result.Length > 0) result.Append("\n");
-
-                if (matchOptions.Contains("-l"))
-                {
-                    result.Append(fileName);
-
-                    if (files.Count() > 1)
-                        break;
-
-                    continue;
-                }
-
-                if (files.Count() > 1) result.Append($"{fileName}:");
-
-                if (matchOptions.Contains("-n"))
-                    result.Append($"{lineNumber}:{lineContent}");
-                else
-                    result.Append(lineContent);
+                   
+                result.Append(matchedLine);
             }
-                
         }
 
         return result.ToString();
     }
 
 
-    private static (bool IsCaseInsensitiveMatch, bool IsWholeLineMatch, bool IsInvertedMatch) MatchOption(IEnumerable<string> matchOptions)
+    private static string FileMatch(
+        string pattern, string fileName, string[] files, IEnumerable<string> matchOptions,
+        (bool IsCaseInsensitiveMatch, bool IsWholeLineMatch, bool IsInvertedMatch) match)
+    {
+        var result = new StringBuilder();
+
+        var fileContents = File.ReadAllText(fileName)
+                        .Split("\n")
+                        .Where(x => x != string.Empty)
+                        .ToArray();
+
+        for (int lineNumber = 1; lineNumber <= fileContents.Length; lineNumber++)
+        {
+            var isMatch = false;
+
+            var lineContent = fileContents[lineNumber - 1];
+
+            if (match.IsCaseInsensitiveMatch && lineContent.ToLower().Contains(pattern.ToLower()))
+                isMatch = true;
+            else if (match.IsWholeLineMatch && lineContent == pattern)
+                isMatch = true;
+            else if (!(match.IsCaseInsensitiveMatch || match.IsWholeLineMatch))
+                isMatch = lineContent.Contains(pattern);
+
+            if (match.IsInvertedMatch)
+                isMatch = !isMatch;
+
+            if (!isMatch) continue;
+
+            if (result.Length > 0) result.Append("\n");
+
+            if (matchOptions.Contains("-l"))
+            {
+                result.Append(fileName);
+
+                if (files.Count() > 1)
+                    break;
+
+                continue;
+            }
+
+            if (files.Count() > 1) result.Append($"{fileName}:");
+
+            if (matchOptions.Contains("-n"))
+                result.Append($"{lineNumber}:{lineContent}");
+            else
+                result.Append(lineContent);
+        }
+
+        return result.ToString();
+    }
+
+
+    private static (bool IsCaseInsensitiveMatch, bool IsWholeLineMatch, bool IsInvertedMatch)
+        MatchOption(IEnumerable<string> matchOptions)
     {
         var isCaseInsensitiveMatch = false;
         var isWholeLineMatch = false;
