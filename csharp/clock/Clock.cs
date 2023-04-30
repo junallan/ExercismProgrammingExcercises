f2b0d2898039b4b34fc8aac2eb1ad959d51402f2
junallan <junallan@telus.net>
2023-04-30 15:34:09 -0600

    Implemented base case of toString

---
+++ csharp/clock/Clock.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Clock
+{
+    public int Hours { get; set; }
+    public int Minutes { get; set; }
+    public Clock(int hours, int minutes)
+    {
+        Hours = hours;
+        Minutes = minutes;
+    }
+
+    public override string ToString()
+        => $"{Hours.ToString().PadLeft(2, '0')}:{Minutes.ToString().PadLeft(2, '0')}";
+
+    public Clock Add(int minutesToAdd)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public Clock Subtract(int minutesToSubtract)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
