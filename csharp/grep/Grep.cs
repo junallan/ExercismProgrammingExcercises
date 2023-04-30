4b0e7fe946ebebbba8682fb8dbe0558575517420
junallan <junallan@telus.net>
2023-04-29 19:03:36 -0600

    Refactored utilizing enum flag options

--- csharp/grep/Grep.cs
+++ csharp/grep/Grep.cs
@@ -7,17 +7,53 @@ using System.Text;
 
 public static class Grep
 {
+    [Flags]
+    public enum Options
+    {
+        LineNumbers = 1,//-n
+        FileNames = 2,//-l
+        CaseInsensitiveMatch = 4, //-i
+        InvertProgramMatch = 8,//-v
+        EntireLineMatch = 16//-x
+    }
+
+    public static class OptionTranslation
+    {
+        private static readonly Dictionary<string, Options> FlagsToOptions= new Dictionary<string, Options>
+        {
+            { "-n", Options.LineNumbers },
+            { "-l", Options.FileNames },
+            { "-i", Options.CaseInsensitiveMatch },
+            { "-v", Options.InvertProgramMatch },
+            { "-x", Options.EntireLineMatch }
+        };
+
+        public static List<Options> GetOptions(string flags)
+        {
+            var optionsSelected = new List<Options>();
+
+            if (string.IsNullOrEmpty(flags)) return optionsSelected;
+
+            var matchOptions = flags.Split().AsEnumerable();
+
+            foreach(var option in matchOptions)
+            {
+                optionsSelected.Add(FlagsToOptions[option]);
+            }
+
+            return optionsSelected;
+        }
+    }
+
+
     public static string Match(string pattern, string flags, string[] files)
     {
         var result = new StringBuilder();
-
-        var matchOptions = flags.Split().AsEnumerable();
-
-        var match = MatchOption(matchOptions);
+        var optionsSelected = OptionTranslation.GetOptions(flags);
 
         foreach (var fileName in files)
         {
-            var matchedLine = FileMatch(pattern, fileName, files, matchOptions, match);
+            var matchedLine = FileMatch(pattern, fileName, files, optionsSelected);
 
             if (!string.IsNullOrEmpty(matchedLine))
             {
@@ -32,8 +68,7 @@ public static class Grep
 
 
     private static string FileMatch(
-        string pattern, string fileName, string[] files, IEnumerable<string> matchOptions,
-        (bool IsCaseInsensitiveMatch, bool IsWholeLineMatch, bool IsInvertedMatch) match)
+        string pattern, string fileName, string[] files, List<Options> optionsSelected)
     {
         var result = new StringBuilder();
 
@@ -48,21 +83,23 @@ public static class Grep
 
             var lineContent = fileContents.ElementAt(lineNumber - 1);
 
-            if (match.IsCaseInsensitiveMatch && lineContent.ToLower().Contains(pattern.ToLower()))
+            if (optionsSelected.Contains(Options.CaseInsensitiveMatch)
+                && lineContent.ToLower().Contains(pattern.ToLower()))
                 isMatch = true;
-            else if (match.IsWholeLineMatch && lineContent == pattern)
+            else if (optionsSelected.Contains(Options.EntireLineMatch) && lineContent == pattern)
                 isMatch = true;
-            else if (!(match.IsCaseInsensitiveMatch || match.IsWholeLineMatch))
+            else if (!(optionsSelected.Contains(Options.CaseInsensitiveMatch)
+                || optionsSelected.Contains(Options.EntireLineMatch)))
                 isMatch = lineContent.Contains(pattern);
 
-            if (match.IsInvertedMatch)
+            if (optionsSelected.Contains(Options.InvertProgramMatch))
                 isMatch = !isMatch;
 
             if (!isMatch) continue;
 
             if (result.Length > 0) result.Append("\n");
 
-            if (matchOptions.Contains("-l"))
+            if (optionsSelected.Contains(Options.FileNames))
             {
                 result.Append(fileName);
 
@@ -74,7 +111,7 @@ public static class Grep
 
             if (files.Count() > 1) result.Append($"{fileName}:");
 
-            if (matchOptions.Contains("-n"))
+            if (optionsSelected.Contains(Options.LineNumbers))
                 result.Append($"{lineNumber}:{lineContent}");
             else
                 result.Append(lineContent);
@@ -82,25 +119,4 @@ public static class Grep
 
         return result.ToString();
     }
-
-
-    private static (bool IsCaseInsensitiveMatch, bool IsWholeLineMatch, bool IsInvertedMatch)
-        MatchOption(IEnumerable<string> matchOptions)
-    {
-        var isCaseInsensitiveMatch = false;
-        var isWholeLineMatch = false;
-        var isInvertedMatch = false;
-
-        foreach (var option in matchOptions)
-        {
-            if (option.Contains("-i"))
-                isCaseInsensitiveMatch = true;
-            else if (option.Contains("-x"))
-                isWholeLineMatch = true;
-            else if (option.Contains("-v"))
-                isInvertedMatch = true;
-        }
-        return (isCaseInsensitiveMatch, isWholeLineMatch, isInvertedMatch);
-    }
-
 }
