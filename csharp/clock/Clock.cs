81a736b18aeed20ae91384276e81352eecb10f0e
junallan <junallan@telus.net>
2023-04-30 20:04:53 -0600

    Handle negative minutes

--- csharp/clock/Clock.cs
+++ csharp/clock/Clock.cs
@@ -3,20 +3,22 @@ using System;
 public class Clock
 {
     public const int HoursInADay = 24;
-    public const int MinutesInADay = 60;
+    public const int MinutesInAnHour = 60;
 
     public int Hours { get; set; }
     public int Minutes { get; set; }
+    public int TotalMinutes { get; set; }
+
     public Clock(int hours, int minutes)
     {
-        Hours = hours >= 0 ? hours : HoursInADay + (hours % HoursInADay);
-        Minutes = minutes % MinutesInADay;
-        Hours += minutes / MinutesInADay;
-        Hours %= HoursInADay;
+        TotalMinutes = (hours >= 0 ? hours : HoursInADay + (hours % HoursInADay)) * MinutesInAnHour + minutes;
+        Hours = (TotalMinutes / MinutesInAnHour) % HoursInADay;
+        Minutes = (minutes >= 0 ? minutes : MinutesInAnHour + minutes) % MinutesInAnHour;
     }
 
     public override string ToString()
         => $"{Hours.ToString().PadLeft(2, '0')}:{Minutes.ToString().PadLeft(2, '0')}";
+    //=> $"{Hours.ToString().PadLeft(2, '0')}:{Minutes.ToString().PadLeft(2, '0')}";
 
     public Clock Add(int minutesToAdd)
     {
