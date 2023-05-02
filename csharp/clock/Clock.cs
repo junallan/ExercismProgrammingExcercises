8910b4932a016a73f84c0a6cd9f5035db7afa3bd
junallan <junallan@telus.net>
2023-05-01 20:42:19 -0600

    Handle Add method

--- csharp/clock/Clock.cs
+++ csharp/clock/Clock.cs
@@ -9,32 +9,33 @@ public class Clock
     public int Minutes { get; set; }
     public int TotalMinutes { get; set; }
 
+
     public Clock(int hours, int minutes)
     {
         TotalMinutes = (hours >= 0 ? hours : HoursInADay + (hours % HoursInADay)) * MinutesInAnHour + minutes;
 
-        if (minutes < (MinutesInAnHour * -1))
+        if (minutes >= -MinutesInAnHour)
+            Hours = (TotalMinutes / MinutesInAnHour) % HoursInADay;
+        else    
         {
             Hours = HoursInADay + (hours + ((minutes / MinutesInAnHour) - 1) % HoursInADay);
 
             while (Hours < 0) Hours = HoursInADay + Hours;
         }
-        else
-        {
-            Hours = (TotalMinutes / MinutesInAnHour) % HoursInADay;
-        }
-
+   
         Minutes = (minutes >= 0 ? minutes : MinutesInAnHour + (minutes % MinutesInAnHour)) % MinutesInAnHour;
     }
 
+
     public override string ToString()
         => $"{Hours.ToString().PadLeft(2, '0')}:{Minutes.ToString().PadLeft(2, '0')}";
-    //=> $"{Hours.ToString().PadLeft(2, '0')}:{Minutes.ToString().PadLeft(2, '0')}";
+
 
     public Clock Add(int minutesToAdd)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new Clock(this.Hours, this.Minutes + minutesToAdd);
     }
+
 
     public Clock Subtract(int minutesToSubtract)
     {
