06fa20b370f69b8912fa908713dceb3ab6c45182
junallan <junallan@telus.net>
2023-05-03 20:02:16 -0600

    Stringbulider method and benchmark comparison

--- csharp/markdown/MarkdownOriginal.cs
+++ csharp/markdown/MarkdownOriginal.cs
@@ -1,55 +1,25 @@
-using System;
+﻿using System;
 using System.Text;
 using System.Text.RegularExpressions;
 
-public static class Markdown
+
+public static class MarkdownOriginal
 {
-    private static string Wrap(string text, string tag)
-    {
-        var result = new StringBuilder();
-        result.Append("<");
-        result.Append(tag);
-        result.Append(">");
-        result.Append(text);
-        result.Append("</");
-        result.Append(tag);
-        result.Append(">");
-
-        return result.ToString();
-    }
-
+    private static string Wrap(string text, string tag) => $"<{tag}>{text}</{tag}>";
     private static bool IsTag(string text, string tag) => text.StartsWith($"<{tag}>");
-
     private static string Parse(string markdown, string delimiter, string tag)
     {
-        var pattern = new StringBuilder();
-        pattern.Append(delimiter);
-        pattern.Append("(.+)");
-        pattern.Append(delimiter);
-
-        var replacement = new StringBuilder();
-        replacement.Append("<");
-        replacement.Append(tag);
-        replacement.Append(">$1</");
-        replacement.Append(tag);
-        replacement.Append(">");
-
-        return Regex.Replace(markdown, pattern.ToString(), replacement.ToString());
+        var pattern = $"{delimiter}(.+){delimiter}";
+        var replacement = $"<{tag}>$1</{tag}>";
+        return Regex.Replace(markdown, pattern, replacement);
     }
-
     private static string Parse__(string markdown) => Parse(markdown, "__", "strong");
-
     private static string Parse_(string markdown) => Parse(markdown, "_", "em");
-
     private static string ParseText(string markdown, bool list)
     {
         var parsedText = Parse_(Parse__((markdown)));
-
-        if (list) return parsedText;
-        else return Wrap(parsedText, "p");
+        return list ? parsedText : Wrap(parsedText, "p");
     }
-
-
     private static string ParseHeader(string markdown, bool list, out bool inListAfter)
     {
         var count = 0;
@@ -69,19 +39,11 @@ public static class Markdown
             inListAfter = list;
             return null;
         }
-        var headerTag = new StringBuilder("h");
-        headerTag.Append(count);
-
-        var headerHtml = new StringBuilder(Wrap(markdown.Substring(count + 1), headerTag.ToString()));
-
+        var headerTag = $"h{count}";
+        var headerHtml = Wrap(markdown.Substring(count + 1), headerTag);
         inListAfter = false;
-
-        if (list) headerHtml.Insert(0, headerHtml);
-
-        return headerHtml.ToString();
+        return list ? $"</ul>{headerHtml}" : headerHtml;
     }
-
-
     private static string ParseLineItem(string markdown, bool list, out bool inListAfter)
     {
         if (markdown.StartsWith("*"))
@@ -93,40 +55,28 @@ public static class Markdown
         inListAfter = list;
         return null;
     }
-
-
     private static string ParseParagraph(string markdown, bool list, out bool inListAfter)
     {
         inListAfter = false;
-
-        var parseText = new StringBuilder(ParseText(markdown, false));
-
-        if (list) parseText.Insert(0, "</ul>");
-
-        return parseText.ToString();
+        return list ? $"</ul>{ParseText(markdown, false)}" : ParseText(markdown, false);
     }
-
     private static string ParseLine(string markdown, bool list, out bool inListAfter)
-         => ParseHeader(markdown, list, out inListAfter)
-             ?? ParseLineItem(markdown, list, out inListAfter)
-             ?? ParseParagraph(markdown, list, out inListAfter)
-             ?? throw new ArgumentException("Invalid markdown");
-
+        => ParseHeader(markdown, list, out inListAfter)
+            ?? ParseLineItem(markdown, list, out inListAfter)
+            ?? ParseParagraph(markdown, list, out inListAfter)
+            ?? throw new ArgumentException("Invalid markdown");
 
     public static string Parse(string markdown)
     {
-        var result = new StringBuilder();
-
         var lines = markdown.Split('\n');
+        var result = "";
         var list = false;
-
         for (int i = 0; i < lines.Length; i++)
         {
-            result.Append(ParseLine(lines[i], list, out list));
+            var lineResult = ParseLine(lines[i], list, out list);
+            result += lineResult;
         }
-
-        if (list) result.Append("</ul>");
-
-        return result.ToString();
+        return list ? $"{result}</ul>" : result;
     }
 }
+
