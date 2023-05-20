31193366c212d2c6921ade6935083a027327e1cd
junallan <junallan@telus.net>
2023-05-20 11:13:49 -0600

    Handle Uranus

--- prolog/space-age/space_age.pl
+++ prolog/space-age/space_age.pl
@@ -11,6 +11,7 @@ orbital_period("Venus", 0.61519726).
 orbital_period("Mars", 1.8808158).
 orbital_period("Jupiter", 11.862615).
 orbital_period("Saturn", 29.447498).
+orbital_period("Uranus", 84.016846).
 
 
 
