2f516c94a60a2c859dc2bbb0814f29a05d5e58f1
junallan <junallan@telus.net>
2023-05-20 11:17:05 -0600

    Handle Neptune planet

--- prolog/space-age/space_age.pl
+++ prolog/space-age/space_age.pl
@@ -12,7 +12,4 @@ orbital_period("Mars", 1.8808158).
 orbital_period("Jupiter", 11.862615).
 orbital_period("Saturn", 29.447498).
 orbital_period("Uranus", 84.016846).
-
-
-
-    
+orbital_period("Neptune", 164.79132).
