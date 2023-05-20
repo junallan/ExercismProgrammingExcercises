53538d30cbdc59a50339bb04322f9622b1df60ed
junallan <junallan@telus.net>
2023-05-20 11:00:17 -0600

    Revised to statisfy Mercury Planet

--- prolog/space-age/space_age.pl
+++ prolog/space-age/space_age.pl
@@ -1,6 +1,8 @@
-space_age(Planet, AgeSec, Years) :-
-    planet_years(Planet, AgeSec, Years).
+space_age(Planet, AgeSec, Years):-
+    planet_seconds_per_earth_year(Planet, SecondsFactor),
+    Years is AgeSec / SecondsFactor.
 
-planet_years("Earth", AgeSec, Years) :-
-    Years is AgeSec / 31557600.
+planet_seconds_per_earth_year("Earth", 31557600).
+planet_seconds_per_earth_year("Mercury", 7600543.82).
+
     
