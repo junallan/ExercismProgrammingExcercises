26b3b7e3c414961ac3ae8aea8e1b17c0c5436deb
junallan <junallan@telus.net>
2023-06-04 10:07:35 -0600

    Simplified string concatenation with foldl

--- prolog/raindrops/raindrops.pl
+++ prolog/raindrops/raindrops.pl
@@ -1,21 +1,25 @@
-convert(N, Sounds) :- 
+convert(N, Sounds) :-
     pling_sound(N, PlingSound),
     plang_sound(N, PlangSound),
     plong_sound(N, PlongSound),
-    string_concat(PlingSound, PlangSound, PlingPlangSound),
-    string_concat(PlingPlangSound, PlongSound, PlingPlangPlongSound),
-    (string_length(PlingPlangPlongSound, 0) -> 
+    foldl(concat_sound, [PlingSound, PlangSound, PlongSound], "", PlingPlangPlongSound),
+    (PlingPlangPlongSound = "" ->
         number_codes(N, Codes),
         string_codes(Sounds, Codes)
-        ;
-        string_chars(Sounds, PlingPlangPlongSound)
+    ;
+        Sounds = PlingPlangPlongSound
     ).
 
-pling_sound(N,"Pling") :- S is N mod 3, S=:=0.
+concat_sound(Sound, Acc, Result) :- string_concat(Acc, Sound, Result).
+
+pling_sound(N, "Pling") :- S is N mod 3, S =:= 0.
 pling_sound(_, "").
-plang_sound(N,"Plang") :- S is N mod 5, S=:=0.
-plang_sound(_,"").
-plong_sound(N,"Plong") :- S is N mod 7, S=:=0.
-plong_sound(_,"").
+
+plang_sound(N, "Plang") :- S is N mod 5, S =:= 0.
+plang_sound(_, "").
+
+plong_sound(N, "Plong") :- S is N mod 7, S =:= 0.
+plong_sound(_, "").
+
 
 
