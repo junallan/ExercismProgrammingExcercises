1f31486ab52e2f778659d664cea8ecb07c0de05b
junallan <junallan@telus.net>
2023-06-06 21:21:02 -0600

    Lambda expression to extract first letter in word

--- prolog/acronym/acronym.pl
+++ prolog/acronym/acronym.pl
@@ -1,6 +1,6 @@
 abbreviate(Sentence, Acronym) :-
     split_string(Sentence, " .,;:-_!?()[]{}", "", Words),
     maplist(string_chars, Words, CharsList),
-    convlist(nth0(0), CharsList, FirstChars),
+    convlist([X,Y]>>(X=[C|_], Y=C), CharsList, FirstChars),
     atomics_to_string(FirstChars,'',CharsAsAString),
     string_upper(CharsAsAString, Acronym).
