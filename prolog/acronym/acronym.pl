2f04b76e551c7ee4c9800eee2c5f503dde6894e4
junallan <junallan@telus.net>
2023-06-06 21:25:42 -0600

    Lambda expression to include string uppercase

--- prolog/acronym/acronym.pl
+++ prolog/acronym/acronym.pl
@@ -1,6 +1,5 @@
 abbreviate(Sentence, Acronym) :-
     split_string(Sentence, " .,;:-_!?()[]{}", "", Words),
     maplist(string_chars, Words, CharsList),
-    convlist([X,Y]>>(X=[C|_], Y=C), CharsList, FirstChars),
-    atomics_to_string(FirstChars,'',CharsAsAString),
-    string_upper(CharsAsAString, Acronym).
+    convlist([X,Y]>>(X=[C|_], string_upper(C, Y)), CharsList, FirstChars),
+    atomics_to_string(FirstChars,'',Acronym).
