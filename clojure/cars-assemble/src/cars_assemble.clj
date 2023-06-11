7526f60cba3391cd4c5e833f9d0a7d374c3abadb
junallan <junallan@telus.net>
2023-06-10 21:59:31 -0600

    Implemented cars assemble

---
+++ clojure/cars-assemble/src/cars_assemble.clj
@@ -0,0 +1,19 @@
+(ns cars-assemble)
+
+(def CARS_PRODUCED_PER_HOUR 221.0)
+
+(defn production-rate
+  "Returns the assembly line's production rate per hour,
+   taking into account its success rate"
+  [speed]
+  (cond (> speed 9) (* (* 0.77 CARS_PRODUCED_PER_HOUR) speed)
+        (> speed 8) (* (* 0.8 CARS_PRODUCED_PER_HOUR) speed)
+        (> speed 4) (* (* 0.9 CARS_PRODUCED_PER_HOUR) speed)
+        (> speed 0) (* CARS_PRODUCED_PER_HOUR speed)
+        :else 0) 
+  )
+
+(defn working-items
+  "Calculates how many working cars are produced per minute"
+  [speed]
+  )
