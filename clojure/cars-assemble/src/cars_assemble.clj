e8eb1ed0b8ca8ed8885cfad1e9d12ac73d025299
junallan <junallan@telus.net>
2023-06-10 23:24:40 -0600

    All tests passing

--- clojure/cars-assemble/src/cars_assemble.clj
+++ clojure/cars-assemble/src/cars_assemble.clj
@@ -3,16 +3,23 @@
 (def CARS_PRODUCED_PER_HOUR 221.0)
 (def MINUTES_PER_HOUR 60)
 
+(defn round-to-decimal-places
+  [num places]
+  (let [scale (Math/pow 10 places)]
+    (/ (Math/round (* num scale)) scale)))
+
 (defn production-rate
   "Returns the assembly line's production rate per hour,
    taking into account its success rate"
   [speed]
-  (cond (> speed 9) (* (* 0.77 CARS_PRODUCED_PER_HOUR) speed)
-        (> speed 8) (* (* 0.8 CARS_PRODUCED_PER_HOUR) speed)
-        (> speed 4) (* (* 0.9 CARS_PRODUCED_PER_HOUR) speed)
-        (> speed 0) (* CARS_PRODUCED_PER_HOUR speed)
-        :else 0.0) 
-  )
+  (let [rate (* speed
+              (* CARS_PRODUCED_PER_HOUR
+               (cond (> speed 9) 0.77
+                     (> speed 8) 0.8
+                     (> speed 4) 0.9
+                     (> speed 0) 1
+                     :else 0)))]
+    (round-to-decimal-places rate 1)))
 
 (defn working-items
   "Calculates how many working cars are produced per minute"
