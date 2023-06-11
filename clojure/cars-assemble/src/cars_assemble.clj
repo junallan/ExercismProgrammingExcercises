9e083a31ee206e5284332581f0d24cfdeccba051
junallan <junallan@telus.net>
2023-06-11 13:48:55 -0600

    Extact success-percentage defn and rename of variables

--- clojure/cars-assemble/src/cars_assemble.clj
+++ clojure/cars-assemble/src/cars_assemble.clj
@@ -1,34 +1,36 @@
 (ns cars-assemble)
 
-(def ^:private cars_produced_per_hour 221.0)
-(def ^:private minutes_per_hour 60)
+(def ^:private cars-produced-per-hour 221.0)
+(def ^:private minutes-per-hour 60)
 
 (defn round-to-decimal-places
   [num places]
   (let [scale (Math/pow 10 places)]
     (/ (Math/round (* num scale)) scale)))
 
-(defn success_rate
+(defn success-percentage
   [speed]
-  (* cars_produced_per_hour
-    (cond 
-        (> speed 9) 0.77
-        (> speed 8) 0.8
-        (> speed 4) 0.9
-        (> speed 0) 1
-        :else 0)
-  ))
+  (cond 
+    (> speed 9) 0.77
+    (> speed 8) 0.8
+    (> speed 4) 0.9
+    (> speed 0) 1
+    :else 0)
+)
+
+(defn success-rate
+  [speed]
+  (* cars-produced-per-hour (success-percentage speed)))
 
 (defn production-rate
   "Returns the assembly line's production rate per hour,
    taking into account its success rate"
   [speed]
   (let [rate 
-    (* speed
-      (success_rate speed))]
-    (round-to-decimal-places rate 1)))
+     (* speed (success-rate speed))]
+   (round-to-decimal-places rate 1)))
 
 (defn working-items
   "Calculates how many working cars are produced per minute"
   [speed]
-  (int (/ (production-rate speed) minutes_per_hour)))
+  (int (/ (production-rate speed) minutes-per-hour)))
