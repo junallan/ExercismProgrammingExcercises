0b18c1c69ce379e0146eaef5b08c2e5db64b703f
junallan <junallan@telus.net>
2023-06-11 16:49:39 -0600

    Siplified with combining condition or and

--- clojure/leap/src/leap.clj
+++ clojure/leap/src/leap.clj
@@ -1,10 +1,14 @@
 (ns leap)
 
 (defn leap-year? [year] 
-  (if (zero? (mod year 4))
-    (if (zero? (mod year 100)) 
-      (if (zero? (mod year 400))
-        true
-        false)
-      true)
-    false))  
+  (or (and (zero? (mod year 4))
+           (not (zero? (mod year 100))))
+      (zero? (mod year 400))))
+
+  ;; (if (zero? (mod year 4))
+  ;;   (if (zero? (mod year 100)) 
+  ;;     (if (zero? (mod year 400))
+  ;;       true
+  ;;       false)
+  ;;     true)
+  ;;   false))  
