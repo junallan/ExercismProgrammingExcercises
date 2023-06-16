b5871bb62a24cc8fc5042805887396439b93e53d
junallan <junallan@telus.net>
2023-06-15 21:01:54 -0600

    Implemented last-week

---
+++ clojure/bird-watcher/src/bird_watcher.clj
@@ -0,0 +1,22 @@
+(ns bird-watcher)
+
+(def last-week 
+  [0 2 5 3 7 8 4])
+
+(defn today [birds]
+  )
+
+(defn inc-bird [birds]
+  )
+
+(defn day-without-birds? [birds]
+  )
+
+(defn n-days-count [birds n]
+  )
+
+(defn busy-days [birds]
+  )
+
+(defn odd-week? [birds]
+  )
