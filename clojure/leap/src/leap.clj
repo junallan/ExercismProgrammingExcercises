7d42020a7264e3fca9ad356ef3ccd8a89f8d92c5
junallan <junallan@telus.net>
2023-06-17 23:24:22 -0600

    Fixed leap year case logic

--- clojure/leap/src/leap.clj
+++ clojure/leap/src/leap.clj
@@ -4,10 +4,22 @@
 (defn is-divisible-by? [year number]
   (zero? (mod year number)))
 
-(defn leap-year? [year] 
-  (or (and (is-divisible-by? year 4)
-           (not (is-divisible-by? year 100)))
-      (is-divisible-by? year 400)))
+(defn leap-year? [year]
+  (let [factor-of-4 (is-divisible-by? year 4)
+        factor-of-100 (is-divisible-by? year 100)
+        factor-of-400 (is-divisible-by? year 400)]
+  (case [factor-of-4 factor-of-100 factor-of-400]
+    [true false false] :true
+    [true false true] :true
+    [true true true] :true 
+    [false false true] :true 
+    [false true true] :true 
+    false)))
+  
+;; (defn leap-year? [year] 
+;;   (or (and (is-divisible-by? year 4)
+;;            (not (is-divisible-by? year 100)))
+;;       (is-divisible-by? year 400)))
 
   ;; (if (zero? (mod year 4))
   ;;   (if (zero? (mod year 100)) 
