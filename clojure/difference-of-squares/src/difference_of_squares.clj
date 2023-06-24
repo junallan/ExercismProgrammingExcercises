f32edbacfc251ec217a592e9b5ad66e7781c199e
junallan <junallan@telus.net>
2023-06-24 13:20:32 -0600

    Sum of squres implementation with out recursion

--- clojure/difference-of-squares/src/difference_of_squares.clj
+++ clojure/difference-of-squares/src/difference_of_squares.clj
@@ -8,9 +8,16 @@
 )
 
 (defn sum-of-squares [num] 
-  (if (<= num 0)
-    0
-    (+ (square num) (sum-of-squares (dec num)))))
+  (loop [n num
+         acc 0]
+    (if (<= n 0)
+      acc
+      (recur (dec n) (+ acc (* n n))))))
+
+;; (defn sum-of-squares [num] 
+;;   (if (<= num 0)
+;;     0
+;;     (+ (square num) (sum-of-squares (dec num)))))
 
 (defn square-of-sum [num]
   (let [sum (reduce + (range 1 (inc num)))]
