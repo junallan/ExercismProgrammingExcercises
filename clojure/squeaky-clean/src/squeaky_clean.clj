a0d23dbe31113b38557000d670e78e8aab877c3d
junallan <junallan@telus.net>
2023-06-27 23:06:02 -0600

    Handle kebab case

--- clojure/squeaky-clean/src/squeaky_clean.clj
+++ clojure/squeaky-clean/src/squeaky_clean.clj
@@ -1,9 +1,20 @@
 (ns squeaky-clean
   (:require [clojure.string :as str]))
 
+(defn kebab-to-camel
+  "Converts a kebab-case string to camelCase."
+  [s]
+  (->> (clojure.string/split s #"-")
+       (map-indexed (fn [i part]
+                      (if (zero? i)
+                        part
+                        (clojure.string/capitalize part))))
+       (apply str)))
+
 (defn clean
   "Replaces spaces with underscores and control characters with 'CTRL' in the input string."
   [s]
   (-> s
       (str/replace #"\p{Cc}" "CTRL")
+      (kebab-to-camel)
       (str/replace " " "_")))
