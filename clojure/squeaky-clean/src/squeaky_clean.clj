9db0aebad93e843be16148d00a92e7e9e602b00f
junallan <junallan@telus.net>
2023-06-28 20:18:35 -0600

    Fixed bug with CTRL replacement

--- clojure/squeaky-clean/src/squeaky_clean.clj
+++ clojure/squeaky-clean/src/squeaky_clean.clj
@@ -1,6 +1,10 @@
 (ns squeaky-clean
   (:require [clojure.string :as str]))
 
+(defn capitalize-first-letter [input]
+  (str (str/upper-case (first input))
+       (subs input 1)))
+
 (defn kebab-to-camel
   "Converts a kebab-case string to camelCase."
   [s]
@@ -8,14 +12,16 @@
        (map-indexed (fn [i part]
                       (if (zero? i)
                         part
-                        (clojure.string/capitalize part))))
+                        (capitalize-first-letter part))))
        (apply str)))
 
 (defn clean
   "Replaces spaces with underscores and control characters with 'CTRL' in the input string."
   [s]
   (-> s
+      (kebab-to-camel)
       (str/replace #"\p{Cc}" "CTRL")
+      (str/replace #"\p{InEmoticons}" "")
       (str/replace #"\d" "")
       (str/replace #"[α-ω]" "")
       (kebab-to-camel)
