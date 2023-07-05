6b2dcecee6ae473651cc29e351c08c562a1e1e9c
junallan <junallan@telus.net>
2023-07-04 20:35:30 -0600

    Removed unecessary preprocessing of brackets

--- clojure/matching-brackets/src/matching_brackets.clj
+++ clojure/matching-brackets/src/matching_brackets.clj
@@ -1,28 +1,23 @@
 (ns matching-brackets)
 
-(require '[clojure.string :as str])
-
-(defn only-bracket-chars [string]
-  (str/replace string #"[^{}()\[\]]" ""))
-
-(defn validate-brackets [string, stack]
+(defn validate-brackets [string stack]
   (if (empty? string)
     (if (empty? stack)
       true
       false)
-    (let [
-          [first-char & rest] string
+    (let [[first-char & rest] string
           [top & bottom] stack]
       (cond
         (and (= first-char \}) (= top \{)) (validate-brackets rest bottom)
         (and (= first-char \]) (= top \[)) (validate-brackets rest bottom)
         (and (= first-char \)) (= top \()) (validate-brackets rest bottom)
-        :else (validate-brackets rest (cons first-char stack))
-        ))))
-
+        (#{\{ \[ \( \} \] \)} first-char) (validate-brackets rest (cons first-char stack))
+        :else (validate-brackets rest stack)))))
+        
 (defn valid? [string]
-  (validate-brackets (only-bracket-chars string) [])
-)
+  (validate-brackets string []))
+
+
 
 
 
