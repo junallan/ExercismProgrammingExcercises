ccadc3563af91c82c86f0ecba733761c21cac68c
junallan <junallan@telus.net>
2023-07-08 08:59:16 -0600

    Hello world

---
+++ fortran/hello-world/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/junallanparreno/Projects/ExercismProgrammingExcercises/fortran/hello-world
+# Build directory: /Users/junallanparreno/Projects/ExercismProgrammingExcercises/fortran/hello-world/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(hello-world_test "hello-world")
+set_tests_properties(hello-world_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/junallanparreno/Projects/ExercismProgrammingExcercises/fortran/hello-world/CMakeLists.txt;67;add_test;/Users/junallanparreno/Projects/ExercismProgrammingExcercises/fortran/hello-world/CMakeLists.txt;0;")
+subdirs("testlib")
