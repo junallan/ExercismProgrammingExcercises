c9e57ea0ab6a4f482d2d05b5a0b34ec0f91215ca
Jun Allan Parreno <junallanparreno@Juns-MacBook-Pro.local>
2023-07-16 11:50:29 -0600

    Refactored consolidating lower and upper case functions

--- fortran/bob/bob.f90
+++ fortran/bob/bob.f90
@@ -10,19 +10,19 @@ contains
     logical :: is_yelling
     logical :: ends_with_question_mark
 
-    is_lowercase_found = check_lowercase(statement)
-    is_uppercase_found = check_uppercase(statement)
+    is_lowercase_found = check_character_range(statement, 'a', 'z')
+    is_uppercase_found = check_character_range(statement, 'A', 'Z')
     
     if (LEN_TRIM(statement) == 0) then
       hey = "Fine. Be that way!"
       return
     end if
 
-    is_yelling = (.NOT. is_lowercase_found) .and. is_uppercase_found
+    is_yelling = (.NOT. is_lowercase_found) .AND. is_uppercase_found
 
-    ends_with_question_mark = LEN_TRIM(statement) > 0 .and. statement(LEN_TRIM(statement):LEN_TRIM(statement)) == '?'
+    ends_with_question_mark = LEN_TRIM(statement) > 0 .AND. statement(LEN_TRIM(statement):LEN_TRIM(statement)) == '?'
 
-    if (ends_with_question_mark .and. is_yelling) then
+    if (ends_with_question_mark .AND. is_yelling) then
       hey = "Calm down, I know what I'm doing!"
     else if (ends_with_question_mark) then
       hey = "Sure."
@@ -33,31 +33,19 @@ contains
     end if
   end function hey
 
-  function check_lowercase(str) result(is_lowercase)
+  function check_character_range(str, lower_char, upper_char) result(is_found)
     character(len=*), intent(in) :: str
-    logical :: is_lowercase
+    character(len=1), intent(in) :: lower_char, upper_char
+    logical :: is_found
     integer :: i
 
-    is_lowercase = .FALSE.
+    is_found = .FALSE.
     do i = 1, len_trim(str)
-      if (ichar(str(i:i)) >= ichar('a') .and. ichar(str(i:i)) <= ichar('z')) then
-        is_lowercase = .TRUE.
+      if (ichar(str(i:i)) >= ichar(lower_char) .AND. ichar(str(i:i)) <= ichar(upper_char)) then
+        is_found = .TRUE.
         exit
       end if
     end do
-  end function check_lowercase
+  end function check_character_range
 
-  function check_uppercase(str) result(is_uppercase)
-    character(len=*), intent(in) :: str
-    logical :: is_uppercase
-    integer :: i
-
-    is_uppercase = .FALSE.
-    do i = 1, len_trim(str)
-      if (ichar(str(i:i)) >= ichar('A') .and. ichar(str(i:i)) <= ichar('Z')) then
-        is_uppercase = .TRUE.
-        exit
-      end if
-    end do
-  end function check_uppercase
 end module bob
