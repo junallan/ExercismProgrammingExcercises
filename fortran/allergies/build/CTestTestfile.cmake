0c4a15266fbc5a7e773b6c39d158b018da08a5f5
Jun Allan Parreno <junallanparreno@Juns-MacBook-Pro.local>
2023-07-23 11:11:08 -0600

    Implemented allergies algorithm

---
+++ fortran/allergies/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/junallanparreno/Projects/ExercismProgrammingExcercises/fortran/allergies
+# Build directory: /Users/junallanparreno/Projects/ExercismProgrammingExcercises/fortran/allergies/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(allergies_test "allergies")
+set_tests_properties(allergies_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/junallanparreno/Projects/ExercismProgrammingExcercises/fortran/allergies/CMakeLists.txt;67;add_test;/Users/junallanparreno/Projects/ExercismProgrammingExcercises/fortran/allergies/CMakeLists.txt;0;")
+subdirs("testlib")
