5369b642dbe242034b8b4baca1c276ea30a8f917
Jun Allan Parreno <junallanparreno@Juns-MacBook-Pro-2.local>
2023-08-21 23:02:27 -0600

    Simplified isIscoceles test

--- typescript/triangle/triangle.ts
+++ typescript/triangle/triangle.ts
@@ -15,17 +15,11 @@ export class Triangle {
     if (this.sides.length !== 3 || this,this.sides.includes(0)) return false;
     if (this.isEquilateral) return true;
 
-    const elementCounts = getElementCounts(this.sides);
+    if(!isTriangle(this.sides[0], this.sides[1], this.sides[2])) return false;
 
-    if (elementCounts.size !== 2) return false;
-
-    const [firstLengthCount, secondLengthCount] = elementCounts.values();
-    const [firstLengthKey, secondLengthKey] = elementCounts.keys();
-
-    return (
-      (firstLengthCount === 2 && (firstLengthKey + firstLengthKey) > secondLengthKey) ||
-      (secondLengthCount === 2 && (secondLengthKey + secondLengthKey) > firstLengthKey)
-    );
+    return this.sides[0] === this.sides[1] 
+      || this.sides[0] === this.sides[2]
+      || this.sides[1] === this.sides[2];
   }
 
   get isScalene() {
@@ -33,10 +27,11 @@ export class Triangle {
   }
 }
 
-function getElementCounts(array: number[]) : Map<number, number> {
-  return array.reduce((counts, item) => {
-    counts.set(item, (counts.get(item) || 0) + 1);
-    return counts;
-  }, new Map<number, number>());
+function isTriangle(a: number, b: number, c: number) :boolean {
+  return ((a + b) >= c)
+    && ((b + c) >= a)
+    && ((a + c) >= b);
 }
+
+
 
